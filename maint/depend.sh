65970eddaefbd647410bf0bf11be391ba6ad594e
Phil Carns <carns@mcs.anl.gov>
2003-01-10 02:57:26 +0000

    modified build system to build seperate objects with a "-server.o" suffix for use in the server; so far only doing this with the gossip library as a test case
    SVN revision 18

--- maint/depend.sh
+++ maint/depend.sh
@@ -3,9 +3,9 @@ DIR="$1"
 shift 1
 case "$DIR" in
 "" | ".")
-gcc -M "$@" | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@'
+gcc -M "$@" | sed -e 's@^\(.*\)\.o:@\1.d \1.o \1-server.o:@'
 ;;
 *)
-gcc -M "$@" | sed -e "s@^\(.*\)\.o:@$DIR/\1.d $DIR/\1.o:@"
+gcc -M "$@" | sed -e "s@^\(.*\)\.o:@$DIR/\1.d $DIR/\1.o $DIR/\1-server.o:@"
 ;;
 esac
