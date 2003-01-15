496263bb544cf90c10bdc9c4c50d5c6ab230fd23
Dale Whitchurch <dalew@parl.clemson.edu>
2003-01-15 19:10:47 +0000

    *** empty log message ***
    SVN revision 120

--- src/proto/contig/contig-response-d.c
+++ src/proto/contig/contig-response-d.c
@@ -18,24 +18,26 @@ DECODE_RESP_HEAD(do_decode_resp)
 
 	struct PVFS_server_resp_s *response = input_buffer;
 
-	target_msg->buffer = input_buffer;
+	target_msg->buffer = malloc(response->rsize);
+	memcpy(target_msg->buffer,response,response->rsize);
 	switch(response->op)
 	{
 		case PVFS_SERV_CREATE:
-			//memcpy(target_msg->buffer,response,sizeof(struct PVFS_server_resp_s));
 			return 0;
 		case PVFS_SERV_GETCONFIG:
-			response->u.getconfig.meta_server_mapping = (char *)input_buffer 
-											+ sizeof(struct PVFS_server_resp_s);
-			response->u.getconfig.io_server_mapping = (char *)input_buffer 
-											+ sizeof(struct PVFS_server_resp_s)
-											+ strlen(response->u.getconfig.meta_server_mapping)+1;
+			((struct PVFS_server_resp_s *)target_msg->buffer)->u.getconfig.meta_server_mapping 
+					= (char *)(target_msg->buffer 
+							+ sizeof(struct PVFS_server_resp_s));
+			((struct PVFS_server_resp_s *)target_msg->buffer)->u.getconfig.io_server_mapping 
+					= (char *)(target_msg->buffer 
+							+ sizeof(struct PVFS_server_resp_s)
+							+ strlen(((struct PVFS_server_resp_s *)target_msg->buffer)->u.getconfig.meta_server_mapping)+1);
 			return 0;
 		case PVFS_SERV_LOOKUP_PATH:
-			response->u.lookup_path.handle_array = 
-										(PVFS_handle *) ((char*)input_buffer + 
-									  sizeof(struct PVFS_server_resp_s));
-			response->u.lookup_path.attr_array = 
+			((struct PVFS_server_resp_s *)target_msg->buffer)->u.lookup_path.handle_array = 
+							(PVFS_handle *) ((char*)input_buffer + 
+							  sizeof(struct PVFS_server_resp_s));
+			((struct PVFS_server_resp_s *)target_msg->buffer)->u.lookup_path.attr_array = 
 						(PVFS_object_attr *) ((char*)input_buffer 
 						+ sizeof(struct PVFS_server_resp_s) + 
 						sizeof(PVFS_handle)*response->u.lookup_path.count);
