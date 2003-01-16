8318e56f1bc4b85a55dadc2ccd92f6d72ebbb8ca
Frank Shorter <fshorte@clemson.edu>
2003-01-16 22:14:49 +0000

    moved structures related to the pcache into pcache.h
    SVN revision 162

--- src/client/sysint/pint-sysint.h
+++ src/client/sysint/pint-sysint.h
@@ -115,41 +115,6 @@ struct error_stats
 	int num_pending;
 };
 
-/* Pcache Related Declarations */
-/* Pinode structure */
-typedef struct 
-{
-	pinode_reference pinode_ref; /* pinode reference - entity to
-											  uniquely identify a pinode */
-	gen_mutex_t *pinode_mutex;	  /* mutex lock */
-	struct PVFS_object_attr attr;	/* attributes of PVFS object */
-	PVFS_bitfield mask;			  /* attribute mask */
-	PVFS_size size;				  /* PVFS object size */
-	struct timeval tstamp_handle;/* timestamp for handle consistency */
-	struct timeval tstamp_attr;  /* timestamp for attribute consistency */
-	struct timeval tstamp_size;  /* timestamp for size consistency */
-} pinode, *pinode_p;
-
-/* Pinode Cache Element */
-struct cache_t 
-{
-	pinode *pnode;
-	int16_t prev;
-	int16_t next;
-};
-
-/* Pinode Cache Management structure */
-struct pinodecache {
-	struct cache_t element[MAX_ENTRIES];
-	int count;
-	int16_t top; 	/* Add at top */
-	int16_t free;	/* Points to next free array element - kind of like
-						   a free list */
-	int16_t bottom; /* Replace at bottom */
-	gen_mutex_t *mt_lock;/* Mutex */
-};
-typedef struct pinodecache pcache;
-
 /* PVFS Object - File name + Collection ID */
 /*typedef struct
 {
