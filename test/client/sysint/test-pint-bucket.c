eb337494a2f0d26109b6fb13d2d3c378e3ae86d9
Phil Carns <carns@mcs.anl.gov>
2003-01-16 20:55:21 +0000

    Added the beggining of a test harness for the bucket interface
    SVN revision 158

---
+++ test/client/sysint/test-pint-bucket.c
@@ -0,0 +1,17 @@
+/*
+ * (C) 2001 Clemson University and The University of Chicago
+ *
+ * See COPYING in top-level directory.
+ */
+
+#include <pint-bucket.h>
+
+/* this is a test program that exercises the bucket interface and
+ * demonstrates how to use it.
+ */
+
+int main(int argc, char **argv)	
+{
+
+	return(0);
+}
