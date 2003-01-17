27a17fddd57dceb95e0f5b3d32ea6ade2a0e819e
Rob Ross <rross@mcs.anl.gov>
2003-01-17 15:54:14 +0000

    formatted so i can print it.
    SVN revision 182

--- src/client/sysint/pcache.h
+++ src/client/sysint/pcache.h
@@ -10,44 +10,45 @@
 
 #include <limits.h>
 #include <string.h>
-#include <gen-locks.h>
-#include <pvfs2-types.h>
-#include <pvfs2-attr.h>
-#include <pint-sysint.h>
+
+#include "gen-locks.h"
+#include "pvfs2-types.h"
+#include "pvfs2-attr.h"
+#include "pint-sysint.h"
 
 /* Pcache Related Declarations */
 /* Pinode structure */
 typedef struct
 {
-        pinode_reference pinode_ref; /* pinode reference - entity to
+    pinode_reference pinode_ref; /* pinode reference - entity to
 
-          uniquely identify a pinode */
-        gen_mutex_t *pinode_mutex;        /* mutex lock */
-        struct PVFS_object_attr attr;   /* attributes of PVFS object */
-        PVFS_bitfield mask;                       /* attribute mask */
-        PVFS_size size;                           /* PVFS object size */
-        struct timeval tstamp_handle;/* timestamp for handle consistency */
-        struct timeval tstamp_attr;  /* timestamp for attribute consistency */
-        struct timeval tstamp_size;  /* timestamp for size consistency */
+				    uniquely identify a pinode */
+    gen_mutex_t *pinode_mutex;        /* mutex lock */
+    struct PVFS_object_attr attr;   /* attributes of PVFS object */
+    PVFS_bitfield mask;                       /* attribute mask */
+    PVFS_size size;                           /* PVFS object size */
+    struct timeval tstamp_handle;/* timestamp for handle consistency */
+    struct timeval tstamp_attr;  /* timestamp for attribute consistency */
+    struct timeval tstamp_size;  /* timestamp for size consistency */
 } pinode, *pinode_p;
 
 /* Pinode Cache Element */
 struct cache_t
 {
-        pinode *pnode;
-        int16_t prev;
-        int16_t next;
+    pinode *pnode;
+    int16_t prev;
+    int16_t next;
 };
 
 /* Pinode Cache Management structure */
 struct pinodecache {
-        struct cache_t element[MAX_ENTRIES];
-        int count;
-        int16_t top;    /* Add at top */
-        int16_t free;   /* Points to next free array element - kind of like
-                                                   a free list */
-        int16_t bottom; /* Replace at bottom */
-        gen_mutex_t *mt_lock;/* Mutex */
+    struct cache_t element[MAX_ENTRIES];
+    int count;
+    int16_t top;    /* Add at top */
+    int16_t free;   /* Points to next free array element - kind of like
+		       a free list */
+    int16_t bottom; /* Replace at bottom */
+    gen_mutex_t *mt_lock;/* Mutex */
 };
 typedef struct pinodecache pcache;
 
@@ -60,5 +61,13 @@ int pcache_remove(pcache *cache, pinode_reference refn,pinode **item);
 int pcache_pinode_alloc(pinode **pnode);
 void pcache_pinode_dealloc(pinode *pnode);
 
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
 
 #endif
