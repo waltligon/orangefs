0f03ffa8ad1ecb6200e5cdcd0117c4fe08ed5dfe
Phil Carns <carns@mcs.anl.gov>
2003-01-17 19:14:28 +0000

    moved a bunch of (now internal) type definitions from the header file to the c file
    SVN revision 184

--- src/client/sysint/pint-dcache.c
+++ src/client/sysint/pint-dcache.c
@@ -8,6 +8,34 @@
 
 #include <pint-dcache.h>
 
+/* Dcache Entry */
+struct dcache_entry_s {
+	pinode_reference parent;   /* the pinode of the parent directory */
+	char name[PVFS_NAME_MAX];  /* PVFS object name */
+	pinode_reference entry;    /* the pinode of entry in parent */
+	struct timeval tstamp_valid;  /* timestamp indicating validity period */
+};
+typedef struct dcache_entry_s dcache_entry;
+
+/* Dcache element */
+struct dcache_t {
+	dcache_entry dentry;
+	int16_t prev;
+	int16_t next;
+};
+
+/* Cache Management structure */
+struct dcache {
+	struct dcache_t element[PINT_DCACHE_MAX_ENTRIES];
+	int count;
+	int16_t top;
+	int16_t free;
+	int16_t bottom;
+	gen_mutex_t *mt_lock;
+};
+typedef struct dcache dcache;
+
+
 static void dcache_remove_dentry(int16_t item);
 static int dcache_update_dentry_timestamp(dcache_entry* entry); 
 static int check_dentry_expiry(struct timeval t2);
--- src/client/sysint/pint-dcache.h
+++ src/client/sysint/pint-dcache.h
@@ -20,36 +20,6 @@
 /* value passed out to indicate lookups that didn't find a match */
 #define PINT_DCACHE_HANDLE_INVALID 0
 
-/* TODO: move these definitions into the c file */
-
-/* Dcache Entry */
-struct dcache_entry_s {
-	pinode_reference parent;   /* the pinode of the parent directory */
-	char name[PVFS_NAME_MAX];  /* PVFS object name */
-	pinode_reference entry;    /* the pinode of entry in parent */
-	struct timeval tstamp_valid;  /* timestamp indicating validity period */
-};
-typedef struct dcache_entry_s dcache_entry;
-
-/* Dcache element */
-struct dcache_t {
-	dcache_entry dentry;
-	int16_t prev;
-	int16_t next;
-};
-
-/* Cache Management structure */
-struct dcache {
-	struct dcache_t element[PINT_DCACHE_MAX_ENTRIES];
-	int count;
-	int16_t top;
-	int16_t free;
-	int16_t bottom;
-	gen_mutex_t *mt_lock;
-};
-typedef struct dcache dcache;
-
-
 /* Function Prototypes */
 int PINT_dcache_lookup(
 	char *name, 
