76d87b76b5ae9e229e793675d82fd9293fbb18e9
Frank Shorter <fshorte@clemson.edu>
2003-03-07 23:36:04 +0000

    test harness to test the string parsing functions I use to parse the path inside of lookup requests:  get_next_path()  get_path_element()
    SVN revision 394

---
+++ test/client/sysint/path-test.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pint-sysint.h>
+
+/*int get_path_element(char *path, char** segment, int element)*/
+
+int main(int argc, char* argv[])
+{
+	char* path = NULL;
+	char *segment = NULL;
+	int num_segments = 10,i, ret=0;
+
+	path = malloc(41);
+
+	strcpy(path,"/home/fshorte/porn/mp3s/crap/foobar.txt");
+
+	printf("path = %s\n",path);
+#if 0
+	for(i= 0; i < num_segments; i++)
+	{
+		ret = get_next_path(path,&segment,25);
+		if (ret < 0)
+		{
+			printf("errcode = %d\n",ret);
+			continue;
+		}
+		printf("segment = %s\n",segment);
+		free(segment);
+	}
+#endif
+	for(i= 0; i < num_segments; i++)
+	{
+		ret = get_path_element(path,&segment,i);
+		if (ret < 0)
+		{
+			printf("errcode = %d\n",ret);
+			continue;
+		}
+		printf("segment = %s\n",segment);
+		free(segment);
+	}
+	free(path);
+	return 0;
+}
