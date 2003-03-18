c3a266bcd2aab73291c9f78b38a1a845ca8ed6ee
Phil Carns <carns@mcs.anl.gov>
2003-03-18 00:53:16 +0000

    added in a test program for the request scheduler (forgot to move it from old pvfs2 tree until now)
    SVN revision 480

---
+++ test/server/request-scheduler/request-scheduler-test.c
@@ -0,0 +1,140 @@
+/*
+ * (C) 2001 Clemson University and The University of Chicago
+ *
+ * See COPYING in top-level directory.
+ */
+
+/* test program for the request scheduler API */
+
+#include <stdio.h>
+
+#include <request-scheduler.h>
+#include <pvfs2-req-proto.h>
+
+int main(int argc, char **argv)	
+{
+	int ret;
+	struct PVFS_server_req_s req_array[4];
+	req_sched_id id_array[4];
+	req_sched_id id_arrayB[4];
+	int count = 0;
+	int status = 0;
+
+	/* setup some requests to test */
+	req_array[0].op = PVFS_SERV_GETATTR;
+	req_array[0].u.getattr.handle = 5;
+
+	req_array[1].op = PVFS_SERV_SETATTR;
+	req_array[1].u.setattr.handle = 5;
+
+	req_array[2].op = PVFS_SERV_GETATTR;
+	req_array[2].u.getattr.handle = 6;
+
+	req_array[3].op = PVFS_SERV_SETATTR;
+	req_array[3].u.setattr.handle = 5;
+
+
+	/* initialize scheduler */
+	ret = PINT_req_sched_initialize();
+	if(ret < 0)
+	{
+		fprintf(stderr, "Error: initialize failure.\n");
+		return(-1);
+	}
+
+	/* try to schedule first request- it should proceed */
+	ret = PINT_req_sched_post(&(req_array[0]), NULL, &(id_array[0]));
+	if(ret != 1)
+	{
+		fprintf(stderr, "Error: 1st post should immediately complete.\n");
+		return(-1);
+	}
+
+	/* try to schedule second request- it should queue up */
+	ret = PINT_req_sched_post(&(req_array[1]), NULL, &(id_array[1]));
+	if(ret != 0)
+	{
+		fprintf(stderr, "Error: 2nd post should queue.\n");
+		return(-1);
+	}
+
+	/* try to schedule third request- it should proceed */
+	ret = PINT_req_sched_post(&(req_array[2]), NULL, &(id_array[2]));
+	if(ret != 1)
+	{
+		fprintf(stderr, "Error: 3rd post should immediately complete.\n");
+		return(-1);
+	}
+
+	/* try to schedule fourth request- it should queue up */
+	ret = PINT_req_sched_post(&(req_array[3]), NULL, &(id_array[3]));
+	if(ret != 0)
+	{
+		fprintf(stderr, "Error: 4th post should queue.\n");
+		return(-1);
+	}
+
+	/*********************************************************/
+
+	/* test the second one and make sure it doesn't finish */
+	ret = PINT_req_sched_test(id_array[1], &count, NULL, &status); 
+	if(ret != 0 || count != 0)
+	{
+		fprintf(stderr, "Error: test of 2nd request failed.\n");
+		return(-1);
+	}
+
+	/* unpost the 2nd request */
+	ret = PINT_req_sched_unpost(id_array[1], NULL);
+	if(ret != 0)
+	{
+		fprintf(stderr, "Error: upost failure.\n");
+		return(-1);
+	}
+
+	/* complete the first request */
+	ret = PINT_req_sched_release(id_array[0], NULL,
+		&(id_arrayB[0]));
+	if(ret != 1)
+	{
+		fprintf(stderr, "Error: release didn't immediately
+			complete.\n");
+		return(-1);
+	}
+	
+	/* now the 4th request should be ready to go */
+	ret = PINT_req_sched_test(id_array[3], &count, NULL, &status); 
+	if(ret != 0 || count != 1 || status != 0)
+	{
+		fprintf(stderr, "Error: test of 4th request failed.\n");
+		return(-1);
+	}
+
+	/* complete the 3rd and 4th requests */
+	ret = PINT_req_sched_release(id_array[2], NULL,
+		&(id_arrayB[2]));
+	if(ret != 1)
+	{
+		fprintf(stderr, "Error: release didn't immediately
+			complete.\n");
+		return(-1);
+	}
+	ret = PINT_req_sched_release(id_array[3], NULL,
+		&(id_arrayB[3]));
+	if(ret != 1)
+	{
+		fprintf(stderr, "Error: release didn't immediately
+			complete.\n");
+		return(-1);
+	}
+
+	/* shut down scheduler */
+	ret = PINT_req_sched_finalize();
+	if(ret < 0)
+	{
+		fprintf(stderr, "Error: finalize failure.\n");
+		return(-1);
+	}
+
+	return(0);
+}
