74f8edbe312cf5dac8c1b3cd6ad23732ec6738a4
Rob Latham <robl@mcs.anl.gov>
2003-03-20 23:02:54 +0000

    a great big ol' reformatting change.
    SVN revision 655

--- test/correctness/pts/create.c
+++ test/correctness/pts/create.c
@@ -17,111 +17,116 @@
 #define ATTR_MTIME 32
 #define ATTR_TYPE 2048
 
-extern int parse_pvfstab(char *fn,pvfs_mntlist *mnt);
-int compare_attribs(PVFS_object_attr attr1, PVFS_object_attr attr2);
+extern int parse_pvfstab(char *fn,
+			 pvfs_mntlist * mnt);
+int compare_attribs(PVFS_object_attr attr1,
+		    PVFS_object_attr attr2);
 
 /* files, directories, tree of directories */
-int create_file(PVFS_fs_id fs_id, char *dirname, char *filename)
+int create_file(PVFS_fs_id fs_id,
+		char *dirname,
+		char *filename)
 {
 
-	PVFS_sysreq_lookup req_look;
-	PVFS_sysresp_lookup resp_look;
-	PVFS_sysreq_create *req_create = NULL;
-	PVFS_sysresp_create *resp_create = NULL;
-	PVFS_sysreq_getattr req_getattr;
-	PVFS_sysresp_getattr resp_getattr;
-	int ret;
+    PVFS_sysreq_lookup req_look;
+    PVFS_sysresp_lookup resp_look;
+    PVFS_sysreq_create *req_create = NULL;
+    PVFS_sysresp_create *resp_create = NULL;
+    PVFS_sysreq_getattr req_getattr;
+    PVFS_sysresp_getattr resp_getattr;
+    int ret;
 
-	/* lookup the root handle */
-	req_look.credentials.perms = 1877;
-	req_look.name = strdup(dirname);
-	req_look.fs_id = fs_id;
-	ret = PVFS_sys_lookup(&req_look,&resp_look);
-	if (ret < 0)
-	{
-		printf("Lookup failed with errcode = %d\n", ret);
-		return(-1);
-	}
-	free(req_look.name);
+    /* lookup the root handle */
+    req_look.credentials.perms = 1877;
+    req_look.name = strdup(dirname);
+    req_look.fs_id = fs_id;
+    ret = PVFS_sys_lookup(&req_look, &resp_look);
+    if (ret < 0)
+    {
+	printf("Lookup failed with errcode = %d\n", ret);
+	return (-1);
+    }
+    free(req_look.name);
 
-	/* test create */
-	req_create = (PVFS_sysreq_create *)malloc(sizeof(PVFS_sysreq_create));
-	if (!req_create)
-	{
-		printf("Error in malloc\n");
-		return(-1);
-	}
-	resp_create = (PVFS_sysresp_create *)malloc(sizeof(PVFS_sysresp_create));
-	if (!resp_create)
-	{
-		printf("Error in malloc\n");
-		return(-1);
-	}
+    /* test create */
+    req_create = (PVFS_sysreq_create *) malloc(sizeof(PVFS_sysreq_create));
+    if (!req_create)
+    {
+	printf("Error in malloc\n");
+	return (-1);
+    }
+    resp_create = (PVFS_sysresp_create *) malloc(sizeof(PVFS_sysresp_create));
+    if (!resp_create)
+    {
+	printf("Error in malloc\n");
+	return (-1);
+    }
 
-	// Fill in the create info 
-	req_create->entry_name = strdup(filename);
-	if (!req_create->entry_name)
-	{
-		printf("Error in malloc\n");
-		return(-1);
-	}
-	req_create->attrmask = (ATTR_UID | ATTR_GID | ATTR_PERM);
-	req_create->attr.owner = 100;
-	req_create->attr.group = 100;
-	req_create->attr.perms = 1877;
-	req_create->attr.atime = req_create->attr.mtime = req_create->attr.ctime = 0xdeadbeef;
+    // Fill in the create info 
+    req_create->entry_name = strdup(filename);
+    if (!req_create->entry_name)
+    {
+	printf("Error in malloc\n");
+	return (-1);
+    }
+    req_create->attrmask = (ATTR_UID | ATTR_GID | ATTR_PERM);
+    req_create->attr.owner = 100;
+    req_create->attr.group = 100;
+    req_create->attr.perms = 1877;
+    req_create->attr.atime = req_create->attr.mtime = req_create->attr.ctime = 0xdeadbeef;
 
-	req_create->credentials.uid = 100;
-	req_create->credentials.gid = 100;
-	req_create->credentials.perms = 1877;
+    req_create->credentials.uid = 100;
+    req_create->credentials.gid = 100;
+    req_create->credentials.perms = 1877;
 
-	req_create->attr.u.meta.nr_datafiles = 4;
+    req_create->attr.u.meta.nr_datafiles = 4;
 
-	req_create->parent_refn.handle = resp_look.pinode_refn.handle;
-	req_create->parent_refn.fs_id = req_look.fs_id;
+    req_create->parent_refn.handle = resp_look.pinode_refn.handle;
+    req_create->parent_refn.fs_id = req_look.fs_id;
 
-	
-	/* Fill in the dist -- NULL means the system interface used the 
-	 * "default_dist" as the default
-	 */
-	req_create->attr.u.meta.dist = NULL;
 
-	// call create 
-	ret = PVFS_sys_create(req_create,resp_create);
-	if (ret < 0)
-	{
-		printf("create failed with errcode = %d\n", ret);
-		return(-1);
-	}
+    /* Fill in the dist -- NULL means the system interface used the 
+     * "default_dist" as the default
+     */
+    req_create->attr.u.meta.dist = NULL;
 
-	req_getattr.pinode_refn.handle = resp_create->pinode_refn.handle;
-	req_getattr.pinode_refn.fs_id = fs_id;
-	req_getattr.attrmask = ATTR_META;
-	/* passing this stuff to the server at one point would make it
-	 * segfault: it's not a propper getattr request, but segfaulting is a
-	 * bit extreme */
+    // call create 
+    ret = PVFS_sys_create(req_create, resp_create);
+    if (ret < 0)
+    {
+	printf("create failed with errcode = %d\n", ret);
+	return (-1);
+    }
+
+    req_getattr.pinode_refn.handle = resp_create->pinode_refn.handle;
+    req_getattr.pinode_refn.fs_id = fs_id;
+    req_getattr.attrmask = ATTR_META;
+    /* passing this stuff to the server at one point would make it
+     * segfault: it's not a propper getattr request, but segfaulting is a
+     * bit extreme */
 #if 0
-	req_getattr.credentials.uid = 100;
-	req_getattr.credentials.gid = 100;
-	req_getattr.credentials.perms = 1877;
+    req_getattr.credentials.uid = 100;
+    req_getattr.credentials.gid = 100;
+    req_getattr.credentials.perms = 1877;
 #endif
-	ret = PVFS_sys_getattr(&req_getattr, &resp_getattr);
-	if (ret < 0)
-	{
-		printf("getattr failed with errcode = %d\n", ret);
-		return(-1);
-	}
-	ret = compare_attribs(req_create->attr, resp_getattr.attr);
-	if (ret < 0)
-	{
-		printf("file created has incorrect attributes\n");
-		return -1;
-	}
-	free(req_create->entry_name);
-	free(req_create);
-	free(resp_create);
-	return 0;
+    ret = PVFS_sys_getattr(&req_getattr, &resp_getattr);
+    if (ret < 0)
+    {
+	printf("getattr failed with errcode = %d\n", ret);
+	return (-1);
+    }
+    ret = compare_attribs(req_create->attr, resp_getattr.attr);
+    if (ret < 0)
+    {
+	printf("file created has incorrect attributes\n");
+	return -1;
+    }
+    free(req_create->entry_name);
+    free(req_create);
+    free(resp_create);
+    return 0;
 }
+
 /*
  * compare members of two attributes structures
  * 	currently reports verbosely what members differ, but perhaps we should
@@ -131,105 +136,131 @@ int create_file(PVFS_fs_id fs_id, char *dirname, char *filename)
  * 	0 	if equivalent	
  * 	-1 	if we found a difference
  */
-int compare_attribs(PVFS_object_attr attr1, PVFS_object_attr attr2) 
+int compare_attribs(PVFS_object_attr attr1,
+		    PVFS_object_attr attr2)
 {
-	int i;
+    int i;
 
-	if (attr1.owner != attr2.owner) {
-		printf("compare_attribs: owner differs\n");
-		return -1;
-	}
-	if (attr1.group != attr2.group) {
-		printf("compare_attribs: group differs\n");
-		return -1;
-	}
-	if (attr1.perms != attr2.perms) {
-		printf("compare_attribs: perms differs\n");
-		return -1;
-	}
-	if (attr1.atime != attr2.atime) {
-		printf("compare_attribs: atime differs\n");
-		return -1;
-	}
-	if (attr1.mtime != attr2.mtime) {
-		printf("compare_attribs: mtime differs\n");
-		return -1;
-	}
-	if (attr1.ctime != attr2.ctime) {
-		printf("compare_attribs: ctime differs\n");
-		return -1;
-	}
-	/* does it make sense to compare  the following attributes? objtype,
-	 * for example, doesn't get set by the caller */
+    if (attr1.owner != attr2.owner)
+    {
+	printf("compare_attribs: owner differs\n");
+	return -1;
+    }
+    if (attr1.group != attr2.group)
+    {
+	printf("compare_attribs: group differs\n");
+	return -1;
+    }
+    if (attr1.perms != attr2.perms)
+    {
+	printf("compare_attribs: perms differs\n");
+	return -1;
+    }
+    if (attr1.atime != attr2.atime)
+    {
+	printf("compare_attribs: atime differs\n");
+	return -1;
+    }
+    if (attr1.mtime != attr2.mtime)
+    {
+	printf("compare_attribs: mtime differs\n");
+	return -1;
+    }
+    if (attr1.ctime != attr2.ctime)
+    {
+	printf("compare_attribs: ctime differs\n");
+	return -1;
+    }
+    /* does it make sense to compare  the following attributes? objtype,
+     * for example, doesn't get set by the caller */
 #if 0
-	if (attr1.objtype != attr2.objtype) {
-		printf("compare_attribs: objtype differs\n");
-		return -1;
+    if (attr1.objtype != attr2.objtype)
+    {
+	printf("compare_attribs: objtype differs\n");
+	return -1;
+    }
+    /* TODO: i know these are going to be metafiles, but if this test is to
+     * be a generic attribute compare, it should switch based on 'objtype'
+     */
+    /* what metafile attributes are worth comparing? */
+    if (attr1.u.meta.dist != attr2.u.meta.dist)
+    {
+	printf("compare_attribs: dist info differs\n");
+	return -1;
+    }
+    if (attr1.u.meta.nr_datafiles != attr2.u.meta.nr_datafiles)
+    {
+	printf("compare_attribs: nr_datafiles differ\n");
+	return -1;
+    }
+    if (attr1.u.meta.dist_size != attr2.u.meta.dist_size)
+    {
+	printf("compare_attribs: dist_size differ\n");
+	return -1;
+    }
+    for (i = 0; i < attr1.u.meta.nr_datafiles; i++)
+    {
+	if (attr1.u.meta.dfh[i] != attr2.u.meta.dfh[i])
+	{
+	    printf("compare_attribs: dfh[%d] differs\n", i);
+	    return -1;
 	}
-	/* TODO: i know these are going to be metafiles, but if this test is to
-	 * be a generic attribute compare, it should switch based on 'objtype'
-	 */
-	/* what metafile attributes are worth comparing? */
-	if (attr1.u.meta.dist != attr2.u.meta.dist) {
-		printf("compare_attribs: dist info differs\n");
-		return -1;
-	}
-	if (attr1.u.meta.nr_datafiles != attr2.u.meta.nr_datafiles) {
-		printf("compare_attribs: nr_datafiles differ\n");
-		return -1;
-	}
-	if (attr1.u.meta.dist_size != attr2.u.meta.dist_size) {
-		printf("compare_attribs: dist_size differ\n");
-		return -1;
-	}
-	for (i=0; i<attr1.u.meta.nr_datafiles; i++) {
-		if (attr1.u.meta.dfh[i] != attr2.u.meta.dfh[i]) {
-			printf("compare_attribs: dfh[%d] differs\n", i);
-			return -1;
-		}
-	}
+    }
 #endif
-	/* TODO: doesn't compare extended attributes */
-	return 0;
+    /* TODO: doesn't compare extended attributes */
+    return 0;
 }
 
-int test_create(MPI_Comm *comm, int rank, char *buf, void *params)
+int test_create(MPI_Comm * comm,
+		int rank,
+		char *buf,
+		void *params)
 {
-	PVFS_sysresp_init resp_init;
-	int ret = -1;
-	pvfs_mntlist mnt = {0,NULL};
-	generic_params *myparams = (generic_params *)params;
-	char name[PVFS_NAME_MAX];
-	int i, nerrs=0;
+    PVFS_sysresp_init resp_init;
+    int ret = -1;
+    pvfs_mntlist mnt = { 0, NULL };
+    generic_params *myparams = (generic_params *) params;
+    char name[PVFS_NAME_MAX];
+    int i, nerrs = 0;
 
-	/* Parse PVFStab */
-	ret = parse_pvfstab(NULL,&mnt);
-	if (ret < 0)
-	{
-		printf("Parsing error\n");
-		return(-1);
-	}
+    /* Parse PVFStab */
+    ret = parse_pvfstab(NULL, &mnt);
+    if (ret < 0)
+    {
+	printf("Parsing error\n");
+	return (-1);
+    }
 
-	/*Init the system interface*/
-	ret = PVFS_sys_initialize(mnt, &resp_init);
-	if(ret < 0)
-	{
-		printf("PVFS_sys_initialize() failure. = %d\n", ret);
-		return(ret);
-	}
+    /*Init the system interface */
+    ret = PVFS_sys_initialize(mnt, &resp_init);
+    if (ret < 0)
+    {
+	printf("PVFS_sys_initialize() failure. = %d\n", ret);
+	return (ret);
+    }
 
-	for (i=0; i<myparams->mode; i++) {
-		snprintf(name, PVFS_NAME_MAX, "/%d-%d-testfile", i, rank);
-		nerrs += create_file(resp_init.fsid_list[0],  myparams->path, name);
-	}
+    for (i = 0; i < myparams->mode; i++)
+    {
+	snprintf(name, PVFS_NAME_MAX, "/%d-%d-testfile", i, rank);
+	nerrs += create_file(resp_init.fsid_list[0], myparams->path, name);
+    }
 
-	//close it down
-	ret = PVFS_sys_finalize();
-	if (ret < 0)
-	{
-		printf("finalizing sysint failed with errcode = %d\n", ret);
-		return (-1);
-	}
+    //close it down
+    ret = PVFS_sys_finalize();
+    if (ret < 0)
+    {
+	printf("finalizing sysint failed with errcode = %d\n", ret);
+	return (-1);
+    }
 
-	return(nerrs);
+    return (nerrs);
 }
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
--- test/correctness/pts/test_dir_torture.c
+++ test/correctness/pts/test_dir_torture.c
@@ -9,8 +9,9 @@
 #include "mpi.h"
 #include "pts.h"
 
-extern int parse_pvfstab(char *filename,pvfs_mntlist *pvfstab_p);
- 
+extern int parse_pvfstab(char *filename,
+			 pvfs_mntlist * pvfstab_p);
+
 /* 
  * helper function to initialize pvfs
  * doesn't take any parameters (relies heavily on some expected default information)
@@ -20,28 +21,29 @@ extern int parse_pvfstab(char *filename,pvfs_mntlist *pvfstab_p);
  */
 PVFS_fs_id system_init(void)
 {
-	PVFS_sysresp_init resp_init;
-	int ret = -1;
-	pvfs_mntlist mnt = {0,NULL}; /* use pvfstab in cwd */
+    PVFS_sysresp_init resp_init;
+    int ret = -1;
+    pvfs_mntlist mnt = { 0, NULL };	/* use pvfstab in cwd */
 
-	memset(&resp_init, 0, sizeof(resp_init));
+    memset(&resp_init, 0, sizeof(resp_init));
 
-	ret = parse_pvfstab(NULL, &mnt);
-	if (ret < 0)
-	{
-		printf("Parsing error\n");
-		return -1;
-	}
+    ret = parse_pvfstab(NULL, &mnt);
+    if (ret < 0)
+    {
+	printf("Parsing error\n");
+	return -1;
+    }
 
-	ret = PVFS_sys_initialize(mnt, &resp_init);
-	if(ret < 0)
-	{
-		printf("PVFS_sys_initialize() failure. = %d\n", ret);
-		return(ret);
-	}
+    ret = PVFS_sys_initialize(mnt, &resp_init);
+    if (ret < 0)
+    {
+	printf("PVFS_sys_initialize() failure. = %d\n", ret);
+	return (ret);
+    }
 
-	return resp_init.fsid_list[0];
+    return resp_init.fsid_list[0];
 }
+
 /*
  * helper function to get the root handle
  * fs_id:   fsid of our file system
@@ -52,26 +54,26 @@ PVFS_fs_id system_init(void)
 
 PVFS_handle get_root(PVFS_fs_id fs_id)
 {
-	PVFS_sysreq_lookup req_look;
-	PVFS_sysresp_lookup resp_look;
-	int ret = -1;
+    PVFS_sysreq_lookup req_look;
+    PVFS_sysresp_lookup resp_look;
+    int ret = -1;
 
-	memset(&req_look, 0, sizeof(req_look));
-	memset(&req_look, 0, sizeof(resp_look));
-			
-	req_look.credentials.perms = 1877;
-	req_look.name = malloc(2);/*null terminator included*/
-	req_look.name[0] = '/';
-	req_look.name[1] = '\0';
-	req_look.fs_id = fs_id;
-	printf("looking up the root handle for fsid = %d\n", req_look.fs_id);
-	ret = PVFS_sys_lookup(&req_look,&resp_look);
-	if (ret < 0)
-	{
-		printf("Lookup failed with errcode = %d\n", ret);
-		return(-1);
-	}
-	return (PVFS_handle)resp_look.pinode_refn.handle;
+    memset(&req_look, 0, sizeof(req_look));
+    memset(&req_look, 0, sizeof(resp_look));
+
+    req_look.credentials.perms = 1877;
+    req_look.name = malloc(2);	/*null terminator included */
+    req_look.name[0] = '/';
+    req_look.name[1] = '\0';
+    req_look.fs_id = fs_id;
+    printf("looking up the root handle for fsid = %d\n", req_look.fs_id);
+    ret = PVFS_sys_lookup(&req_look, &resp_look);
+    if (ret < 0)
+    {
+	printf("Lookup failed with errcode = %d\n", ret);
+	return (-1);
+    }
+    return (PVFS_handle) resp_look.pinode_refn.handle;
 }
 
 /*
@@ -84,36 +86,38 @@ PVFS_handle get_root(PVFS_fs_id fs_id)
  * returns a handle to the new directory
  *          -1 if some error happened
  */
-PVFS_handle create_dir(PVFS_handle parent, PVFS_fs_id fs_id, char *name)
+PVFS_handle create_dir(PVFS_handle parent,
+		       PVFS_fs_id fs_id,
+		       char *name)
 {
-	PVFS_sysreq_mkdir req_mkdir;
-	PVFS_sysresp_mkdir resp_mkdir;
+    PVFS_sysreq_mkdir req_mkdir;
+    PVFS_sysresp_mkdir resp_mkdir;
 
-	int ret=-1;
+    int ret = -1;
 
-	memset(&req_mkdir, 0, sizeof(req_mkdir));
-	memset(&resp_mkdir, 0, sizeof(req_mkdir));
+    memset(&req_mkdir, 0, sizeof(req_mkdir));
+    memset(&resp_mkdir, 0, sizeof(req_mkdir));
 
 
-	req_mkdir.entry_name = name; 
-	req_mkdir.parent_refn.handle = parent;
-	req_mkdir.parent_refn.fs_id = fs_id;
-	req_mkdir.attrmask = ATTR_BASIC;
-	req_mkdir.attr.owner = 100;
-	req_mkdir.attr.group = 100;
-	req_mkdir.attr.perms = 1877;
-	req_mkdir.attr.objtype = ATTR_DIR;
-	req_mkdir.credentials.perms = 1877;
-	req_mkdir.credentials.uid = 100;
-	req_mkdir.credentials.gid = 100;
+    req_mkdir.entry_name = name;
+    req_mkdir.parent_refn.handle = parent;
+    req_mkdir.parent_refn.fs_id = fs_id;
+    req_mkdir.attrmask = ATTR_BASIC;
+    req_mkdir.attr.owner = 100;
+    req_mkdir.attr.group = 100;
+    req_mkdir.attr.perms = 1877;
+    req_mkdir.attr.objtype = ATTR_DIR;
+    req_mkdir.credentials.perms = 1877;
+    req_mkdir.credentials.uid = 100;
+    req_mkdir.credentials.gid = 100;
 
-	ret = PVFS_sys_mkdir(&req_mkdir,&resp_mkdir);
-	if (ret < 0)
-	{
-		printf("mkdir failed\n");
-		return(-1);
-	}
-	return (PVFS_handle)resp_mkdir.pinode_refn.handle;
+    ret = PVFS_sys_mkdir(&req_mkdir, &resp_mkdir);
+    if (ret < 0)
+    {
+	printf("mkdir failed\n");
+	return (-1);
+    }
+    return (PVFS_handle) resp_mkdir.pinode_refn.handle;
 }
 
 /*
@@ -123,28 +127,34 @@ PVFS_handle create_dir(PVFS_handle parent, PVFS_fs_id fs_id, char *name)
  * rank:    rank in the mpi process group 
  */
 
-int recursive_create_dir(PVFS_handle handle, PVFS_fs_id fs_id, 
-		int depth, int ndirs, int rank)
+int recursive_create_dir(PVFS_handle handle,
+			 PVFS_fs_id fs_id,
+			 int depth,
+			 int ndirs,
+			 int rank)
 {
-	int i;
-	char name[PVFS_NAME_MAX];
-	PVFS_handle dir_handle;
+    int i;
+    char name[PVFS_NAME_MAX];
+    PVFS_handle dir_handle;
 
-	/* base case: we've gone far enough */
-	if (depth == 0) return 0;
+    /* base case: we've gone far enough */
+    if (depth == 0)
+	return 0;
 
-	for(i=0; i<ndirs; i++) {
-		snprintf(name, PVFS_NAME_MAX, "depth=%d-rank=%d-iter=%d", 
-				depth, rank, i);
-		dir_handle = create_dir(handle, fs_id, name);
-		if (dir_handle < 0) {
-			return -1;
-		} else {
-			recursive_create_dir(dir_handle, fs_id, 
-					depth-1, ndirs, rank);
-		}
+    for (i = 0; i < ndirs; i++)
+    {
+	snprintf(name, PVFS_NAME_MAX, "depth=%d-rank=%d-iter=%d", depth, rank, i);
+	dir_handle = create_dir(handle, fs_id, name);
+	if (dir_handle < 0)
+	{
+	    return -1;
 	}
-	return 0; 
+	else
+	{
+	    recursive_create_dir(dir_handle, fs_id, depth - 1, ndirs, rank);
+	}
+    }
+    return 0;
 }
 
 /*
@@ -158,33 +168,44 @@ int recursive_create_dir(PVFS_handle handle, PVFS_fs_id fs_id,
  * 	0:  	all went well
  * 	nonzero: errors encountered making one or more directories
  */
-int test_dir_torture(MPI_Comm *comm, int rank,  char *buf, void *rawparams) 
+int test_dir_torture(MPI_Comm * comm,
+		     int rank,
+		     char *buf,
+		     void *rawparams)
 {
-	int ret = -1;
-	PVFS_fs_id fs_id;
-	PVFS_handle  root_handle;
-	generic_params *myparams = (generic_params *)rawparams;
-	int nerrs=0;
+    int ret = -1;
+    PVFS_fs_id fs_id;
+    PVFS_handle root_handle;
+    generic_params *myparams = (generic_params *) rawparams;
+    int nerrs = 0;
 
-	fs_id = system_init();
-	if (fs_id < 0)
-	{
-		printf("System initialization error\n");
-		return(fs_id);
-	}
+    fs_id = system_init();
+    if (fs_id < 0)
+    {
+	printf("System initialization error\n");
+	return (fs_id);
+    }
 
-	root_handle = get_root(fs_id); 
+    root_handle = get_root(fs_id);
 
-	/* this will make n^n directories, so be careful about running the test with mode=100 */
-	nerrs = recursive_create_dir(root_handle, fs_id, myparams->mode, 
-			myparams->mode, rank);
+    /* this will make n^n directories, so be careful about running the test with mode=100 */
+    nerrs = recursive_create_dir(root_handle, fs_id, myparams->mode, myparams->mode, rank);
 
-	ret = PVFS_sys_finalize();
-	if (ret < 0)
-	{
-		printf("finalizing sysint failed with errcode = %d\n", ret);
-		return (-1);
-	}
+    ret = PVFS_sys_finalize();
+    if (ret < 0)
+    {
+	printf("finalizing sysint failed with errcode = %d\n", ret);
+	return (-1);
+    }
 
-	return -nerrs;
+    return -nerrs;
 }
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
