cb4d099fb77919f5ba9ee577fb9b831b0d543906
Rob Ross <rross@mcs.anl.gov>
2003-03-21 17:53:08 +0000

    Removed unused files.
    SVN revision 685

--- src/server/job-consist.h
+++
@@ -1,7 +0,0 @@
-int job_check_consistency(
-		PVFS_server_op op,
-		PVFS_coll_id coll_id,
-		PVFS_handle handle,
-		void *user_ptr,
-		job_status_s* out_status_p,
-		job_id_t* id);
--- src/server/remove.c
+++ src/server/remove.c
@@ -129,7 +129,7 @@ static int remove_init(PINT_server_op *s_op,
 
     int job_post_ret;
 
-    gossip_debug(SERVER_DEBUG, "remove state: init\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: init\n", s_op);
 
     job_post_ret = job_req_sched_post(s_op->req,
 				      s_op,
@@ -151,7 +151,7 @@ static int remove_read_object_metadata(PINT_server_op *s_op,
     int job_post_ret;
     job_id_t j_id;
 
-    gossip_debug(SERVER_DEBUG, "remove state: read_object_metadata\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: read_object_metadata\n", s_op);
 
     /* set up key and value structures for reading metadata */
     s_op->key.buffer    = Trove_Common_Keys[METADATA_KEY].key;
@@ -196,7 +196,7 @@ static int remove_fallback_to_dspace_getattr(PINT_server_op *s_op,
     int job_post_ret;
     job_id_t j_id;
 
-    gossip_debug(SERVER_DEBUG, "remove state: fallback_to_dspace_getattr\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: fallback_to_dspace_getattr\n", s_op);
 
     gossip_debug(SERVER_DEBUG,
 		 "  reading dspace attributes (coll_id = 0x%x, handle = 0x%08Lx)\n",
@@ -222,7 +222,7 @@ static int remove_verify_dspace_attributes(PINT_server_op *s_op,
 					   job_status_s *ret)
 {
 
-    gossip_debug(SERVER_DEBUG, "remove state: verify_dspace_attributes\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: verify_dspace_attributes\n", s_op);
 
     if (ret->error_code != 0) {
 	gossip_debug(SERVER_DEBUG,
@@ -258,7 +258,7 @@ static int remove_verify_object_metadata(PINT_server_op *s_op,
 					 job_status_s *ret)
 {
 
-    gossip_debug(SERVER_DEBUG, "remove state: verify_object_metadata\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: verify_object_metadata\n", s_op);
 
     if (ret->error_code != 0) {
 	gossip_debug(SERVER_DEBUG,
@@ -293,7 +293,7 @@ static int remove_remove_dspace(PINT_server_op *s_op,
     int job_post_ret;
     job_id_t j_id;
 
-    gossip_debug(SERVER_DEBUG, "remove state: remove_dspace\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: remove_dspace\n", s_op);
 
     job_post_ret = job_trove_dspace_remove(s_op->req->u.remove.fs_id,
 					   s_op->req->u.remove.handle,
@@ -323,7 +323,7 @@ static int remove_send_response(PINT_server_op *s_op,
     int job_post_ret;
     job_id_t j_id;
 
-    gossip_debug(SERVER_DEBUG, "remove state: send_response\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: send_response\n", s_op);
 
     /* fill in response -- status field is the only generic one we should have to set */
     s_op->resp->status = ret->error_code;
@@ -373,7 +373,7 @@ static int remove_release_posted_job(PINT_server_op *s_op,
     int job_post_ret;
     job_id_t j_id;
 
-    gossip_debug(SERVER_DEBUG, "remove state: release_posted_job\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: release_posted_job\n", s_op);
 
     job_post_ret = job_req_sched_release(s_op->scheduled_id,
 					 s_op,
@@ -393,7 +393,7 @@ static int remove_cleanup(PINT_server_op *s_op,
 			  job_status_s *ret)
 {
 
-    gossip_debug(SERVER_DEBUG, "remove state: cleanup\n");
+    gossip_debug(SERVER_DEBUG, "(%p) remove state: cleanup\n", s_op);
 
     /* free decoded, encoded requests */
     PINT_decode_release(&(s_op->decoded),PINT_DECODE_REQ,0);
