721e81cb5636c4460da55ac3c3803c53d6eb2da3
Phil Carns <carns@mcs.anl.gov>
2003-03-22 21:30:56 +0000

    Added another string parsing function, called "PINT_remove_dir_prefix()", for converting client side absolute pathnames like /mnt/pvfs2/foo/bar into pvfs2 file system absolute path names like /foo/bar.  UNTESTED
    SVN revision 719

--- src/common/misc/str_utils.c
+++ src/common/misc/str_utils.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 /* PINT_string_count_segments()
  *
@@ -220,4 +221,115 @@ int PINT_string_next_segment(char *pathname,
     }
     return 0;
 }
+
+/* PINT_remove_dir_prefix()
+ *
+ * Strips prefix directory out of the path, output includes beginning
+ * slash
+ *
+ * Parameters:
+ * pathname     - pointer to directory string (absolute)
+ * prefix       - pointer to prefix dir string (absolute)
+ * out_path     - pointer to output dir string
+ * max_out_len  - max length of out_base_dir buffer
+ *
+ * All incoming arguments must be valid and non-zero
+ *
+ * Returns 0 on success; -errno on failure
+ *
+ * Example inputs and outputs/return values:
+ *
+ * pathname: /mnt/pvfs2/foo, prefix: /mnt/pvfs2
+ *     out_path: /foo, returns 0
+ * pathname: /mnt/pvfs2/foo, prefix: /mnt/pvfs2/
+ *     out_path: /foo, returns 0
+ * pathname: /mnt/pvfs2/foo/bar, prefix: /mnt/pvfs2
+ *     out_path: /foo/bar, returns 0
+ * pathname: /mnt/pvfs2/foo/bar, prefix: /
+ *     out_path: /mnt/pvfs2/foo/bar, returns 0
+ *
+ * invalid pathname input examples:
+ * pathname: /mnt/foo/bar, prefix: /mnt/pvfs2
+ *     out_path: undefined, returns -ENOENT
+ * pathname: /mnt/pvfs2fake/foo/bar, prefix: /mnt/pvfs2
+ *     out_path: undefined, returns -ENOENT
+ * pathname: /mnt/foo/bar, prefix: mnt/pvfs2
+ *     out_path: undefined, returns -EINVAL
+ * pathname: mnt/foo/bar, prefix: /mnt/pvfs2
+ *     out_path: undefined, returns -EINVAL
+ * out_max_len not large enough for buffer, returns -ENAMETOOLONG
+ */
+int PINT_remove_dir_prefix(char *pathname, char* prefix, char *out_path, 
+    int out_max_len)
+{
+    int ret = -EINVAL;
+    int prefix_len, pathname_len;
+    int cut_index;
+
+    if(!pathname || !prefix || !out_path || !out_max_len)
+    {
+	return(-EINVAL);
+    }
+
+    /* make sure we are given absolute paths */
+    if ((pathname[0] != '/') || (prefix[0] != '/'))
+    {
+	return ret;
+    }
+
+    prefix_len = strlen(prefix);
+    pathname_len = strlen(pathname);
+
+    /* account for trailing slashes on prefix */
+    while(prefix[prefix_len-1] == '/')
+    {
+	prefix_len--;
+    }
+
+    /* if prefix_len is now zero, then prefix must have been root
+     * directory; return copy of entire pathname
+     */
+    if(prefix_len == 0)
+    {
+	cut_index = 0;
+    }
+    else
+    {
+	
+	/* make sure prefix would fit in pathname */
+	if(prefix_len > (pathname_len + 1))
+	    return(-ENOENT);
+
+	/* see if we can find prefix at beginning of path */
+	if(strncmp(prefix, pathname, prefix_len) == 0)
+	{
+	    /* apparent match; see if next element is a slash */
+	    if(pathname[prefix_len] != '/')
+		return(-ENOENT);
+	    
+	    /* this was indeed a match */
+	    cut_index = prefix_len;
+	}
+    }
+
+    /* if we hit this point, then we were successful */
+
+    /* is the buffer large enough? */
+    if((1+strlen(&(pathname[cut_index]))) > out_max_len)
+	return(-ENAMETOOLONG);
+
+    /* copy out appropriate part of pathname */
+    strcpy(out_path, &(pathname[cut_index]));
+    return(0);
+}
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
+
 
