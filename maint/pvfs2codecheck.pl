ccc753a8aa3af485c929006e796454f808a0262b
Phil Carns <carns@mcs.anl.gov>
2003-04-02 19:25:13 +0000

    added a --fix option to pvfs2codecheck.pl that tacks on copyright information or editor formatting options as needed to files that seem to lack them
    SVN revision 780

--- maint/pvfs2codecheck.pl
+++ maint/pvfs2codecheck.pl
@@ -1,9 +1,20 @@
 #! /usr/bin/perl -w
 
+use Getopt::Long;
+
+$fix_flag = 0;
+
+GetOptions("fix" => \$fix_flag) or die "GetOptions: $!";
+
 @files = ();
 
 foreach $arg (@ARGV) {
     $files[$#files+1] = $arg;
+}
+
+if($fix_flag && $#files) {
+    print "Error: please don't specify multiple targets when using --fix.\n";
+    exit;
 }
 
 foreach $file (@files) {
@@ -31,17 +42,41 @@ foreach $file (@files) {
 
     close(FD);
 
-    if ($has_copyright < 1) { print "$file: missing or incorrect copyright block.\n"; }
-    if ($has_copyright > 1) { print "$file: too many copyright lines!\n"; }
+    if(!$fix_flag){
+	if ($has_copyright < 1) { print "$file: missing or incorrect copyright block.\n"; }
+	if ($has_copyright > 1) { print "$file: too many copyright lines!\n"; }
 
-    if ($has_emacs_c_basic_offset < 1) { print "$file: missing emacs c-basic-offset: 4 line.\n"; }
-    if ($has_emacs_c_basic_offset > 1) { print "$file: too many emacs c-basic-offset lines!\n"; }
+	if ($has_emacs_c_basic_offset < 1) { print "$file: missing emacs c-basic-offset: 4 line.\n"; }
+	if ($has_emacs_c_basic_offset > 1) { print "$file: too many emacs c-basic-offset lines!\n"; }
 
-    if ($has_emacs_c_indent_level < 1) { print "$file: missing emacs c-indent-level: 4 line.\n"; }
-    if ($has_emacs_c_indent_level > 1) { print "$file: too many emacs c-indent-level lines!\n"; }
+	if ($has_emacs_c_indent_level < 1) { print "$file: missing emacs c-indent-level: 4 line.\n"; }
+	if ($has_emacs_c_indent_level > 1) { print "$file: too many emacs c-indent-level lines!\n"; }
 
-    if ($has_vim_formatting < 1) { print "$file: missing or incorrect vim formatting line.\n"; }
-    if ($has_vim_formatting > 1) { print "$file: too many vim formatting lines!\n"; }
+	if ($has_vim_formatting < 1) { print "$file: missing or incorrect vim formatting line.\n"; }
+	if ($has_vim_formatting > 1) { print "$file: too many vim formatting lines!\n"; }
+    }
+    else {
+    	if(!$has_copyright) {
+	    open(TP_FD, "<TEMPLATE_COPYRIGHT_HEADER.txt") || die "error opening $file";
+	    while(<TP_FD>){
+	    	print $_;
+	    }
+	    close(TP_FD);
+	}
 
+    	open(FD, "<$file") || die "error opening $file";
+	while(<FD>){
+	    print $_;
+	}
+	close(FD);
+
+    	if($has_emacs_c_basic_offset < 1 && $has_emacs_c_indent_level < 1 && $has_vim_formatting < 1) {
+	    open(TP_FD, "<TEMPLATE_FORMAT_TRAILER.txt") || die "error opening $file";
+	    while(<TP_FD>){
+	    	print $_;
+	    }
+	    close(TP_FD);
+	}
+    }
 }
 
