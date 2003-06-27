1edbeb4ea9e7caceca89f037d87be6b7159f8923
Phil Carns <carns@mcs.anl.gov>
2003-06-27 05:57:58 +0000

    moved all id generator code into header file and reformatted.  I originally meant to turn the functions into macros, but backed off and just made them static inlines for now instead
    SVN revision 1197

--- src/common/id-generator/id-generator.h
+++ src/common/id-generator/id-generator.h
@@ -1,44 +1,86 @@
 /*
- * copyright (c) 2000 Clemson University, all rights reserved.
+ * (C) 2001 Clemson University and The University of Chicago
  *
- * Written by Phil Carns.
- *
- * This program is free software; you can redistribute it and/or
- * modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * Contacts:  Phil Carns  pcarns@parl.clemson.edu
+ * See COPYING in top-level directory.
  */
 
-/*
- * April 2001
- *
- * A small library for generating and looking up fully opaque id's for
+
+/* A small library for generating and looking up fully opaque id's for
  * an arbitrary piece of data.
- */ 
+ */
+
+/* This will hopefully eventually be a library of mechanisms for doing
+ * fast registration and lookups of data structures.  Right now it only
+ * has routines that directly convert pointers into integer types and vice 
+ * versa.
+ */
 
 #ifndef __ID_GENERATOR_H
-#define __ID_GENERATOR_H 
+#define __ID_GENERATOR_H
 
 #include "pvfs2-types.h"
+#include "pvfs2-config.h"
+#include "errno.h"
 
-/********************************************************************
- * Visible interface
+/* id_gen_fast_register()
+ * 
+ * registers a piece of data (a pointer of some sort) and
+ * returns an opaque id for it.  
+ *
+ * returns 0 on success, -errno on failure
  */
+static inline int id_gen_fast_register(
+    PVFS_id_gen_t * new_id,
+    void *item)
+{
+    if (!item)
+	return (-EINVAL);
 
-int id_gen_fast_register(PVFS_id_gen_t* new_id, void* item);
-void* id_gen_fast_lookup(PVFS_id_gen_t id);
+#if SIZEOF_VOID_P == 4
+    *new_id = 0;
+    *new_id += (int32_t) item;
+#elif SIZEOF_VOID_P == 8
+    *new_id = (int64_t) item;
+#else
+    #error "Unsupported Architecture..."
+#endif
+
+    return (0);
+}
+
+/* id_gen_fast_lookup()
+ * 
+ * Returns the piece of data registered with an id.  It does no error
+ * checking!  It does not make sure that that the id was actually
+ * registered before proceeding.
+ *
+ * returns pointer to data on success, NULL on failure
+ */
+static inline void *id_gen_fast_lookup(
+    PVFS_id_gen_t id)
+{
+    int32_t little_int = 0;
+
+    if (!id)
+	return (NULL);
+
+#if SIZEOF_VOID_P == 4
+    little_int += id;
+    return ((void *) little_int);
+#elif SIZEOF_VOID_P == 8
+    return ((void *) id);
+#else
+    #error "Unsupported Architecture..."
+#endif
+}
 
 #endif /* __ID_GENERATOR_H */
 
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
