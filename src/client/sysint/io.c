87ed1798c063027b296d3099759a896d7e0ed62b
Rob Latham <robl@mcs.anl.gov>
2003-06-28 01:40:08 +0000

    wow, is that one hell of an embarasing logic error
    SVN revision 1199

--- src/client/sysint/io.c
+++ src/client/sysint/io.c
@@ -188,7 +188,7 @@ int PVFS_sys_io(PVFS_pinode_reference pinode_refn, PVFS_Request io_req,
     PINT_Free_request_state(req_state);
     req_state = NULL;
 
-    if (target_handle_count > 0) {
+    if (target_handle_count == 0) {
 	/* not necessarily an error: we might have gotten a 
 	 * zero-byte request.  that's fine.. less work for us! */
 	resp->total_completed = 0;
