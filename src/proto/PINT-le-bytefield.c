d8a6ae75b3dfe61046882abfc69429f8c9d42abc
Phil Carns <carns@mcs.anl.gov>
2003-07-07 01:47:42 +0000

    filled in more of the API; should be enough for a full getconfig exchange (req and resp), but untested.  Not convinced this is a good approach- will think about it a little more before continuing...
    SVN revision 1259

--- src/proto/PINT-le-bytefield.c
+++ src/proto/PINT-le-bytefield.c
@@ -10,6 +10,7 @@
 #include <assert.h>
 
 #include "bmi.h"
+#include "bmi-byteswap.h"
 #include "pvfs2-req-proto.h"
 #include "gossip.h"
 #include "PINT-reqproto-encode.h"
@@ -19,17 +20,50 @@
 #define PINT_DEC 2
 #define PINT_CALC_SIZE 3
 
+/**************************************************************
+ * macros for transforming basic types
+ */
+
 /* unsigned 32 bit int */
-#define PINT_XENC_UINT32(msg_p,x_p)			    \
-do{							    \
-    if(PINT_XENC_MODE == PINT_ENC){			    \
-	*((uint32_t*)((msg_p)->ptr_current)) = *(x_p);	    \
-    }							    \
-    else if(PINT_XENC_MODE == PINT_DEC){		    \
-	*(x_p) = *((uint32_t*)((msg_p)->ptr_current));	    \
-    }							    \
-    (msg_p)->ptr_current += sizeof(uint32_t);		    \
+#define PINT_XENC_UINT32(msg_p,x_p)				    \
+do{								    \
+    if(PINT_XENC_MODE == PINT_ENC){				    \
+	*((uint32_t*)((msg_p)->ptr_current)) = htobmi32(*(x_p));    \
+    }								    \
+    else if(PINT_XENC_MODE == PINT_DEC){			    \
+	*(x_p) = bmitoh32(*((uint32_t*)((msg_p)->ptr_current)));    \
+    }								    \
+    (msg_p)->ptr_current += sizeof(uint32_t);			    \
 }while(0)
+
+/* signed 32 bit int */
+#define PINT_XENC_INT32(msg_p,x_p)				    \
+do{								    \
+    if(PINT_XENC_MODE == PINT_ENC){				    \
+	*((int32_t*)((msg_p)->ptr_current)) = htobmi32(*(x_p));     \
+    }								    \
+    else if(PINT_XENC_MODE == PINT_DEC){			    \
+	*(x_p) = bmitoh32(*((int32_t*)((msg_p)->ptr_current)));     \
+    }								    \
+    (msg_p)->ptr_current += sizeof(int32_t);			    \
+}while(0)
+
+
+/* strings */
+#define PINT_XENC_STRING(msg_p,x_p,size)			    \
+do{								    \
+    if(PINT_XENC_MODE == PINT_ENC){				    \
+	memcpy((msg_p)->ptr_current, x_p, size);		    \
+    }								    \
+    else if(PINT_XENC_MODE == PINT_DEC){			    \
+	memcpy(x_p, (msg_p)->ptr_current, size);		    \
+    }								    \
+    (msg_p)->ptr_current += size;				    \
+}while(0)
+
+/************************************************************
+ * macros for transforming specific structures
+ */
 
 /* operates on the generic part of a request structure */
 #define PINT_XENC_REQ_GEN(msg_p,req)			    \
@@ -40,9 +74,29 @@ do{							    \
     PINT_XENC_UINT32(msg_p,&((req)->credentials.perms));    \
 }while(0)
 
+/* operates on the generic part of a response structure */
+#define PINT_XENC_RESP_GEN(msg_p,resp)			    \
+do{							    \
+    PINT_XENC_UINT32(msg_p,&((resp)->op));		    \
+    PINT_XENC_INT32(msg_p,&((resp)->status));		    \
+}while(0)
+
 /* operates on a getconfig request */
-#define PINT_XENC_REQ_GETCONFIG(msg_p,req)		    \
-do{}while(0)
+#define PINT_XENC_REQ_GETCONFIG(msg_p,req) do{}while(0)
+
+/* operates on a getconfig response */
+#define PINT_XENC_RESP_GETCONFIG_A(msg_p,resp)		    \
+do{							    \
+    PINT_XENC_UINT32(msg_p,&((resp)->u.getconfig.fs_config_buf_size));	\
+    PINT_XENC_UINT32(msg_p,&((resp)->u.getconfig.server_config_buf_size)); \
+}while(0)
+#define PINT_XENC_RESP_GETCONFIG_B(msg_p,resp)		    \
+do{							    \
+    PINT_XENC_STRING(msg_p, (resp)->u.getconfig.fs_config_buf, \
+	(resp)->u.getconfig.fs_config_buf_size);		    \
+    PINT_XENC_STRING(msg_p, (resp)->u.getconfig.server_config_buf, \
+	(resp)->u.getconfig.server_config_buf_size);	    \
+}while(0)
 
 static int lebf_encode_req(
     struct PVFS_server_req *request,
@@ -107,7 +161,8 @@ static struct max_size max_size_array[PVFS_MAX_SERVER_OP+1];
  */
 static void lebf_initialize(void)
 {
-    struct PVFS_server_req* tmp_req = NULL;
+    struct PVFS_server_req tmp_req;
+    struct PVFS_server_resp tmp_resp;
     struct PINT_encoded_msg tmp_msg;
     int PINT_XENC_MODE = PINT_CALC_SIZE;
 
@@ -118,9 +173,17 @@ static void lebf_initialize(void)
 
     /* getconfig request */
     tmp_msg.ptr_current = 0;
-    PINT_XENC_REQ_GEN(&tmp_msg, tmp_req);
-    PINT_XENC_REQ_GETCONFIG(&tmp_msg, tmp_req);
+    PINT_XENC_REQ_GEN(&tmp_msg, &tmp_req);
+    PINT_XENC_REQ_GETCONFIG(&tmp_msg, &tmp_req);
     max_size_array[PVFS_SERV_GETCONFIG].max_req = (int)(tmp_msg.ptr_current);
+    /* getconfig response */
+    tmp_msg.ptr_current = 0;
+    tmp_resp.u.getconfig.fs_config_buf_size = PVFS_REQ_LIMIT_CONFIG_FILE_BYTES;
+    tmp_resp.u.getconfig.server_config_buf_size = PVFS_REQ_LIMIT_CONFIG_FILE_BYTES;
+    PINT_XENC_RESP_GEN(&tmp_msg, &tmp_resp);
+    PINT_XENC_RESP_GETCONFIG_A(&tmp_msg, &tmp_resp);
+    PINT_XENC_RESP_GETCONFIG_B(&tmp_msg, &tmp_resp);
+    max_size_array[PVFS_SERV_GETCONFIG].max_resp = (int)(tmp_msg.ptr_current);
 
     return;
 }
@@ -135,8 +198,12 @@ static int lebf_encode_calc_max_size(
     enum PINT_encode_msg_type input_type,
     enum PVFS_server_op op_type)
 {
-    gossip_lerr("Error: unimplemented.\n");
-    return(-ENOSYS);
+    if(input_type == PINT_ENCODE_REQ)
+	return(max_size_array[op_type].max_req);
+    else if(input_type == PINT_ENCODE_RESP)
+	return(max_size_array[op_type].max_resp);
+
+    return(-EINVAL);
 }
 
 /* lebf_encode_req()
@@ -178,9 +245,6 @@ static int lebf_encode_req(
 	    PINT_XENC_REQ_GEN(target_msg, request);
 	    PINT_XENC_REQ_GETCONFIG(target_msg, request);
 
-	    /* check sanity */
-	    assert(target_msg->total_size == (int)(target_msg->ptr_current -
-		(char*)(target_msg->buffer_list[0])));
 	    ret = 0;
 	    break;
 	default:
@@ -190,25 +254,121 @@ static int lebf_encode_req(
     }
 
 
+    /* check sanity */
+    if(ret == 0)
+    {
+	assert(target_msg->total_size == (int)(target_msg->ptr_current -
+	    (char*)(target_msg->buffer_list[0])));
+    }
     return(ret);
 }
 
+
+/* lebf_encode_resp()
+ *
+ * encodes a response structure
+ *
+ * returns 0 on success, -errno on failure
+ */
 static int lebf_encode_resp(
     struct PVFS_server_resp *response,
     struct PINT_encoded_msg *target_msg)
 {
-    gossip_lerr("Error: unimplemented.\n");
-    return(-ENOSYS);
+    int ret = -1;
+    int PINT_XENC_MODE = PINT_ENC;
+
+    /* this encoder always uses just one buffer */
+    target_msg->buffer_list = &target_msg->buffer_stub;
+    target_msg->size_list = &target_msg->size_stub;
+    target_msg->list_count = 1;
+    target_msg->buffer_type = BMI_PRE_ALLOC;
+
+    /* set size of buffer needed, although some requests may override this */
+    target_msg->size_list[0] = target_msg->total_size = 
+	max_size_array[response->op].max_resp + PINT_ENC_GENERIC_HEADER_SIZE;
+
+    switch(response->op)
+    {
+	case PVFS_SERV_GETCONFIG:
+	    /* adjust size estimage based on what we are sending */
+	    target_msg->ptr_current = NULL;
+	    PINT_XENC_RESP_GEN(target_msg, response);
+	    PINT_XENC_RESP_GETCONFIG_A(target_msg, response);
+	    PINT_XENC_RESP_GETCONFIG_B(target_msg, response);
+	    target_msg->size_list[0] = target_msg->total_size = 
+		(int)(target_msg->ptr_current) + PINT_ENC_GENERIC_HEADER_SIZE;
+
+	    /* create a buffer */
+	    target_msg->buffer_list[0] = target_msg->ptr_current =  
+		BMI_memalloc(target_msg->dest, target_msg->total_size, BMI_SEND);
+	    if(!target_msg->buffer_list[0])
+		ret = -ENOMEM;
+
+	    /* encode */
+	    memcpy(target_msg->ptr_current, le_bytefield_table.generic_header,
+		PINT_ENC_GENERIC_HEADER_SIZE);
+	    target_msg->ptr_current += PINT_ENC_GENERIC_HEADER_SIZE;
+	    PINT_XENC_RESP_GEN(target_msg, response);
+	    PINT_XENC_RESP_GETCONFIG_A(target_msg, response);
+	    PINT_XENC_RESP_GETCONFIG_B(target_msg, response);
+	    ret = 0;
+	default:
+	    gossip_lerr("Error: unsupported operation.\n");
+	    ret = -ENOSYS;
+	    break;
+    }
+
+    return(ret);
 }
 
+
+/* lebf_decode_resp()
+ *
+ * decodes a response structure
+ *
+ * returns 0 on success, -errno on failure
+ */
 static int lebf_decode_resp(
     void *input_buffer,
     int input_size,
     struct PINT_decoded_msg *target_msg,
     bmi_addr_t target_addr)
 {
-    gossip_lerr("Error: unimplemented.\n");
-    return(-ENOSYS);
+    int ret = -1;
+    int PINT_XENC_MODE = PINT_DEC;
+
+    target_msg->buffer = &(target_msg->stub_dec.resp);
+    target_msg->ptr_current = input_buffer;
+
+    /* decode generic part of response (enough to get op number) */
+    PINT_XENC_RESP_GEN(target_msg, &(target_msg->stub_dec.resp));
+
+    switch(target_msg->stub_dec.resp.op)
+    {
+	case PVFS_SERV_GETCONFIG:
+	    PINT_XENC_RESP_GETCONFIG_A(target_msg, &(target_msg->stub_dec.resp));
+	    target_msg->stub_dec.resp.u.getconfig.fs_config_buf =
+		(char*)malloc(target_msg->stub_dec.resp.u.getconfig.fs_config_buf_size);
+	    if(!target_msg->stub_dec.resp.u.getconfig.fs_config_buf)
+		return(-ENOMEM);
+	    target_msg->stub_dec.resp.u.getconfig.server_config_buf =
+		(char*)malloc(target_msg->stub_dec.resp.u.getconfig.server_config_buf_size);
+	    if(!target_msg->stub_dec.resp.u.getconfig.server_config_buf)
+	    {
+		free(target_msg->stub_dec.resp.u.getconfig.fs_config_buf);
+		return(-ENOMEM);
+	    }
+	    PINT_XENC_RESP_GETCONFIG_B(target_msg, &(target_msg->stub_dec.resp));
+	    ret = 0;
+	    break;
+	default:
+	    gossip_lerr("Error: unkown server operation: %d\n",
+		(int)target_msg->stub_dec.req.op);
+	    ret = -EPROTO;
+	    break;
+    }
+
+    return(ret);
 }
 
 
@@ -249,19 +409,59 @@ static int lebf_decode_req(
     return(ret);
 }
 
+
+/* lebf_decode_rel()
+ *
+ * releases resources consumed while decoding
+ *
+ * no return value
+ */
 static void lebf_decode_rel(
     struct PINT_decoded_msg *msg,
     enum PINT_encode_msg_type input_type)
 {
-    gossip_lerr("Error: unimplemented.\n");
+    if(input_type == PINT_ENCODE_REQ)
+    {
+	switch(msg->stub_dec.req.op)
+	{
+	    case PVFS_SERV_GETCONFIG:
+		break;
+	    default:
+		gossip_lerr("Error: unsupported operation.\n");
+		break;
+	}
+    }
+    else if(input_type == PINT_ENCODE_RESP)
+    {
+	switch(msg->stub_dec.resp.op)
+	{
+	    case PVFS_SERV_GETCONFIG:
+		free(msg->stub_dec.resp.u.getconfig.fs_config_buf);
+		free(msg->stub_dec.resp.u.getconfig.server_config_buf);
+		break;
+	    default:
+		gossip_lerr("Error: unsupported operation.\n");
+		break;
+	}
+    }
+
     return;
 }
 
+
+/* lebf_encode_rel()
+ *
+ * releases resources consumed while encoding
+ *
+ * no return value 
+ */
 static void lebf_encode_rel(
     struct PINT_encoded_msg *msg,
     enum PINT_encode_msg_type input_type)
 {
-    gossip_lerr("Error: unimplemented.\n");
+    /* just a single buffer to free */
+    BMI_memfree(msg->dest, msg->buffer_list[0], msg->total_size, BMI_SEND);
+
     return;
 }
 
