b188c868d86b6086fc713249d812905f118a7d10
Phil Carns <carns@mcs.anl.gov>
2003-07-07 15:32:25 +0000

    added a new test program that just tests initialize and finalize sysint functions
    SVN revision 1261

---
+++ test/client/sysint/initialize.c
@@ -0,0 +1,49 @@
+/*
+ * (C) 2001 Clemson University and The University of Chicago
+ *
+ * See COPYING in top-level directory.
+ */
+
+#include <client.h>
+#include <sys/time.h>
+
+
+extern int parse_pvfstab(char *fn,pvfs_mntlist *mnt);
+
+int main(int argc,char **argv)
+{
+	PVFS_sysresp_init resp_init;
+	int ret = -1;
+	pvfs_mntlist mnt = {0,NULL};
+
+	gossip_enable_stderr();
+	gossip_set_debug_mask(1,CLIENT_DEBUG);
+
+	/* Parse PVFStab */
+	ret = parse_pvfstab(NULL,&mnt);
+	if (ret < 0)
+	{
+		printf("Parsing error\n");
+		return(-1);
+	}
+
+	/*Init the system interface*/
+	ret = PVFS_sys_initialize(mnt, &resp_init);
+	if(ret < 0)
+	{
+		printf("PVFS_sys_initialize() failure. = %d\n", ret);
+		return(ret);
+	}
+
+	/*close it down*/
+	ret = PVFS_sys_finalize();
+	if (ret < 0)
+	{
+		printf("finalizing sysint failed with errcode = %d\n", ret);
+		return (-1);
+	}
+
+	gossip_disable();
+
+	return(0);
+}
