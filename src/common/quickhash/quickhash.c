329f5f6d50d2dae05e38e77b5e86b96a71522b3f
Neill Miller <neillm@mcs.anl.gov>
2003-07-08 18:13:42 +0000

    Modified quickhash to prepare sharing the implementation with userspace and kernel land.  In kernel land, spin locks are used.  Also added a new function that both finds an element and removes it atomically.
    SVN revision 1296

--- src/common/quickhash/quickhash.c
+++ src/common/quickhash/quickhash.c
@@ -3,11 +3,10 @@
  * See COPYING in top-level directory.
  */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<quickhash.h>
-
+#include "quickhash.h"
 
 /* qhash_init()
  *
@@ -18,7 +17,7 @@
  * returns pointer to table on success, NULL on failure
  */
 struct qhash_table* qhash_init(
-	int(*compare)(void* key, struct qlist_head* link), 
+	int(*compare)(void* key, struct qhash_head* link), 
 	int(*hash)(void* key, int table_size),
 	int table_size)
 {
@@ -26,7 +25,8 @@ struct qhash_table* qhash_init(
 	int i = 0;
 
 	/* create struct to contain table information */
-	new_table = (struct qhash_table*)malloc(sizeof(struct qhash_table));
+	new_table = (struct qhash_table*)
+            qhash_malloc(sizeof(struct qhash_table));
 	if(!new_table)
 	{
 		return(NULL);
@@ -38,19 +38,20 @@ struct qhash_table* qhash_init(
 	new_table->table_size = table_size;
 
 	/* create array for actual table */
-	new_table->array = (struct qlist_head*)malloc(sizeof(struct
-		qlist_head) * table_size);
+	new_table->array = (struct qhash_head*)
+            qhash_malloc(sizeof(struct qhash_head) * table_size);
 	if(!new_table->array)
 	{
-		free(new_table);
+		qhash_free(new_table);
 		return(NULL);
 	}
 	
 	/* initialize a doubly linked at each hash table index */
 	for(i=0; i<table_size; i++)
 	{
-		INIT_QLIST_HEAD(&new_table->array[i]);
+		INIT_QHASH_HEAD(&new_table->array[i]);
 	}
+	qhash_lock_init(&new_table->lock);
 
 	return(new_table);
 }
@@ -65,8 +66,8 @@ struct qhash_table* qhash_init(
 void qhash_finalize(
 	struct qhash_table* old_table)
 {
-	free(old_table->array);
-	free(old_table);
+	qhash_free(old_table->array);
+	qhash_free(old_table);
 	return;
 }
 
@@ -79,7 +80,7 @@ void qhash_finalize(
 void qhash_add(
 	struct qhash_table* table,
 	void* key,
-	struct qlist_head* link)
+	struct qhash_head* link)
 {
 	int index = 0;
 
@@ -87,7 +88,9 @@ void qhash_add(
 	index = table->hash(key,table->table_size);
 
 	/* add to the tail of the linked list at that offset */
-	qlist_add_tail(link, &(table->array[index]));
+	qhash_lock(&table->lock);
+	qhash_add_tail(link, &(table->array[index]));
+	qhash_unlock(&table->lock);
 
 	return;
 }
@@ -100,24 +103,58 @@ void qhash_add(
  * returns pointer to link on success, NULL on failure (or item
  * not found)
  */
-struct qlist_head* qhash_search(
+struct qhash_head* qhash_search(
 	struct qhash_table* table,
 	void* key)
 {
 	int index = 0;
-	struct qlist_head* tmp_link;
+	struct qhash_head* tmp_link;
 
 	/* find the hash value */
 	index = table->hash(key, table->table_size);
 
 	/* linear search at index to find match */
-	qlist_for_each(tmp_link, &(table->array[index]))
+	qhash_lock(&table->lock);
+	qhash_for_each(tmp_link, &(table->array[index]))
 	{
 		if(table->compare(key, tmp_link))
 		{
 			return(tmp_link);
 		}
 	}
+	qhash_unlock(&table->lock);
 	return(NULL);
 }
 
+/* qhash_search_and_remove()
+ *
+ * searches for a link in the hash table that matches the given
+ * key
+ *
+ * returns pointer to link on success, NULL on failure (or item
+ * not found).  On success, link is removed from hashtable.
+ */
+struct qhash_head* qhash_search_and_remove(
+	struct qhash_table* table,
+	void* key)
+{
+	int index = 0;
+	struct qhash_head* tmp_link;
+
+	/* find the hash value */
+	index = table->hash(key, table->table_size);
+
+	/* linear search at index to find match */
+	qhash_lock(&table->lock);
+	qhash_for_each(tmp_link, &(table->array[index]))
+	{
+		if(table->compare(key, tmp_link))
+		{
+			qhash_del(tmp_link);
+			qhash_unlock(&table->lock);
+			return(tmp_link);
+		}
+	}
+	qhash_unlock(&table->lock);
+	return(NULL);
+}
--- src/common/quickhash/quickhash.h
+++ src/common/quickhash/quickhash.h
@@ -6,31 +6,67 @@
 #ifndef QUICKHASH_H
 #define QUICKHASH_H
 
-#include<quicklist.h>
+#include "quicklist.h"
+
+#ifdef __KERNEL__
+
+#define qhash_malloc(x)            kmalloc(x, GFP_KERNEL)
+#define qhash_free(x)              kfree(x)
+#define qhash_head                 list_head
+#define INIT_QHASH_HEAD            INIT_LIST_HEAD
+#define qhash_entry                list_entry
+#define qhash_add_tail             list_add_tail
+#define qhash_del                  list_del
+#define qhash_for_each             list_for_each
+
+#define qhash_lock_init(lock_ptr)  spin_lock_init(lock_ptr)
+#define qhash_lock(lock_ptr)       spin_lock(lock_ptr)
+#define qhash_unlock(lock_ptr)     spin_unlock(lock_ptr)
+
+#else
+
+#define qhash_malloc(x)            malloc(x)
+#define qhash_free(x)              free(x)
+#define qhash_head                 qlist_head
+#define INIT_QHASH_HEAD            INIT_QLIST_HEAD
+#define qhash_entry                qlist_entry
+#define qhash_add_tail             qlist_add_tail
+#define qhash_del                  qlist_del
+#define qhash_for_each             qlist_for_each
+
+#define qhash_lock_init(lock_ptr)  do {} while(0)
+#define qhash_lock(lock_ptr)       do {} while(0)
+#define qhash_unlock(lock_ptr)     do {} while(0)
+
+#endif /* __KERNEL__ */
 
 struct qhash_table
 {
-	struct qlist_head* array;
+	struct qhash_head* array;
 	int table_size;
-	int(*compare)(void* key, struct qlist_head* link);
+	int(*compare)(void* key, struct qhash_head* link);
 	int(*hash)(void* key, int table_size);
+
+#ifdef __KERNEL__
+	spinlock_t lock;
+#endif
 };
 
 struct qhash_table* qhash_init(
-	int(*compare)(void* key, struct qlist_head* link), 
+	int(*compare)(void* key, struct qhash_head* link), 
 	int(*hash)(void* key, int table_size),
 	int table_size);
 
 void qhash_finalize(
 	struct qhash_table* old_table);
 
-struct qlist_head* qhash_search(
+struct qhash_head* qhash_search(
 	struct qhash_table* table,
 	void* key);
 
 void qhash_add(
 	struct qhash_table* table,
 	void* key,
-	struct qlist_head* link);
+	struct qhash_head* link);
 
 #endif /* QUICKHASH_H */
