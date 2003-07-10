8ee8caf302c205e0fc575da8cf2f919a218815df
Phil Carns <carns@mcs.anl.gov>
2003-07-10 23:49:58 +0000

    filled in some of the read operation.  not sure how to setup upcall/downcall, but this is enough to test a little bit of the bufmap functionality
    SVN revision 1377

--- src/kernel/linux-2.6/file.c
+++ src/kernel/linux-2.6/file.c
@@ -11,6 +11,7 @@
 #include <linux/slab.h>
 #include <asm/atomic.h>
 #include "pvfs2-kernel.h"
+#include "pvfs2-bufmap.h"
 
 extern kmem_cache_t *op_cache;
 extern struct list_head pvfs2_request_list;
@@ -40,11 +41,16 @@ static ssize_t pvfs2_file_read(
     size_t count,
     loff_t * offset)
 {
-    pvfs2_kernel_op_t *new_op = (pvfs2_kernel_op_t *) 0;
+    size_t each_count = 0;
+    size_t total_count = 0;
+    pvfs2_kernel_op_t *new_op = NULL;
+    struct pvfs_bufmap_desc* desc;
+    int ret = -1;
+    char* current_buf = buf;
 
     pvfs2_print("pvfs2: pvfs2_file_read called on %s\n",
 		file->f_dentry->d_name.name);
-
+    
     new_op = kmem_cache_alloc(op_cache, SLAB_KERNEL);
     if (!new_op)
     {
@@ -52,20 +58,49 @@ static ssize_t pvfs2_file_read(
 		    "kmem_cache_alloc failed!\n");
 	return -ENOMEM;
     }
-    new_op->upcall.type = PVFS2_VFS_OP_FILE_READ;
-    new_op->upcall.req.read.buf = buf;
-    new_op->upcall.req.read.count = count;
-    new_op->upcall.req.read.offset = offset;
 
-    /* post req and wait for request to be serviced here */
-    pvfs2_error("FIXME: SKIPPING READ: DOING NOTHING!\n");
-/*     add_op_to_request_list(new_op); */
-/*     wait_for_matching_downcall(new_op); */
+    while(total_count < count)
+    {
+	/* get a buffer for the transfer */
+	/* note that we get a new buffer each time for fairness, though
+	 * it may speed things up in the common case more if we kept one
+	 * buffer the whole time; need to measure performance difference
+	 */
+	ret = pvfs_bufmap_get(&desc);
+	if(ret < 0)
+	{
+	    op_release(new_op);
+	    return(ret);
+	}
+    
+	/* how much to transfer in this loop iteration */
+	if((count - total_count) > pvfs_bufmap_size_query())
+	    each_count = pvfs_bufmap_size_query();
+	else
+	    each_count = count - total_count;
 
-    /* when request is serviced, free req op struct */
+	/* TODO: how do I tell it what file I want to work on? */
+	new_op->upcall.type = PVFS2_VFS_OP_FILE_READ;
+	new_op->upcall.req.read.buf = desc->uaddr;
+	new_op->upcall.req.read.count = each_count;
+	new_op->upcall.req.read.offset = *offset;
+
+	/* TODO: submit upcall */
+	/* TODO: wait for downcall */
+
+	/* copy data out to application */
+	pvfs_bufmap_copy_to_user(current_buf, desc, each_count);
+
+	pvfs_bufmap_put(desc);
+
+	current_buf += each_count;
+	*offset += each_count;
+	total_count += each_count;
+    }
+
     op_release(new_op);
 
-    return 0;
+    return(total_count); 
 }
 
 static ssize_t pvfs2_file_write(
--- src/kernel/linux-2.6/upcall.h
+++ src/kernel/linux-2.6/upcall.h
@@ -11,7 +11,7 @@ typedef struct
 {
     char *buf;
     size_t count;
-    loff_t *offset;
+    loff_t offset;
 } pvfs2_read_request_t;
 
 typedef struct
