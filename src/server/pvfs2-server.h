29946b10668143ec8ed8b785f04916298d851cea
Phil Carns <carns@mcs.anl.gov>
2003-07-12 04:05:46 +0000

    crdirent state machine now uses final_response nested machine to handle sending responses and releasing the scheduler
    SVN revision 1440

--- src/server/pvfs2-server.h
+++ src/server/pvfs2-server.h
@@ -160,6 +160,7 @@ typedef struct PINT_server_op
 
 /* nested state machines */
 extern struct PINT_state_machine_s pvfs2_prelude_sm;
+extern struct PINT_state_machine_s pvfs2_final_response_sm;
 
 /* Exported Prototypes */
 struct server_configuration_s *get_server_config_struct(void);
