5d4adae81d410a4b4c3b4b2f5c2b39a72533fd91
Phil Carns <carns@mcs.anl.gov>
2003-07-17 11:59:17 +0000

    more tinkering with thread benchmarks
    SVN revision 1543

--- test/io/job/thread-bench3.c
+++ test/io/job/thread-bench3.c
@@ -8,9 +8,21 @@ pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mut1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond2 = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mut2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond1B = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mut1B = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond2B = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mut2B = PTHREAD_MUTEX_INITIALIZER;
 
 int done = 0;
 int todo = 0;
+int doneB = 0;
+int todoB = 0;
+
+double wtime(void);
+void* thread1_fn(void* foo);
+void* thread1_fnB(void* foo);
+void wakeywakeyB(void);
+void wakeywakey(void);
 
 double wtime(void)
 {
@@ -32,6 +44,7 @@ void* thread1_fn(void* foo)
 		ret = pthread_cond_wait(&cond1, &mut1);
 		assert(ret == 0);
 	    }
+	    wakeywakeyB();
 	    todo = 0;
 	pthread_mutex_unlock(&mut1);
 	
@@ -41,6 +54,50 @@ void* thread1_fn(void* foo)
 	pthread_cond_signal(&cond2);
     }
 
+}
+
+void* thread1_fnB(void* foo)
+{
+    int ret = -1;
+
+    while(1)
+    {
+	pthread_mutex_lock(&mut1B);
+	    while(todoB == 0)
+	    {
+		ret = pthread_cond_wait(&cond1B, &mut1B);
+		assert(ret == 0);
+	    }
+	    todoB = 0;
+	pthread_mutex_unlock(&mut1B);
+	
+	pthread_mutex_lock(&mut2B);
+	doneB = 1;
+	pthread_mutex_unlock(&mut2B);
+	pthread_cond_signal(&cond2B);
+    }
+
+}
+
+
+void wakeywakeyB(void)
+{
+    int ret = -1;
+
+    pthread_mutex_lock(&mut1B);
+    todoB = 1;
+    pthread_mutex_unlock(&mut1B);
+    pthread_cond_signal(&cond1B);
+    
+    pthread_mutex_lock(&mut2B);
+	while(doneB == 0)
+	{
+	    ret = pthread_cond_wait(&cond2B, &mut2B);
+	    assert(ret == 0);
+	}
+	doneB = 0;
+    pthread_mutex_unlock(&mut2B);
+    
 }
 
 void wakeywakey(void)
@@ -67,11 +124,14 @@ void wakeywakey(void)
 int main(int argc, char **argv)	
 {
     pthread_t thread1;
+    pthread_t thread1B;
     int ret = -1;
     int i = 0;
     double time1, time2;
 
     ret = pthread_create(&thread1, NULL, thread1_fn, NULL);
+    assert(ret == 0);
+    ret = pthread_create(&thread1B, NULL, thread1_fnB, NULL);
     assert(ret == 0);
 
     time1 = wtime();
