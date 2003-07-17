4ae0221a374cef59849f304cef7ace001c7f3f9c
Phil Carns <carns@mcs.anl.gov>
2003-07-17 19:01:58 +0000

    took str-utils.h out of pvfs2.h, updated configure to reflect that a module.mk.in has been removed
    SVN revision 1563

--- include/pvfs2.h
+++ include/pvfs2.h
@@ -17,18 +17,6 @@
 #include "pvfs2-debug.h"
 #include "pvfs2-util.h"
 
-/* TODO: things that shouldn't be included here in the long run are 
- * below this line, along with some comments to help indicate what needs 
- * to be fixed to make them go away...
- */
-/******************************************************************************/
-
-/* needed to get string / path manipulation functions, maybe these should
- * be renamed and put in some kind of helper interface that looks more 
- * like something that's ok to call outside of the system interface?
- */
-#include "str-utils.h"
-
 #endif /* __PVFS2_H */
 
 /*
