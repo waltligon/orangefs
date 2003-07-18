458ad528b21a12e306c614006d20cb799efc89a8
Phil Carns <carns@mcs.anl.gov>
2003-07-18 12:44:07 +0000

    put in stubs for our first prototype management function; this one will set a runtime parameter on all active servers for a given file system
    SVN revision 1581

---
+++ include/pvfs2-mgmt.h
@@ -0,0 +1,34 @@
+/*
+ * (C) 2001 Clemson University and The University of Chicago
+ *
+ * See COPYING in top-level directory.
+ */
+
+/* This header includes prototypes for management functions.  */
+
+#ifndef __PVFS2_MGMT_H
+#define __PVFS2_MGMT_H
+
+#include "pvfs2-types.h"
+
+enum PVFS_server_param
+{
+    PVFS_SERV_PARAM_GOSSIP_ACTIVE = 1  /* gossip debuggin on or off */
+};
+
+int PVFS_mgmt_setparam_all(
+    PVFS_fs_id fs_id,
+    PVFS_credentials credentials,
+    enum PVFS_server_param param,
+    int64_t value);
+
+#endif /* __PVFS2_MGMT_H */
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
