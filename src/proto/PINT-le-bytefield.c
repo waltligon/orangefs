8cbf3b0b6f95a314f26b8a578e2ba50a320b2723
Phil Carns <carns@mcs.anl.gov>
2003-08-27 16:22:37 +0000

    more cleaning up
    SVN revision 1986

--- src/proto/PINT-le-bytefield.c
+++ src/proto/PINT-le-bytefield.c
@@ -162,6 +162,10 @@ static int lebf_encode_calc_max_size(
     enum PVFS_server_op op_type);
 static void lebf_initialize(
     void);
+static int lebf_encode_alloc_resp(
+    struct PVFS_server_resp *response,
+    struct PINT_encoded_msg *target_msg);
+    
 
 static PINT_encoding_functions_s lebf_functions = {
     lebf_encode_req,
@@ -210,16 +214,19 @@ static void lebf_initialize(void)
 
     /* getconfig request */
     tmp_msg.ptr_current = 0;
+    memset(&tmp_req, 0, sizeof(tmp_req));
     PINT_XENC_REQ_GEN(&tmp_msg, &tmp_req);
     PINT_XENC_REQ_GETCONFIG(&tmp_msg, &tmp_req);
     max_size_array[PVFS_SERV_GETCONFIG].max_req = (int)(tmp_msg.ptr_current);
+
+
     /* getconfig response */
     tmp_msg.ptr_current = 0;
-    tmp_resp.u.getconfig.fs_config_buf_size = PVFS_REQ_LIMIT_CONFIG_FILE_BYTES;
-    tmp_resp.u.getconfig.server_config_buf_size = PVFS_REQ_LIMIT_CONFIG_FILE_BYTES;
+    memset(&tmp_resp, 0, sizeof(tmp_resp));
     PINT_XENC_RESP_GEN(&tmp_msg, &tmp_resp);
     PINT_XENC_RESP_GETCONFIG(&tmp_msg, &tmp_resp);
-    max_size_array[PVFS_SERV_GETCONFIG].max_resp = (int)(tmp_msg.ptr_current);
+    max_size_array[PVFS_SERV_GETCONFIG].max_resp = (int)(tmp_msg.ptr_current)
+	+ (PVFS_REQ_LIMIT_CONFIG_FILE_BYTES * 2);
 
     return;
 }
@@ -319,32 +326,22 @@ static int lebf_encode_resp(
     target_msg->list_count = 1;
     target_msg->buffer_type = BMI_PRE_ALLOC;
 
-    /* set size of buffer needed, although some requests may override this */
-    target_msg->size_list[0] = target_msg->total_size = 
-	max_size_array[response->op].max_resp + PINT_ENC_GENERIC_HEADER_SIZE;
+    /* compute size and allocate a buffer */
+    ret = lebf_encode_alloc_resp(response, target_msg);
+    if(ret < 0)
+    {
+	return(ret);
+    }
+
+    /* tack on generic header */
+    memcpy(target_msg->ptr_current, le_bytefield_table.generic_header,
+	PINT_ENC_GENERIC_HEADER_SIZE);
+    target_msg->ptr_current += PINT_ENC_GENERIC_HEADER_SIZE;
 
     switch(response->op)
     {
 	case PVFS_SERV_GETCONFIG:
-	    /* adjust size estimage based on what we are sending */
-	    target_msg->ptr_current = NULL;
-	    PINT_XENC_MODE = PINT_CALC_SIZE;
-	    PINT_XENC_RESP_GEN(target_msg, response);
-	    PINT_XENC_RESP_GETCONFIG(target_msg, response);
-	    target_msg->size_list[0] = target_msg->total_size = 
-		(int)(target_msg->ptr_current) + PINT_ENC_GENERIC_HEADER_SIZE;
-	    PINT_XENC_MODE = PINT_ENC;
-
-	    /* create a buffer */
-	    target_msg->buffer_list[0] = target_msg->ptr_current =  
-		BMI_memalloc(target_msg->dest, target_msg->total_size, BMI_SEND);
-	    if(!target_msg->buffer_list[0])
-		ret = -ENOMEM;
-
 	    /* encode */
-	    memcpy(target_msg->ptr_current, le_bytefield_table.generic_header,
-		PINT_ENC_GENERIC_HEADER_SIZE);
-	    target_msg->ptr_current += PINT_ENC_GENERIC_HEADER_SIZE;
 	    PINT_XENC_RESP_GEN(target_msg, response);
 	    PINT_XENC_RESP_GETCONFIG(target_msg, response);
 	    ret = 0;
@@ -494,6 +491,49 @@ static void lebf_encode_rel(
     return;
 }
 
+/* lebf_encode_alloc_resp()
+ *
+ * internal function, calculates size needed for encoded version 
+ * of a response and allocates the buffer
+ *
+ * returns 0 on success, -errno on failure
+ */
+static int lebf_encode_alloc_resp(
+    struct PVFS_server_resp* response,
+    struct PINT_encoded_msg* target_msg)
+{
+    int PINT_XENC_MODE = PINT_CALC_SIZE;
+
+    target_msg->ptr_current = NULL;
+    target_msg->total_size = 0;
+
+    PINT_XENC_RESP_GEN(target_msg, response);
+
+    switch(response->op)
+    {
+	case PVFS_SERV_GETCONFIG:
+	    PINT_XENC_RESP_GETCONFIG(target_msg, response);
+	    target_msg->total_size = (int)(target_msg->ptr_current) + 
+		PINT_ENC_GENERIC_HEADER_SIZE;
+	    break;
+	default:
+	    gossip_lerr("Error: unsupported operation.\n");
+	    return(-ENOSYS);
+	    break;
+    }
+
+    target_msg->size_list[0] = target_msg->total_size;
+    target_msg->buffer_list[0] = 
+	BMI_memalloc(target_msg->dest, target_msg->total_size,
+	BMI_SEND);
+    if(!target_msg->buffer_list[0])
+    {
+	return(-ENOMEM);
+    }
+
+    target_msg->ptr_current = target_msg->buffer_list[0];
+    return(0);
+}
 
 /*
  * Local variables:
