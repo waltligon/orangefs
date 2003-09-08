b59360d7466b1f30fc193ed402216362d604dd45
Phil Carns <carns@mcs.anl.gov>
2003-09-08 15:18:31 +0000

    implemented a starting point for sysint statfs function, will not return very accurate info until some more work is done.  untested.
    SVN revision 2047

---
+++ src/client/sysint/sys-statfs.c
@@ -0,0 +1,92 @@
+/* 
+ * (C) 2003 Clemson University and The University of Chicago 
+ *
+ * See COPYING in top-level directory.
+ */
+
+#include <string.h>
+#include <assert.h>
+
+#include "client-state-machine.h"
+#include "state-machine-fns.h"
+#include "pvfs2-debug.h"
+#include "gossip.h"
+#include "pvfs2-mgmt.h"
+
+
+int PVFS_sys_statfs(
+    PVFS_fs_id fs_id,
+    PVFS_credentials credentials,
+    PVFS_sysresp_statfs* resp)
+{
+    int num_servers = 0;
+    int check_num_servers = 0;
+    int overflow_flag = 0;
+    int ret = -1;
+    PVFS_statfs* statfs_array = NULL;
+    int i;
+
+    /* first, determine how many servers are in the file system */
+    ret = PVFS_mgmt_count_servers(fs_id, credentials, &num_servers);
+    if(ret < 0)
+    {
+	return(ret);
+    }
+
+    /* allocate room for statfs info from all servers */
+    statfs_array = (PVFS_statfs*)malloc(num_servers*sizeof(PVFS_statfs));
+    if(!statfs_array)
+    {
+	return(-PVFS_ENOMEM);
+    }
+
+    /* gather all statfs information */
+    ret = PVFS_mgmt_statfs_all(
+	fs_id,
+	credentials,
+	num_servers,
+	&check_num_servers,
+	&overflow_flag,
+	statfs_array);
+    if(ret < 0)
+    {
+	free(statfs_array);
+	return(ret);
+    }
+
+    /* our logic is wrong somewhere if we didn't correctly calculate the
+     * number of servers to receive statfs information from
+     */
+    assert(num_servers == check_num_servers && overflow_flag == 0);
+
+    /* aggregate statistics down into one statfs structure */
+
+    /* TODO: this is all wrong!
+     * need to put in a better free size calculation that takes into 
+     * account which servers are I/O servers, and what the default 
+     * distribution is?  Need to think about this some more...
+     */
+    resp->statfs_buf.fs_id = fs_id;
+    resp->statfs_buf.bytes_available = 0;
+    resp->statfs_buf.bytes_total = 0;
+    for(i=0; i<num_servers; i++)
+    {
+	resp->statfs_buf.bytes_available += statfs_array[i].bytes_available;
+	resp->statfs_buf.bytes_total += statfs_array[i].bytes_total;
+    }
+    resp->server_count = num_servers;
+
+    free(statfs_array);
+
+    return(0);
+}
+
+/*
+ * Local variables:
+ *  mode: c
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ft=c ts=8 sts=4 sw=4 noexpandtab
+ */
