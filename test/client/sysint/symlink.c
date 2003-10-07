a0795848d0c725eec3b28e496837749b1dbf6a4d
Neill Miller <neillm@mcs.anl.gov>
2003-10-07 17:18:17 +0000

    use real uid/gid when testing symlink creation
    SVN revision 2356

--- test/client/sysint/symlink.c
+++ test/client/sysint/symlink.c
@@ -6,6 +6,8 @@
 
 #include <time.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 #include "client.h"
 #include "pvfs2-util.h"
@@ -65,8 +67,8 @@ int main(int argc, char **argv)
 
     entry_name = str_buf;
     attr.mask = PVFS_ATTR_SYS_ALL_SETABLE;
-    attr.owner = 100;
-    attr.group = 100;
+    attr.owner = getuid();
+    attr.group = getgid();
     attr.perms = 1877;
     attr.atime = attr.ctime = attr.mtime = time(NULL);
     credentials.uid = attr.owner;
