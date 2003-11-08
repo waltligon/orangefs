1f4431a2e4838f006e56d28d5dc3212229bda350
Jiesheng Wu <wujs@mcs.anl.gov>
2003-11-08 13:24:06 +0000

    bug fix.   -- All extents associated with a same Trove request were not linked      correctly for more than two extents.
    SVN revision 2566

--- src/io/buffer/state.c
+++ src/io/buffer/state.c
@@ -23,19 +23,19 @@ int NCAC_extent_read_access(NCAC_req_t *ncac_req, struct extent *page, unsigned 
 	struct cache_stack *cache_stack;
 	int ret;
 
-    cache_stack = ncac_req->mapping->cache_stack;
+	cache_stack = ncac_req->mapping->cache_stack;
 
-    IncReadCount(page);
+	IncReadCount(page);
 
-    if ( !PageActive(page) && PageLRU(page) )
+	if ( !PageActive(page) && PageLRU(page) )
 	{
-	    cache_lock(&(cache_stack->lock));
+		cache_lock(&(cache_stack->lock));
 
 		cache_stack = get_extent_cache_stack(page);
 		NCAC_move_inactive_to_active(cache_stack, page);
 		SetPageActive(page);
 
-	    cache_unlock(&(cache_stack->lock));
+		cache_unlock(&(cache_stack->lock));
 
 	}
 
@@ -48,9 +48,9 @@ int NCAC_extent_read_access(NCAC_req_t *ncac_req, struct extent *page, unsigned 
 	}
 
 	/* in active list */
-    if (PageLRU(page) && PageActive(page)){
+	if (PageLRU(page) && PageActive(page)){
 
-	    if (PageRcomm(page)){ /* read communication pending */
+		if (PageRcomm(page)){ /* read communication pending */
 			ret = 1;
 			goto out;
 		}
@@ -66,26 +66,27 @@ int NCAC_extent_read_access(NCAC_req_t *ncac_req, struct extent *page, unsigned 
 		/* The following cases are those without pending communication */
 
 		/* clean or dirty extent */
-	    if (PageClean(page) || PageDirty(page) ) {
+		if (PageClean(page) || PageDirty(page) ) {
 			SetPageRcomm(page);	ret = 1; 
 			goto out;
 		}
 
 		/* read pending: Trove read operation is pending.  */
 		/* write pending: for example flushing daemon is working on it */
-	    if (PageReadPending(page) || PageWritePending(page)) {
+		if (PageReadPending(page) || PageWritePending(page)) {
 			error = NCAC_check_ioreq(page);
 			if (error <0) {
 				NCAC_error("%s:%d NCAC_check_ioreq error\n", __FILE__, __LINE__);
 				ret = 0; goto out;
 			}
+
 			if (error) {
-                /* set all other related extents */
+				/* set all other related extents */
 				list_set_clean_page(page);
 
-			    /* the request is done and the read is finished.
+				/* the request is done and the read is finished.
 				 * Note that NCAC_check_ioreq sets page flags in this case.*/
-			    SetPageRcomm(page);	
+				SetPageRcomm(page);	
 				ret = 0; 
 				goto out;
 			}
@@ -113,14 +114,14 @@ int NCAC_extent_write_access(NCAC_req_t *ncac_req, struct extent *page, unsigned
 
 	IncWriteCount(page);
 
-    /* the first write */
-    if ( PageLRU(page) && page->writes - page->wcmp ==1 ){
+	/* the first write */
+	if ( PageLRU(page) && page->writes - page->wcmp ==1 ){
 
-	    if (PageRcomm(page) || PageWcomm(page)) 
+		if (PageRcomm(page) || PageWcomm(page)) 
 			return 0;
 
 
-	    if (PageReadPending(page) || PageWritePending(page)) {
+		if (PageReadPending(page) || PageWritePending(page)) {
 			error = NCAC_check_ioreq(page);
 			if (error <0) {
 				NCAC_error("%s:%d NCAC_check_ioreq error\n", __FILE__, __LINE__);
@@ -128,31 +129,33 @@ int NCAC_extent_write_access(NCAC_req_t *ncac_req, struct extent *page, unsigned
 			}
 			if (error) { /* completion */
 
-                /* set all other related extents */
-				list_set_clean_page(page);
+			/* set all other related extents */
+			list_set_clean_page(page);
 
-			    /* the request is done and the read is finished.
-				 * Note that NCAC_check_ioreq sets page flags in this case.*/
-			    SetPageWcomm(page);	
+			/* the request is done and the read is finished.
+			 * Note that NCAC_check_ioreq sets page flags in 
+			 * this case.*/
+
+			SetPageWcomm(page);	
 				return 1;
 			}
-		    return 0;	
+			return 0;	
 		}
 
 		/* No pending comm, clean or dirty extent */
-	    if (PageClean(page) || PageDirty(page) ) {
+		if (PageClean(page) || PageDirty(page) ) {
 			SetPageWcomm(page);	
 			return 1;
 		}
 	}
 
-    /* it is not the first write */
-    if ( page->writes - page->wcmp >1 ){
+	/* it is not the first write */
+	if ( page->writes - page->wcmp >1 ){
 		return 0;
 	}
 
-    /* there is pending reads */
-    if ( page->reads != page->rcmp  ){
+	/* there is pending reads */
+	if ( page->reads != page->rcmp  ){
 		return 0;
 	}
 
@@ -192,7 +195,7 @@ int NCAC_extent_first_write_access(NCAC_req_t *ncac_req, struct extent *page)
  * */
 int NCAC_extent_done_access(NCAC_req_t *ncac_req)
 {
-    int  i;
+	int  i;
 	struct inode *inode;
 	struct list_head *dirty_list = NULL;
 	struct cache_stack *cache;
@@ -201,65 +204,73 @@ int NCAC_extent_done_access(NCAC_req_t *ncac_req)
 
 	inode = ncac_req->mapping;
 
-    inode_lock( &inode->lock);
+	inode_lock (&inode->lock);
 
-    if (ncac_req->optype == NCAC_READ ){
-        for ( i = 0; i < ncac_req->cbufcnt; i ++ ) {
-            if ( ncac_req->cbufflag[i] && ncac_req->cbufhash[i] ) {
-                NCAC_extent_read_comm_done(ncac_req->cbufhash[i]); 
-                //DecReadCount(ncac_req->cbufhash[i]);
-                ncac_req->cbufhash[i]->rcmp ++;
-            }
-        }   
-    }
-    if (ncac_req->optype == NCAC_WRITE) {
+	if (ncac_req->optype == NCAC_READ)
+	{
+		for (i = 0; i < ncac_req->cbufcnt; i++)
+		{
+			if (ncac_req->cbufflag[i] && ncac_req->cbufhash[i])
+			{
+				NCAC_extent_read_comm_done (ncac_req->cbufhash[i]);
+				//DecReadCount(ncac_req->cbufhash[i]);
+				ncac_req->cbufhash[i]->rcmp++;
+			}
+      		}
+  	}
+
+	if (ncac_req->optype == NCAC_WRITE)
+	{
 		dirty_list = &(inode->dirty_pages);
-        for ( i = 0; i < ncac_req->cbufcnt; i ++ ) {
-            if ( ncac_req->cbufflag[i] && ncac_req->cbufhash[i] ) {
-                NCAC_extent_write_comm_done(ncac_req->cbufhash[i]); 
-                //DecWriteCount(ncac_req->cbufhash[i]);
-                ncac_req->cbufhash[i]->wcmp ++;
+		for (i = 0; i < ncac_req->cbufcnt; i++)
+		{
+			if (ncac_req->cbufflag[i] && ncac_req->cbufhash[i])
+			{
+				NCAC_extent_write_comm_done (ncac_req->cbufhash[i]);
+				//DecWriteCount(ncac_req->cbufhash[i]);
+				ncac_req->cbufhash[i]->wcmp++;
 
 				/* add dirty pages */
-				list_add_tail(&ncac_req->cbufhash[i]->list, dirty_list);
-				nr_dirty ++;
+				list_add_tail (&ncac_req->cbufhash[i]->list, dirty_list);
+				nr_dirty++;
 
-            }
-        }   
+			}
+		}
 		inode->nr_dirty += nr_dirty;
 
-        /* handle sync stuff: there are three cases:
+		/* handle sync stuff: there are three cases:
 		 * 1) if a file is opend with O_SYNC, we should do sync
 		 * 2) if we define aggressive flush, we do sync here.
 		 * 3) otherwise, sync depends on our flush policy.
 		 * TODO: currently we don't know O_SYNC flag 
 		 */
 
-		 /* if ( ncac_req->f_flags & O_SYNC || IS_SYNC(inode) ) {
+		/* if ( ncac_req->f_flags & O_SYNC || IS_SYNC(inode) ) {
 			data_sync(inode);
-		 }
+		   }
 		 */
 
 #if defined(LAZY_SYNC)
-		ret = balance_dirty_extents( ncac_req->mapping->cache_stack);
+		ret = balance_dirty_extents (ncac_req->mapping->cache_stack);
 
 #else /* aggressive sync */
-		ret = data_sync_inode(inode);
+		ret = data_sync_inode (inode);
 #endif
-    }
-
-    inode_unlock(&inode->lock);
-
-	if ( dirty_list ) {  /* atomic add */
-		cache = inode->cache_stack;
-
-		cache_lock(&cache->lock);
-		cache->nr_dirty += ( nr_dirty - ret);
-		cache_unlock(&cache->lock);
-		DPRINT("-------nr_dirty=%d, flush=%d\n", nr_dirty, ret);
 	}
 
-    return 0;
+	inode_unlock (&inode->lock);
+
+	if (dirty_list)
+	{				/* atomic add */
+		cache = inode->cache_stack;
+
+		cache_lock (&cache->lock);
+		cache->nr_dirty += (nr_dirty - ret);
+		cache_unlock (&cache->lock);
+		DPRINT ("-------nr_dirty=%d, flush=%d\n", nr_dirty, ret);
+	}
+
+	return 0;
 }
 
 /* read commnication is done */
@@ -279,10 +290,11 @@ int NCAC_extent_write_comm_done(struct extent *page)
 }
 
 void list_set_clean_page(struct extent *page)
+
 {
     struct extent *next;
 
-	int cnt = 0;  /* for debug only */
+    int cnt = 0;  
 
     DPRINT("iodone before: flags:%lx\n", page->flags);
     ClearPageDirty(page);
@@ -290,25 +302,27 @@ void list_set_clean_page(struct extent *page)
     ClearPageReadPending(page);
     ClearPageWritePending(page);
 
-	page->ioreq = INVAL_IOREQ; 
+    page->ioreq = INVAL_IOREQ; 
     next = page->ioreq_next;
 
-	cnt ++;
+    cnt ++;
 
     while ( next != page ) {
-        SetPageClean(next);
-	    ClearPageDirty(next);
-    	ClearPageReadPending(next);
-    	ClearPageWritePending(next);
+        SetPageClean (next);
+        ClearPageDirty (next);
+        ClearPageReadPending (next);
+        ClearPageWritePending (next);
 
-	    next->ioreq = INVAL_IOREQ; 
+        next->ioreq = INVAL_IOREQ;
 
-		cnt ++;
+        cnt++;
 
         next = next->ioreq_next;
     }
 
-	DPRINT("clean_page: %d\n", cnt);
+    DPRINT("clean_page: %d\n", cnt);
+
+    return;
 }
 
 
@@ -420,6 +434,7 @@ int NCAC_extent_write_access_recheck(NCAC_req_t *ncac_req, struct extent *page, 
     return NCAC_INVAL_FLAGS;
 }
 
+
 #if 0
 static void	balance_dirty_extents( struct cache_stack *cache_stack)
 {
@@ -452,13 +467,13 @@ int data_sync_inode(struct inode *inode)
 	DPRINT("data_sync_inode: nr_dirty=%d\n", nr_dirty);
 	if ( !nr_dirty ) return 0;
 
-    /* allocate Trove parameters */
+	/* allocate Trove parameters */
 	offset = (PVFS_offset *)malloc(nr_dirty*(sizeof(PVFS_offset) + sizeof(char*) + 2*sizeof(PVFS_size)) );
 	mem = (char**)&offset[nr_dirty];
 	offsize = (PVFS_size*)&mem[nr_dirty];
 	memsize = &offsize[nr_dirty];
 	
-    list = &(inode->dirty_pages);
+	list = &(inode->dirty_pages);
 	
 	/* from the head to the tail to keep the order */
 	tail = list->next; 
@@ -469,45 +484,48 @@ int data_sync_inode(struct inode *inode)
 		mem[cnt] = extent->addr;
 		memsize[cnt] = NCAC_dev.extsize;
 
-        DPRINT("to write: pos:%Ld, size=%Ld, buf=%p\n", 
-					 offset[cnt], offsize[cnt], mem[cnt]);
-        cnt ++;
+		DPRINT("to write: pos:%Ld, size=%Ld, buf=%p\n", 
+			 offset[cnt], offsize[cnt], mem[cnt]);
+		cnt ++;
 		tail = tail->next;
 	}
 
 	ret = NCAC_aio_write(inode->coll_id, inode->handle, 
-						inode->context_id, nr_dirty, 
-					    offset, offsize, mem, memsize, &ioreq);
+			inode->context_id, nr_dirty, 
+			offset, offsize, mem, memsize, &ioreq);
 	if ( ret < 0 ) {
 		NCAC_error("data_sync_inode: NCAC_aio_write error \n");
 		return ret;
 	}else{
-		/* we need to associate all extents to  an IOreq */
-	    tail = list->next; 
+		/* we need to associate all extents to an IOreq */
+
+		tail = list->next; 
 		extent = list_entry(tail, struct extent, list);
-	    SetPageWritePending(extent);
+
+		SetPageWritePending(extent);
 		extent->ioreq_next = extent;
 		prev = first = extent;
 
 		extent->ioreq = ioreq;
-		tail = tail->next;
 		list_del(&extent->list);
+		tail = list->next;
 
-        for ( i = 1; i < nr_dirty; i ++ ) { 
-		    extent = list_entry(tail, struct extent, list);
-	        SetPageWritePending(extent);
-		    prev->ioreq_next = extent;
+		for ( i = 1; i < nr_dirty; i ++ ) { 
+			extent = list_entry(tail, struct extent, list);
+			SetPageWritePending(extent);
+			prev->ioreq_next = extent;
 			extent->ioreq = ioreq;
 
-		    tail = tail->next;
-		    list_del(&extent->list);
-	    }
+			prev = extent;
+			list_del(&extent->list);
+			tail = list->next;
+		}
 		extent->ioreq_next = first;
 
 		inode->nr_dirty = 0;
 	}
 
-    return nr_dirty;
+	return nr_dirty;
 }
 
 
