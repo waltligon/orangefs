f41509a4e76ba5be7d6322ac63f40bdc4e8d465a
Neill Miller <neillm@mcs.anl.gov>
2004-01-16 17:55:21 +0000

    - some cleanups and error handling
    SVN revision 2772

--- src/client/sysint/getparent.c
+++ src/client/sysint/getparent.c
@@ -17,12 +17,15 @@ int PVFS_sys_getparent(
     PVFS_credentials credentials,
     PVFS_sysresp_getparent *resp)
 {
+    int ret = -PVFS_EINVAL;
     char parent_buf[PVFS_NAME_MAX] = {0};
     char file_buf[PVFS_SEGMENT_MAX] = {0};
-    
     PVFS_sysresp_lookup resp_look;
 
-    memset(&resp_look,0,sizeof(PVFS_sysresp_lookup));
+    if ((entry_name == NULL) || (resp == NULL))
+    {
+        return ret;
+    }
 
     if (PINT_get_base_dir(entry_name,parent_buf,PVFS_NAME_MAX))
     {
@@ -31,22 +34,23 @@ int PVFS_sys_getparent(
             gossip_err("Invalid dirname (no leading '/')\n");
         }
         gossip_err("cannot get parent directory of %s\n",entry_name);
-        return -1;
+        return ret;
     }
 
-    /* retrieve the parent handle */
-    if (PVFS_sys_lookup(fs_id, parent_buf, credentials,
-                        &resp_look, PVFS2_LOOKUP_LINK_NO_FOLLOW))
+    memset(&resp_look,0,sizeof(PVFS_sysresp_lookup));
+    ret = PVFS_sys_lookup(fs_id, parent_buf, credentials,
+                          &resp_look, PVFS2_LOOKUP_LINK_NO_FOLLOW);
+    if (ret)
     {
         gossip_err("Lookup failed on %s\n",parent_buf);
-        return -1;
+        return ret;
     }
     resp->parent_refn = resp_look.pinode_refn;
 
     if (PVFS_util_remove_base_dir(entry_name,file_buf,PVFS_SEGMENT_MAX))
     {
 	gossip_err("invalid filename: %s\n", entry_name);
-	return -1;
+	return ret;
     }
     strncpy(resp->basename, file_buf, PVFS_SEGMENT_MAX);
 
