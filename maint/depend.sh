d61f4db0ea0d788bdd623bc0d37cb1e65969c987
Pete Wyckoff <peter.wyckoff@gmail.com>
2004-03-12 17:58:50 +0000

    - Build system:     - list explicit include directories     - add cflags provided by module makefile stubs     - install kernel module if configured     - simplify dependency generator script, add .po
    SVN revision 3139

--- maint/depend.sh
+++ maint/depend.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 DIR="$1"
-shift 1
-case "$DIR" in
-"" | ".")
-gcc -M "$@" | sed -e 's@^\(.*\)\.o:@\1.d \1.o \1-server.o:@'
-;;
-*)
-gcc -M "$@" | sed -e "s@^\(.*\)\.o:@$DIR/\1.d $DIR/\1.o $DIR/\1-server.o:@"
-;;
-esac
-exit $?
+shift
+if [ -n "$DIR" ] ; then
+    DIR="$DIR"/
+fi
+exec gcc -M "$@" | sed -e "s@^\(.*\)\.o:@$DIR\1.d $DIR\1.o $DIR\1-server.o $DIR\1.po:@"
