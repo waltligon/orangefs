c133635c5d1b7ebc231261a71b245ee8a8ae349d
Rob Ross <rross@mcs.anl.gov>
2004-04-28 15:48:16 +0000

    Added PVFS_error_details functions and definitions for use in partial success handling cases.  Not called anywhere yet.
    SVN revision 3402

---
+++ src/client/sysint/error-details.c
@@ -0,0 +1,49 @@
+/*
+ * (C) 2004 Clemson University and The University of Chicago
+ *
+ * See COPYING in top-level directory.
+ */
+
+#include <stdlib.h>
+
+#include "pvfs2-types.h"
+
+/* PVFS2 error details functions
+ *
+ * The error details system is used for returning server-specific errors
+ * for cases where one or more errors have occurred when talking to
+ * a collection of servers.
+ *
+ * The PVFS_error_details structure is defined in pvfs2-types.h.
+ */
+
+/* PVFS_error_details_new(count)
+ *
+ * Q: SHOULD WE RETURN AN INTEGER ERROR INSTEAD?
+ */
+PVFS_error_details *PVFS_error_details_new(int count)
+{
+    PVFS_error_details *details;
+    int sz;
+
+    sz = sizeof(PVFS_error_details) +
+	(count-1) * sizeof(PVFS_error_server);
+
+    details = (PVFS_error_details *) malloc(sz);
+
+    return details;
+}
+
+void PVFS_error_details_free(PVFS_error_details *details)
+{
+    free(details);
+}
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
