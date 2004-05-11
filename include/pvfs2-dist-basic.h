5ae55d6e38f30c2f9cc1d57f2fb71b8f4a96b693
Brad Settlemyer <settlemyerbw@ornl.gov>
2004-05-11 16:12:27 +0000

    Continuing distribution changes.
    Default distribution (which wasn't really the default), is now called the
    basic distribution and conforms to new interfaces.
    
    
    SVN revision 3497

---
+++ include/pvfs2-dist-basic.h
@@ -0,0 +1,27 @@
+/*
+ * (C) 2002 Clemson University.
+ *
+ * See COPYING in top-level directory.
+ */       
+
+#ifndef  __PVFS2_DIST_BASIC_H 
+#define  __PVFS2_DIST_BASIC_H 
+
+/* Identifier to use when looking up this distribution */
+const char* const PVFS_dist_simple_stripe_id = "basic_dist";
+const int32_t PVFS_dist_simple_stripe_id_size = 11;
+
+struct PVFS_basic_params_s {
+};
+typedef struct PVFS_basic_params_s PVFS_basic_params;
+
+#endif
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
---
+++ src/io/description/dist-basic.c
@@ -0,0 +1,111 @@
+/*
+ * (C) 2002 Clemson University.
+ *
+ * See COPYING in top-level directory.
+ */       
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "pvfs-distribution.h"
+#include "pvfs2-types.h"
+#include "pvfs2-dist-basic.h"
+
+#define CONTIGBLOCKSZ 65536
+
+/* in this distribution all data is stored on a single server */
+
+static PVFS_offset logical_to_physical_offset(void* params,
+                                              uint32_t server_nr,
+                                              uint32_t server_ct,
+                                              PVFS_offset logical_offset)
+{
+    return logical_offset;
+}
+
+static PVFS_offset physical_to_logical_offset(void* params,
+                                              uint32_t server_nr,
+                                              uint32_t server_ct,
+                                              PVFS_offset physical_offset)
+{
+    return physical_offset;
+}
+
+static PVFS_offset next_mapped_offset(void* params,
+                                      uint32_t server_nr,
+                                      uint32_t server_ct,
+                                      PVFS_offset logical_offset)
+{
+    return logical_offset;
+}
+
+static PVFS_size contiguous_length(void* params,
+                                   uint32_t server_nr,
+                                   uint32_t server_ct,
+                                   PVFS_offset physical_offset)
+{
+    return CONTIGBLOCKSZ;
+}
+
+static PVFS_size logical_file_size(void* params,
+                                   uint32_t server_ct,
+                                   PVFS_size *psizes)
+{
+    if (!psizes)
+        return -1;
+    return psizes[0];
+}
+
+static void encode(void* params, void *buffer)
+{
+    memcpy(buffer, params, sizeof(PVFS_basic_params));
+}
+
+static void decode(void* params, void *buffer)
+{
+	memcpy(params, buffer, sizeof(PVFS_basic_params));
+}
+
+static void encode_lebf(char **pptr, void* params)
+{
+}
+
+static void decode_lebf(char **pptr, void* params)
+{
+}
+
+static PVFS_basic_params basic_params;
+
+static PVFS_Dist_methods basic_methods = {
+	logical_to_physical_offset,
+	physical_to_logical_offset,
+	next_mapped_offset,
+	contiguous_length,
+	logical_file_size,
+	encode,
+	decode,
+	encode_lebf,
+	decode_lebf,
+};
+
+PVFS_Dist basic_dist = {
+	"basic_dist",
+	roundup8(11), /* name size */
+	0, /* param size */
+	&basic_params,
+	&basic_methods
+};
+
+#ifdef MODULE
+
+void init_module()
+{
+	 PVFS_register_distribution(&basic_dist);
+}
+
+void cleanup_module()
+{
+	 PVFS_unregister_distribution("basic_dist");
+}
+
+#endif
