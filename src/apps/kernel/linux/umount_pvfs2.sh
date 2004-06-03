7153561e4384b8c03125bc1537b1b35bf355bc12
Neill Miller <neillm@mcs.anl.gov>
2004-06-03 14:14:40 +0000

    - helpful umount testing info
    SVN revision 3560

--- src/apps/kernel/linux/umount_pvfs2.sh
+++ src/apps/kernel/linux/umount_pvfs2.sh
@@ -3,6 +3,12 @@
 umount /tmp/mnt
 killall -TERM pvfs2-client
 sleep 2
-rmmod pvfs2
+
+if test "x`fuser /dev/pvfs2-req`" = "x"; then
+    rmmod pvfs2
+else
+    echo "It seems that the pvfs2 device file is still open!"
+    fuser /dev/pvfs2-req
+fi
 
 
