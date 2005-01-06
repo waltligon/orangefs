39ddf7f6357def0c703f178f0600257a86383fbc
no author <devel@lists.orangefs.org>
2005-01-06 21:42:03 +0000

    This commit was manufactured by cvs2svn to create tag 'pvfs2-'.
    SVN revision 4191

--- maint/pvfs2htmlfixup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# this is the complement to the text added to the head of all generated .html
-# through latex2html-init .  I couldn't find a good way to make latex2html add
-# the closing tags and '#include virtual=...' stuff
-
-perl -p -i -e 's~</HEAD>~<!--#include virtual="style_defs.html" -->\n</HEAD>~' $@
-perl -p -i -e 's~</ADDRESS>~</ADDRESS>\n</td>\n</tr>\n</table>\n<!--#include virtual="bottom_of_page.html" -->\n</td>\n</tr>\n</table>~' $@
--- test/correctness/pts/test-romio-noncontig-pattern2.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_ROMIO_NONCONTIG_PATTERN2_H
-#define TEST_ROMIO_NONCONTIG_PATTERN2_H
-
-int test_romio_noncontig_pattern2(MPI_Comm *comm, int rank,  char *buf, void *rawparams);
-
-#endif
