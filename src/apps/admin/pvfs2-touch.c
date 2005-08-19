5e2fa5ad0c105d4de4bea2f5249b122d0c3c70b0
no author <devel@lists.orangefs.org>
2005-08-19 16:44:30 +0000

    This commit was manufactured by cvs2svn to create branch 'slang-event- changes-branch'.
    SVN revision 4567

---
+++ src/apps/admin/pvfs2-touch.c
@@ -0,0 +1,209 @@
+/*
+ * (C) 2001 Clemson University and The University of Chicago
+ *
+ * See COPYING in top-level directory.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#include "pvfs2.h"
+#include "str-utils.h"
+
+#ifndef PVFS2_VERSION
+#define PVFS2_VERSION "Unknown"
+#endif
+
+/* optional parameters, filled in by parse_args() */
+struct options
+{
+    uint32_t num_files;
+    char **filenames;
+};
+
+static struct options *parse_args(int argc, char **argv);
+static void usage(int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+    int ret = -1, i = 0;
+    struct options *user_opts = NULL;
+
+    /* look at command line arguments */
+    user_opts = parse_args(argc, argv);
+    if(!user_opts)
+    {
+	fprintf(stderr, "Error: failed to parse command line arguments.\n");
+	return(-1);
+    }
+
+    /* Initialize the pvfs2 server */
+    ret = PVFS_util_init_defaults();
+    if(ret < 0)
+    {
+	PVFS_perror("PVFS_util_init_defaults", ret);
+	return -1;
+    }
+
+    /* Remove each specified file */
+    for (i = 0; i < user_opts->num_files; ++i)
+    {
+        int rc;
+        int num_segs;
+        char *working_file = user_opts->filenames[i];
+        char directory[PVFS_NAME_MAX];
+        char filename[PVFS_SEGMENT_MAX];
+
+        char pvfs_path[PVFS_NAME_MAX] = {0};
+        PVFS_fs_id cur_fs;
+        PVFS_sysresp_lookup resp_lookup;
+        PVFS_sysresp_create resp_create;
+        PVFS_credentials credentials;
+        PVFS_object_ref parent_ref;
+        PVFS_sys_attr attr;
+
+        /* Translate path into pvfs2 relative path */
+        rc = PINT_get_base_dir(working_file, directory, PVFS_NAME_MAX);
+        num_segs = PINT_string_count_segments(working_file);
+        rc = PINT_get_path_element(working_file, num_segs - 1,
+                                   filename, PVFS_SEGMENT_MAX);
+
+        if (rc)
+        {
+            fprintf(stderr, "Unknown path format: %s\n", working_file);
+            ret = -1;
+            break;
+        }
+
+        rc = PVFS_util_resolve(directory, &cur_fs,
+                               pvfs_path, PVFS_NAME_MAX);
+        if (rc)
+        {
+            PVFS_perror("PVFS_util_resolve", rc);
+            ret = -1;
+            break;
+        }
+
+        credentials.uid = getuid();
+        credentials.gid = getuid();
+
+        memset(&resp_lookup, 0, sizeof(PVFS_sysresp_lookup));
+        rc = PVFS_sys_lookup(cur_fs, pvfs_path, &credentials,
+                             &resp_lookup, PVFS2_LOOKUP_LINK_NO_FOLLOW);
+        if (rc)
+        {
+            PVFS_perror("PVFS_sys_lookup", rc);
+            ret = -1;
+            break;
+        }
+
+        /* Set attributes */
+        memset(&attr, 0, sizeof(PVFS_sys_attr));
+        attr.owner = credentials.uid;
+        attr.group = credentials.gid;
+        attr.perms = 0;
+        attr.atime = time(NULL);
+        attr.mtime = attr.atime;
+        attr.ctime = attr.atime;
+        attr.mask = (PVFS_ATTR_SYS_ALL_SETABLE);
+        attr.dfile_count = 0;
+
+        parent_ref = resp_lookup.ref;
+
+        rc = PVFS_sys_create(filename,
+                             parent_ref,
+                             attr,
+                             &credentials,
+                             NULL,
+                             &resp_create);
+        if (rc)
+        {
+            fprintf(stderr, "Error: An error occurred while creating %s\n",
+                    working_file);
+            PVFS_perror("PVFS_sys_create", rc);
+            ret = -1;
+            break;
+        }
+    }
+
+    PVFS_sys_finalize();
+    free(user_opts);
+
+    return ret;
+}
+
+/* parse_args()
+ *
+ * parses command line arguments
+ *
+ * returns pointer to options structure on success, NULL on failure
+ */
+static struct options* parse_args(int argc, char **argv)
+{
+    int one_opt = 0;
+    char flags[] = "?";
+    struct options *tmp_opts = NULL;
+
+    tmp_opts = (struct options *)malloc(sizeof(struct options));
+    if(!tmp_opts)
+    {
+	return NULL;
+    }
+    memset(tmp_opts, 0, sizeof(struct options));
+
+    tmp_opts->filenames = 0;
+
+    while((one_opt = getopt(argc, argv, flags)) != EOF)
+    {
+	switch(one_opt)
+        {
+	    case('?'):
+		usage(argc, argv);
+		exit(EXIT_FAILURE);
+	}
+    }
+
+    if (optind < argc)
+    {
+        int i = 0;
+        tmp_opts->num_files = argc - optind;
+        tmp_opts->filenames = malloc((argc - optind) * sizeof(char*));
+        while (optind < argc)
+        {
+            tmp_opts->filenames[i] = argv[optind];
+            optind++;
+            i++;
+        }
+    }
+    else
+    {
+	usage(argc, argv);
+	exit(EXIT_FAILURE);
+    }
+    return tmp_opts;
+}
+
+
+static void usage(int argc, char **argv)
+{
+    fprintf(stderr, "Usage: %s [-rf] pvfs2_filename[s]\n", argv[0]);
+}
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 expandtab
+ */
+
---
+++ src/kernel/linux-2.6/xattr.c
@@ -0,0 +1,80 @@
+/*
+ * (C) 2001 Clemson University and The University of Chicago
+ *
+ * See COPYING in top-level directory.
+ */
+
+/** \file
+ *  \ingroup pvfs2linux
+ *
+ *  Linux VFS extended attribute operations.
+ */
+
+#include "pvfs2-kernel.h"
+#include "pvfs2-bufmap.h"
+
+#ifdef HAVE_XATTR
+#include <linux/xattr.h>
+
+/* All pointers are in kernel-space */
+int pvfs2_setxattr(struct dentry *dentry, const char *name,
+		const void *value, size_t size, int flags)
+{
+    struct inode *inode = dentry->d_inode;
+    int internal_flag = 0;
+
+    /* VFS does a whole bunch of checks, but we still do
+    *  some here */
+    if (name == NULL || value == NULL ||
+            size < 0 || size >= PVFS_MAX_XATTR_VALUELEN)
+    {
+        pvfs2_error("pvfs2_setxattr: invalid parameters\n");
+        return -EINVAL;
+    }
+    /* Attribute must exist! */
+    if (flags & XATTR_REPLACE)
+    {
+        internal_flag = PVFS_XATTR_REPLACE;
+    }
+    /* Attribute must not exist */
+    else if (flags & XATTR_CREATE)
+    {
+        internal_flag = PVFS_XATTR_CREATE;
+    }
+    return pvfs2_inode_setxattr(inode, name, value, size, internal_flag);
+}
+
+ssize_t pvfs2_getxattr(struct dentry *dentry, const char *name,
+		         void *buffer, size_t size)
+{
+    struct inode *inode = dentry->d_inode;
+    return pvfs2_inode_getxattr(inode, name, buffer, size);
+}
+
+ssize_t pvfs2_listxattr(struct dentry *dentry, char *buffer, size_t size)
+{
+    /* 
+     * FIXME: The reason I am returning this value here is because 
+     * we need server-side support for this, and I need to talk
+     * to Walt and RobR about this...
+     * Should not be too hard, but nonetheless a pain to get this
+     * right.
+     */
+    return -EOPNOTSUPP;
+}
+
+int pvfs2_removexattr(struct dentry *dentry, const char *name)
+{
+    struct inode *inode = dentry->d_inode;
+    return pvfs2_inode_removexattr(inode, name);
+}
+
+#endif
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 expandtab
+ */
