78c19c8a241305c4ac86c7a0e05e67f2772546cb
no author <devel@lists.orangefs.org>
2006-02-23 23:29:27 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.2.32'.
    SVN revision 4916

--- test/correctness/pts/test-misc.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_MISC_H
-#define TEST_MISC_H
-
-int test_misc(MPI_Comm *comm, int rank,  char *buf, void *rawparams);
-
-#endif
--- test/correctness/pts/test-mix.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_MIX_H
-#define TEST_MIX_H
-
-int test_mix(MPI_Comm *comm, int rank,  char *buf, void *rawparams);
-
-#endif
