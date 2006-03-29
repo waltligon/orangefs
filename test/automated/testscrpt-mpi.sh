49ce9ab19d653c94691660665824815310899ba8
Rob Latham <robl@mcs.anl.gov>
2006-03-29 17:01:41 +0000

    ok, this has to be the last location missing a $CVS_TAG
    SVN revision 5020

--- test/automated/testscrpt-mpi.sh
+++ test/automated/testscrpt-mpi.sh
@@ -4,17 +4,17 @@
 # now is set up PAV and kick off some scripts
 # At this point we have:
 #
-#   ${PVFS2_DEST}/INSTALL-pvfs2          installation prefix for pvfs2
-#   ${PVFS2_DEST}/pvfs2/test/common/pav  location of pav scripts
-#   ${PVFS2_DEST}/soft/mpich2            installation prefix for mpich2
-#   $PVFS2_MOUNTPOINT                    mountpoint for pvfs2
+#   ${PVFS2_DEST}/INSTALL-pvfs2-${CVS_TAG}         installation prefix for pvfs2
+#   ${PVFS2_DEST}/pvfs2-${CVS_TAG}/test/common/pav location of pav scripts
+#   ${PVFS2_DEST}/soft/mpich2                     installation prefix for mpich2
+#   $PVFS2_MOUNTPOINT                             mountpoint for pvfs2
 
 if [ -z "$PVFS2_DEST" ] ; then
 	echo "do not run this script directly."
 	exit 1
 fi
 
-MPIIO_SCRIPTS=${PVFS2_DEST}/pvfs2/test/automated/mpiio-tests.d
+MPIIO_SCRIPTS=${PVFS2_DEST}/pvfs2-${CVS_TAG}/test/automated/mpiio-tests.d
 
 
 # we will only do multi processor tests if there's a pav config file 
