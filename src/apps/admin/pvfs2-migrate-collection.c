f96901398632e25c1fd1aff0ed4baf20277f8267
Rob Latham <robl@mcs.anl.gov>
2006-08-21 19:58:33 +0000

    merge latest fixes from HEAD (strlen instead of sizeof)
    SVN revision 5537

--- src/apps/admin/pvfs2-migrate-collection.c
+++ src/apps/admin/pvfs2-migrate-collection.c
@@ -941,7 +941,7 @@ static int translate_coll_eattr_0_0_1(
         }
         /* skip the version attribute- we don't want to copy that one */
         if(ret == 0 && strncmp(key.data, "trove-dbpf-version", 
-                               sizeof("trove-dbpf-version")) != 0)
+                               strlen("trove-dbpf-version")) != 0)
         {
             if(verbose) printf("VERBOSE Migrating collection eattr: %s\n", (char*)key.data);
 
@@ -1246,31 +1246,31 @@ static int translate_keyvals_0_0_1(
 
 static int translate_keyval_key_0_0_1(TROVE_keyval_s * keyval, DBT * db_key)
 {
-    if(!strncmp(db_key->data, "root_handle", sizeof("root_handle")))
+    if(!strncmp(db_key->data, "root_handle", strlen("root_handle")))
     {
         keyval->buffer = ROOT_HANDLE_KEYSTR;
-        keyval->buffer_sz = sizeof(ROOT_HANDLE_KEYSTR);
+        keyval->buffer_sz = strlen(ROOT_HANDLE_KEYSTR);
     }
-    else if(!strncmp(db_key->data, "dir_ent", sizeof("dir_ent")))
+    else if(!strncmp(db_key->data, "dir_ent", strlen("dir_ent")))
     {
         keyval->buffer = DIRECTORY_ENTRY_KEYSTR;
-        keyval->buffer_sz = sizeof(DIRECTORY_ENTRY_KEYSTR);
+        keyval->buffer_sz = strlen(DIRECTORY_ENTRY_KEYSTR);
     }
     else if(!strncmp(db_key->data, 
-                     "datafile_handles", sizeof("datafile_handles")))
+                     "datafile_handles", strlen("datafile_handles")))
     {
         keyval->buffer = DATAFILE_HANDLES_KEYSTR;
-        keyval->buffer_sz = sizeof(DATAFILE_HANDLES_KEYSTR);
+        keyval->buffer_sz = strlen(DATAFILE_HANDLES_KEYSTR);
     }
-    else if(!strncmp(db_key->data, "metafile_dist", sizeof("metafile_dist")))
+    else if(!strncmp(db_key->data, "metafile_dist", strlen("metafile_dist")))
     {
         keyval->buffer = METAFILE_DIST_KEYSTR;
-        keyval->buffer_sz = sizeof(METAFILE_DIST_KEYSTR);
+        keyval->buffer_sz = strlen(METAFILE_DIST_KEYSTR);
     }
-    else if(!strncmp(db_key->data, "symlink_target", sizeof("symlink_target")))
+    else if(!strncmp(db_key->data, "symlink_target", strlen("symlink_target")))
     {
         keyval->buffer = SYMLINK_TARGET_KEYSTR;
-        keyval->buffer_sz = sizeof(SYMLINK_TARGET_KEYSTR);
+        keyval->buffer_sz = strlen(SYMLINK_TARGET_KEYSTR);
     }
     else
     {
