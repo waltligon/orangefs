740ff8e34c2a8c5b4f6f4a40a09abdeb9e65987e
no author <devel@lists.orangefs.org>
2006-09-13 20:23:17 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.93.4'.
    SVN revision 5659

--- test/io/job/job-help.h
+++
@@ -1,9 +0,0 @@
-#ifndef __JOB_HELP_H
-#define __JOB_HELP_H
-
-#include <job.h>
-
-int block_on_job(job_id_t id, void** returned_user_ptr,
-	job_status_s* out_status_p, job_context_id context);
-
-#endif /* JOB_HELP_H */
