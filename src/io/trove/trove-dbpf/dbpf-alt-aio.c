fdb2d2eb43ee0b00f2c9bcefd8fc3b35d47d2928
Sam Lang <slang@mcs.anl.gov>
2007-03-14 16:36:43 +0000

    this one too
    SVN revision 6166

--- src/io/trove/trove-dbpf/dbpf-alt-aio.c
+++ src/io/trove/trove-dbpf/dbpf-alt-aio.c
@@ -191,13 +191,6 @@ static void* alt_lio_thread(void* foo)
     }
     else if(tmp_item->cb_p->aio_lio_opcode == LIO_WRITE)
     {
-        gossip_debug(GOSSIP_BSTREAM_DEBUG,
-                     "[alt-aio]: pwrite: cb_p: %p, "
-                     "fd: %d, bufp: %p, size: %zd off:%lld\n",
-                     tmp_item->cb_p, tmp_item->cb_p->aio_fildes, 
-                     tmp_item->cb_p->aio_buf, tmp_item->cb_p->aio_nbytes,
-		     tmp_item->cb_p->aio_offset);
-
         ret = pwrite(tmp_item->cb_p->aio_fildes,
                      (const void*)tmp_item->cb_p->aio_buf,
                      tmp_item->cb_p->aio_nbytes,
