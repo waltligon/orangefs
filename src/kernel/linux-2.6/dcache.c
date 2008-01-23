2dbecb921f26db47d8c00ddbd8fd3bd0fda70d89
Phil Carns <carns@mcs.anl.gov>
2008-01-23 19:58:12 +0000

    merging latest dcache.c iteration from trunk
    SVN revision 6553

--- src/kernel/linux-2.6/dcache.c
+++ src/kernel/linux-2.6/dcache.c
@@ -16,8 +16,95 @@
 /* should return 1 if dentry can still be trusted, else 0 */
 static int pvfs2_d_revalidate_common(struct dentry* dentry)
 {
-    gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: invalidating dentry: %p\n", dentry);
-    return 0;
+    int ret = 0;
+    struct inode *inode = (dentry ? dentry->d_inode : NULL);
+    struct inode *parent_inode = NULL; 
+    pvfs2_kernel_op_t *new_op = NULL;
+    pvfs2_inode_t *parent = NULL;
+
+    gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: called on dentry %p.\n", dentry);
+
+    /* find parent inode */
+    if(dentry && dentry->d_parent)
+    {
+        gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: parent found.\n");
+        parent_inode = dentry->d_parent->d_inode;
+    }
+    else
+    {
+        gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: parent not found.\n");
+    }
+    
+    if (inode && parent_inode)
+    {
+        /* first perform a lookup to make sure that the object not only
+         * exists, but is still in the expected place in the name space 
+         */
+        if (!is_root_handle(inode))
+        {
+            gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: attempting lookup.\n");
+            new_op = op_alloc(PVFS2_VFS_OP_LOOKUP);
+            if (!new_op)
+            {
+                return 0;
+            }
+            new_op->upcall.req.lookup.sym_follow = PVFS2_LOOKUP_LINK_NO_FOLLOW;
+            parent = PVFS2_I(parent_inode);
+            if (parent && parent->refn.handle != PVFS_HANDLE_NULL && parent->refn.fs_id != PVFS_FS_ID_NULL)
+            {
+                new_op->upcall.req.lookup.parent_refn = parent->refn;
+            }
+            else
+            {
+#if defined(HAVE_IGET4_LOCKED) || defined(HAVE_IGET5_LOCKED)
+                gossip_lerr("Critical error: i_ino cannot be relied upon when using iget5/iget4\n");
+                op_release(new_op);
+                return 0;
+#endif
+                new_op->upcall.req.lookup.parent_refn.handle =
+                    get_handle_from_ino(parent_inode);
+                new_op->upcall.req.lookup.parent_refn.fs_id =
+                    PVFS2_SB(parent_inode->i_sb)->fs_id;
+            }
+            strncpy(new_op->upcall.req.lookup.d_name,
+                    dentry->d_name.name, PVFS2_NAME_LEN);
+
+            ret = service_operation(
+                new_op, "pvfs2_lookup", 
+                get_interruptible_flag(parent_inode));
+
+            if((new_op->downcall.status != 0) || 
+                    !match_handle(new_op->downcall.resp.lookup.refn.handle, inode))
+            {
+                gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: lookup failure or no match.\n");
+                op_release(new_op);
+                return(0);
+            }
+            
+            op_release(new_op);
+        }
+        else
+        {
+            gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: root handle, lookup skipped.\n");
+        }
+
+        /* now perform revalidation */
+        gossip_debug(GOSSIP_DCACHE_DEBUG, " (inode %llu)\n",
+                    llu(get_handle_from_ino(inode)));
+        gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_revalidate_common: calling pvfs2_internal_revalidate().\n");
+        ret = pvfs2_internal_revalidate(inode);
+    }
+    else
+    {
+        gossip_debug(GOSSIP_DCACHE_DEBUG, "\n");
+    }
+    return ret;
+}
+
+static int pvfs2_d_delete (struct dentry * dentry)
+{
+    gossip_debug(GOSSIP_DCACHE_DEBUG, "pvfs2_d_delete: called on dentry %p.\n", dentry);
+    return 1;
 }
 
 /* should return 1 if dentry can still be trusted, else 0 */
@@ -84,6 +171,7 @@ struct dentry_operations pvfs2_dentry_operations =
     .d_revalidate = pvfs2_d_revalidate,
     .d_hash = pvfs2_d_hash,
     .d_compare = pvfs2_d_compare,
+    .d_delete = pvfs2_d_delete,
 };
 
 /*
