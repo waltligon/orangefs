a50584ff3bb00d41e3c96f3f4572facf309bc34b
Phil Carns <carns@mcs.anl.gov>
2008-02-27 22:52:14 +0000

    reverting keyval_read_list() consolidation from getattr for the time being to simplify file stuffing testing/debugging
    SVN revision 6723

--- src/server/pvfs2-server.h
+++ src/server/pvfs2-server.h
@@ -377,6 +377,7 @@ struct PINT_server_getattr_op
     PVFS_fs_id fs_id;
     PVFS_ds_attributes dirdata_ds_attr;
     uint32_t attrmask;
+    PVFS_error* err_array;
     PVFS_ds_keyval_handle_info keyval_handle_info;
     PVFS_handle dirent_handle;
 };
