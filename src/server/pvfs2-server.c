ecf38f428cfef6fa1c22b044ed420501c74e7a9c
Phil Carns <carns@mcs.anl.gov>
2008-03-12 14:45:12 +0000

    yet another op field to remember to set when an sm transitions from an unexpected machine to a machine for a specific request type
    SVN revision 6778

--- src/server/pvfs2-server.c
+++ src/server/pvfs2-server.c
@@ -1588,6 +1588,7 @@ int server_state_machine_start(
     {
         s_op->req  = (struct PVFS_server_req *)s_op->decoded.buffer;
         ret = PINT_smcb_set_op(smcb, s_op->req->op);
+        s_op->op = s_op->req->op;
     }
     else
     {
