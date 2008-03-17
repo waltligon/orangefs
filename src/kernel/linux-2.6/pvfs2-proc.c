749dd5231cdccafb861fbafe771176dea289528f
Rob Latham <robl@mcs.anl.gov>
2008-03-17 18:40:03 +0000

    from HEAD by way of 2-7-branch: explicitly terminate ctl_table
    SVN revision 6813

--- src/kernel/linux-2.6/pvfs2-proc.c
+++ src/kernel/linux-2.6/pvfs2-proc.c
@@ -327,7 +327,7 @@ static ctl_table pvfs2_acache_table[] = {
         .proc_handler = &pvfs2_param_proc_handler,
         .extra1 = &acache_rec_extra,
     },
-    {0}
+    {.ctl_name = 0}
 };
 static ctl_table pvfs2_static_acache_table[] = {
     /* controls static acache timeout */
@@ -366,7 +366,7 @@ static ctl_table pvfs2_static_acache_table[] = {
         .proc_handler = &pvfs2_param_proc_handler,
         .extra1 = &static_acache_rec_extra,
     },
-    {0}
+    {.ctl_name = 0}
 };
 
 static ctl_table pvfs2_ncache_table[] = {
@@ -406,7 +406,7 @@ static ctl_table pvfs2_ncache_table[] = {
         .proc_handler = &pvfs2_param_proc_handler,
         .extra1 = &ncache_rec_extra
     },
-    {0}
+    {.ctl_name = 0}
 };
 static int acache_perf_count = PVFS2_PERF_COUNT_REQUEST_ACACHE;
 static int static_acache_perf_count = PVFS2_PERF_COUNT_REQUEST_STATIC_ACACHE;
@@ -436,7 +436,7 @@ static ctl_table pvfs2_pc_table[] = {
         .proc_handler = pvfs2_pc_proc_handler,
         .extra1 = &ncache_perf_count
     },
-    {0}
+    {.ctl_name = 0}
 };
 
 pvfs2_stats g_pvfs2_stats;
@@ -475,7 +475,7 @@ static ctl_table pvfs2_stats_table[] = {
         .mode     = 0444,
         .proc_handler = &proc_dointvec,
     },
-    {0}
+    {.ctl_name = 0}
 };
 
 static ctl_table pvfs2_table[] = {
@@ -569,7 +569,7 @@ static ctl_table pvfs2_table[] = {
         .mode = 0555,
         .child = pvfs2_stats_table
     },
-    {0}
+    {.ctl_name = 0}
 };
 static ctl_table fs_table[] = {
     {
@@ -578,7 +578,7 @@ static ctl_table fs_table[] = {
         .mode = 0555,
         .child = pvfs2_table
     },
-    {0}
+    {.ctl_name = 0}
 };
 #endif
 
