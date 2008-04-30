e9a73be06d499dc9ccbd11361d3493fb347bd04a
Scott Atchley <atchleyes@ornl.gov>
2008-04-30 12:40:25 +0000

    Revert default debug output to ERR and WARN only
    SVN revision 6971

--- src/io/bmi/bmi_mx/mx.h
+++ src/io/bmi/bmi_mx/mx.h
@@ -304,7 +304,7 @@ struct bmx_connreq
 
 #if BMX_DEBUG
 /* set the mask to the BMX_DB_* errors that you want gossip to report */
-#define BMX_DB_MASK (BMX_DB_ERR|BMX_DB_WARN|BMX_DB_ALL)
+#define BMX_DB_MASK (BMX_DB_ERR|BMX_DB_WARN)
 #define debug(lvl,fmt,args...)                                                 \
   do {                                                                         \
       if (lvl & BMX_DB_MASK) {                                                 \
