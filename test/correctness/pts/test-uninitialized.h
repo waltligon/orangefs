0f5c465e7a70dd3365cdde070dd2b94039d35259
no author <devel@lists.orangefs.org>
2009-10-07 18:44:00 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.6.1.4'.
    SVN revision 8099

--- test/correctness/pts/test-uninitialized.h
+++
@@ -1,9 +0,0 @@
-#ifndef INCLUDE_UNINITIALIZED_H
-#define INCLUDE_UNINITIALIZED_H
-
-int test_uninitialized(MPI_Comm * comm,
-		       int rank,
-		       char *buf,
-		       void *rawparams);
-
-#endif
