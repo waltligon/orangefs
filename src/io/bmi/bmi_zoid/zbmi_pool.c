be61f00c686372f93e9a5f626593847f12d7ad91
Kamil Iskra <iskra@mcs.anl.gov>
2009-11-10 19:29:22 +0000

    Prevent conflicts with MPI.
    SVN revision 8151

--- src/io/bmi/bmi_zoid/zbmi_pool.c
+++ src/io/bmi/bmi_zoid/zbmi_pool.c
@@ -5,6 +5,27 @@
 /* HAVE_MORECORE defaults to 0 if ONLY_MSPACES is used. */
 #define HAVE_MMAP 0
 
+/*
+ * Avoid duplicate symbol errors when compiling 
+ *  with ZeptoOS mpi compilers
+ */
+#pragma weak destroy_mspace
+#pragma weak mspace_independent_comalloc
+#pragma weak mspace_memalign
+#pragma weak mspace_mallinfo
+#pragma weak mspace_calloc
+#pragma weak mspace_max_footprint
+#pragma weak mspace_free
+#pragma weak mspace_mallopt
+#pragma weak mspace_independent_calloc
+#pragma weak create_mspace_with_base
+#pragma weak mspace_realloc
+#pragma weak mspace_malloc
+#pragma weak mspace_trim
+#pragma weak mspace_malloc_stats
+#pragma weak create_mspace
+#pragma weak mspace_footprint
+
 //#include "dlmalloc.h"
 #include "dlmalloc.c"
 
