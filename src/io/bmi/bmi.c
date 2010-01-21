c89a6b0e2ae16a3ad60b787d9629f4a3ec17331c
Sam Lang <slang@mcs.anl.gov>
2010-01-21 19:01:42 +0000

    Patch that gives expected/unexpected its own method_plan.  Fixes test_unexepected/test_context race.  From Dries Kimpe.
    SVN revision 8211

--- src/io/bmi/bmi.c
+++ src/io/bmi/bmi.c
@@ -126,12 +126,17 @@ static int active_method_count = 0;
 static gen_mutex_t active_method_count_mutex = GEN_MUTEX_INITIALIZER;
 
 static struct bmi_method_ops **active_method_table = NULL;
-static struct {
+
+struct method_usage_t {
     int iters_polled;  /* how many iterations since this method was polled */
     int iters_active;  /* how many iterations since this method had action */
     int plan;
     int flags;
-} *method_usage = NULL;
+};
+
+static struct method_usage_t * expected_method_usage = NULL;
+static struct method_usage_t * unexpected_method_usage = NULL;
+
 static const int usage_iters_starvation = 100000;
 static const int usage_iters_active = 10000;
 static int global_flags;
@@ -537,8 +542,11 @@ int BMI_finalize(void)
     free(known_method_table);
     known_method_count = 0;
 
-    if (method_usage)
-        free(method_usage);
+    if (expected_method_usage)
+        free(expected_method_usage);
+
+    if (unexpected_method_usage)
+       free(unexpected_method_usage);
 
     /* destroy the reference list */
     /* (side effect: destroys all method addresses as well) */
@@ -920,7 +928,8 @@ int BMI_testsome(int incount,
  * poll them all.  Return idle_time per method too.
  */
 static void
-construct_poll_plan(int nmeth, int *idle_time_ms)
+construct_poll_plan(struct method_usage_t * method_usage,
+      int nmeth, int *idle_time_ms)
 {
     int i, numplan;
 
@@ -998,11 +1007,12 @@ int BMI_testunexpected(int incount,
 
     *outcount = 0;
 
-    construct_poll_plan(tmp_active_method_count, &max_idle_time_ms);
+    construct_poll_plan(unexpected_method_usage,
+          tmp_active_method_count, &max_idle_time_ms);
 
     while (position < incount && i < tmp_active_method_count)
     {
-        if (method_usage[i].plan) {
+        if (unexpected_method_usage[i].plan) {
             ret = active_method_table[i]->testunexpected(
                 (incount - position), &tmp_outcount,
                 (&(sub_info[position])), max_idle_time_ms);
@@ -1014,9 +1024,9 @@ int BMI_testunexpected(int incount,
             }
             position += tmp_outcount;
             (*outcount) += tmp_outcount;
-            method_usage[i].iters_polled = 0;
+            unexpected_method_usage[i].iters_polled = 0;
             if (ret)
-                method_usage[i].iters_active = 0;
+                unexpected_method_usage[i].iters_active = 0;
         }
 	i++;
     }
@@ -1095,11 +1105,12 @@ int BMI_testcontext(int incount,
 	return(0);
     }
 
-    construct_poll_plan(tmp_active_method_count, &max_idle_time_ms);
+    construct_poll_plan(expected_method_usage,
+          tmp_active_method_count, &max_idle_time_ms);
 
     while (position < incount && i < tmp_active_method_count)
     {
-        if (method_usage[i].plan) {
+        if (expected_method_usage[i].plan) {
             ret = active_method_table[i]->testcontext(
                 incount - position, 
                 &out_id_array[position],
@@ -1117,9 +1128,9 @@ int BMI_testcontext(int incount,
             }
             position += tmp_outcount;
             (*outcount) += tmp_outcount;
-            method_usage[i].iters_polled = 0;
+            expected_method_usage[i].iters_polled = 0;
             if (ret)
-                method_usage[i].iters_active = 0;
+                expected_method_usage[i].iters_active = 0;
         }
 	i++;
     }
@@ -2024,6 +2035,28 @@ void bmi_method_addr_drop_callback (char* method_name)
     return;
 }
 
+
+/**
+ * Try to increase method_usage_t struct to include room for a new method.
+ */
+static int grow_method_usage (struct method_usage_t ** p, int newflags)
+{
+    struct method_usage_t * x = *p;
+    *p = malloc((active_method_count + 1) * sizeof(**p));
+    if (!*p) {
+        *p = x;
+        return 0;
+    }
+    if (active_method_count) {
+        memcpy(*p, x, active_method_count * sizeof(**p));
+        free(x);
+    }
+    memset(&((*p)[active_method_count]), 0, sizeof(**p));
+    (*p)[active_method_count].flags = newflags;
+
+    return 1;
+ }
+
 /*
  * Attempt to insert this name into the list of active methods,
  * and bring it up.
@@ -2074,18 +2107,16 @@ activate_method(const char *name, const char *listen_addr, int flags)
     }
     active_method_table[active_method_count] = meth;
 
-    x = method_usage;
-    method_usage = malloc((active_method_count + 1) * sizeof(*method_usage));
-    if (!method_usage) {
-        method_usage = x;
-        return -ENOMEM;
-    }
-    if (active_method_count) {
-        memcpy(method_usage, x, active_method_count * sizeof(*method_usage));
-        free(x);
-    }
-    memset(&method_usage[active_method_count], 0, sizeof(*method_usage));
-    method_usage[active_method_count].flags = meth->flags;
+    if (!grow_method_usage (&unexpected_method_usage, meth->flags))
+       return -ENOMEM;
+
+    /**
+     * If we run out of memory here, the unexpected_method_usage will be
+     * larger than strictly required but there is no memory leak.
+     */
+
+    if (!grow_method_usage (&expected_method_usage, meth->flags))
+       return -ENOMEM;
 
     ++active_method_count;
 
