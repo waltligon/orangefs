d4a006577eaf7f00added6a96243e3cbd178df93
Becky Ligon <ligon@clemson.edu>
2011-07-08 19:56:31 +0000

    Silenced compiler warnings.
    SVN revision 8926

--- src/apps/admin/pvfs2-perf-mon-snmp.c
+++ src/apps/admin/pvfs2-perf-mon-snmp.c
@@ -320,7 +320,7 @@ int main(int argc, char **argv)
             fflush(stdout);
             continue;
         }
-        fprintf(stdout, "%s\n%llu\n", returnType, returnValue);
+        fprintf(stdout, "%s\n%llu\n", returnType, llu(returnValue));
         fflush(stdout);
         /* return to top for next command */
     }
