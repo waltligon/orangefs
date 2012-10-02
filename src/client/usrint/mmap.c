0ef60a5a17f3e6cc805a4b84ba61176662d81b92
Walt Ligon <walt@clemson.edu>
2012-10-02 21:00:18 +0000

    changed a few castw from char to u_char, just to be sure.
    SVN revision 9510

--- src/client/usrint/mmap.c
+++ src/client/usrint/mmap.c
@@ -150,7 +150,8 @@ int pvfs_msync(void *start, size_t length, int flags)
     }
     qlist_for_each_entry_safe(mapl, temp, &maplist, link)
     {
-        if (mapl->mst <= start && (char *)mapl->mst + mapl->mlen >= (char *)start + length)
+        if (mapl->mst <= start &&
+            ((u_char *)mapl->mst + mapl->mlen) >= ((u_char *)start + length))
         {
             break;
         }
@@ -164,8 +165,10 @@ int pvfs_msync(void *start, size_t length, int flags)
     {
         /* the diff between start and mst is distance from */
         /* start of buffer, and distnace from original offset */
-        rc = pvfs_pwrite(mapl->mfd, start, length,
-                          mapl->moff + ((char *)start - (char *)mapl->mst));
+        rc = pvfs_pwrite(mapl->mfd,
+                         start,
+                         length,
+                         mapl->moff + ((u_char *)start - (u_char *)mapl->mst));
     }
     return rc;
 }
