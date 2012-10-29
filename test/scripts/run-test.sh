87c640128ac1a8a1ac1c12d46840a0e7f67be6ea
James Burton <jburto2@clemson.edu>
2012-10-29 19:13:27 +0000

    SVN revision 9598

--- test/scripts/run-test.sh
+++ test/scripts/run-test.sh
@@ -6,9 +6,9 @@ mkdir ~/test2
 cd ~/test2
 echo svn export --force -q http://www.orangefs.org/svn/orangefs/${SVNBRANCH}/test/run-nightly-setup
 svn export --force -q http://www.orangefs.org/svn/orangefs/${SVNBRANCH}/test/run-nightly-setup
-./run-nightly-setup testingjdb
+./run-nightly-setup ${SVNBRANCH}
 cd ~/test2/pvfs2/test
 ls
-./run-nightly testingjdb
+./run-nightly ${SVNBRANCH}
 exit
 exit
