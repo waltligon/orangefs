c76b052327b4b8df62216f18d9fe8066d876f85b
James Burton <jburto2@clemson.edu>
2012-10-29 19:17:54 +0000

    SVN revision 9599

--- test/scripts/run-test.sh
+++ test/scripts/run-test.sh
@@ -4,11 +4,11 @@ script runtest.txt
 export LD_LIBRARY_PATH=/opt/db4/lib
 mkdir ~/test2
 cd ~/test2
-echo svn export --force -q http://www.orangefs.org/svn/orangefs/branches/${SVNBRANCH}/test/run-nightly-setup
-svn export --force -q http://www.orangefs.org/svn/orangefs/branches/${SVNBRANCH}/test/run-nightly-setup
-./run-nightly-setup testingjdb
+echo svn export --force -q http://www.orangefs.org/svn/orangefs/${SVNBRANCH}/test/run-nightly-setup
+svn export --force -q http://www.orangefs.org/svn/orangefs/${SVNBRANCH}/test/run-nightly-setup
+./run-nightly-setup ${SVNBRANCH}
 cd ~/test2/pvfs2/test
 ls
-./run-nightly testingjdb
+./run-nightly ${SVNBRANCH}
 exit
 exit
