56915fd892769edd820f144ac7b2d40908cb819e
James Burton <jburto2@clemson.edu>
2012-12-04 19:46:35 +0000

    Updated rpm-based prepare scripts to get prebuilt torque RPMs from devorange
    SVN revision 9723

--- test/scripts/prepare-cloud-fedora17.sh
+++ test/scripts/prepare-cloud-fedora17.sh
@@ -21,15 +21,11 @@ sudo make install &> db4install.out
 
 echo "Installing TORQUE..."
 
-echo '[atrpms]
-name=Fedora Core $releasever - $basearch - ATrpms
-baseurl=http://dl.atrpms.net/el$releasever-$basearch/atrpms/stable
-gpgkey=http://ATrpms.net/RPM-GPG-KEY.atrpms
-gpgcheck=1' > ~/atrpms.repo
+echo "Installing TORQUE..."
 
-sudo cp ~/atrpms.repo /etc/yum.repos.d/
-sudo rpm --import http://packages.atrpms.net/RPM-GPG-KEY.atrpms &> importkey.out
-sudo yum -y install torque torque-server | tee torque.out
+wget -q -np -r -nd http://devorange.clemson.edu/pvfs/fedora17/RPMS/x86_64/
+
+rpm -i torque*.rpms
 
 exit
 exit
