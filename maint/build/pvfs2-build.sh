fb2b3b91725d20e62542427dad00423b93dee556
James Burton <jburto2@clemson.edu>
2013-02-14 21:15:46 +0000

    More changes to LD_PRELOAD. Remove userint from compilation to test previous funcitonality
    SVN revision 9999

--- maint/build/pvfs2-build.sh
+++ maint/build/pvfs2-build.sh
@@ -12,7 +12,8 @@ tarballurl=http://www.mcs.anl.gov/hpio/pvfs2-0.0.6.tar.gz
 cvsroot=:pserver:anonymous@cvs.parl.clemson.edu:/anoncvs 
 # specify extra configure options here; for now we disable karma because
 # of all the gtk warnings
-configureopts="$PVFS2_CONFIGOPTS --enable-shared --enable-ucache --enable-strict --disable-karma --with-db=/opt/db4"
+#configureopts="$PVFS2_CONFIGOPTS --enable-shared --enable-ucache --enable-strict --disable-karma --with-db=/opt/db4"
+configureopts="$PVFS2_CONFIGOPTS --enable-strict --disable-karma --with-db=/opt/db4"
 
 
 #
