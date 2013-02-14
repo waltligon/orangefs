e72538a116252a615ce033c5259bf25cb1ce9ff9
Walt Ligon <walt@clemson.edu>
2013-02-14 21:46:24 +0000

    SVN revision 10002

---
+++ src/common/sidcache/sid.h
@@ -0,0 +1,25 @@
+/*
+ * (C) 2012 Clemson University
+ *
+ * See COPYING in top-level directory.
+*/
+
+#ifndef SID_H
+#define SID_H 1
+
+#include "pvfs-sid.h"
+
+int SID_initialize(void);
+int SID_save(void);
+int SID_finalize(void);
+
+#endif /* SID_H */
+
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 expandtab
+ */
