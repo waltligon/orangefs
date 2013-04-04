044f1b6d2e23bb5169c82d4a84c6c8f3f81ecdad
Sam Sampson <sampson@clemson.edu>
2013-04-04 20:54:22 +0000

    Added capability cache for security modes.
    SVN revision 10125

--- src/common/security/capcache.h
+++ src/common/security/capcache.h
@@ -1,20 +1,31 @@
-#include <pvfs2-config.h>
-#ifdef OFS_CAPCACHE_ENABLE
+/*
+ * (C) 2013 Clemson University and Omnibond Systems LLC
+ *
+ * Capability cache declarations
+ *
+ * See COPYING in top-level directory.
+ */
+
+#include "pvfs2-config.h"
+
+#ifdef ENABLE_CAPCACHE
+
 #ifndef _CAPCACHE_H
 #define _CAPCACHE_H
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
-#include <pvfs2-types.h>
-#include <llist.h>
 
-#define DBG 1
+#include "pvfs2-types.h"
+#include "llist.h"
+
 /* Macro that helps print function info */
-#if DBG == 1
-    #define PFI gossip_debug(GOSSIP_CAPCACHE_DEBUG, "{%s}\n", __func__);
-#else
-    #define PFI
-#endif
+#define CAPCACHE_ENTER_FN() do { \
+                                gossip_debug(GOSSIP_CAPCACHE_DEBUG, "%s: enter\n", __func__); \
+                            } while (0)
+#define CAPCACHE_EXIT_FN()  do { \
+                                gossip_debug(GOSSIP_CAPCACHE_DEBUG, "%s: exit\n", __func__); \
+                            } while (0)
 
 /* Capcache Macros - Adjust accordingly */
 /* Key type, so it can be swapped easily */
@@ -27,9 +38,11 @@
 #define CAPCACHE_SIZE_CAP       (1024 * 1024 * 64)
 /* Number of indexes in our chained hash-table */
 #define CAPCACHE_HASH_MAX       128
+/* Frequency (in lookups) to debugging stats */
+#define CAPCACHE_STATS_FREQ     1000
 
 /* Capability Cache Locking */
-#define LOCK_TYPE 0
+#define LOCK_TYPE 3
 #if (LOCK_TYPE == 0) /* No Locking */
     #define capcache_lock_t uint64_t
 #elif (LOCK_TYPE == 1)
@@ -39,6 +52,7 @@
     #include <pthread.h>
     #define capcache_lock_t pthread_spinlock_t
 #elif (LOCK_TYPE == 3)
+    #include "gen-locks.h"
     #define capcache_lock_t gen_mutex_t
 #endif
 #define LOCK_SIZE sizeof(capcache_lock_t)
@@ -69,6 +83,7 @@ typedef struct capcache_stats_s {
     uint64_t hits;
     uint64_t misses;
     uint64_t removed;
+    uint64_t expired;
     uint64_t entry_cnt;
     PVFS_size cache_size;
 } capcache_stats_t;
@@ -81,8 +96,8 @@ int PINT_capcache_init(void); /**/
 int PINT_capcache_finalize(void); /**/
 struct capcache_entry_s * PINT_capcache_lookup_entry(PVFS_capability *cap);
 int PINT_capcache_insert_entry(const PVFS_capability * cap); /**/
-int PINT_capcache_remove_entry(void); /**/
+int PINT_capcache_remove_entry(struct capcache_entry_s *entry); /**/
 /* End of Externally Visible Capability Cache API */
 
 #endif /* _CAPCACHE_H */
-#endif /* OFS_CAPCACHE_ENABLE */
+#endif /* ENABLE_CAPCACHE */
