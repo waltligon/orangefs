bead25f2ec9c0c39b3f09680eaf20e91111ec0e5
Elaine Quarles <elaine@clemson.edu>
2013-04-04 19:59:05 +0000

    Comment out call to pvfs2-set-debugmask. Fills up a disk quickly.
    SVN revision 10124

--- test/automated/testfunctions.sh
+++ test/automated/testfunctions.sh
@@ -260,10 +260,10 @@ start_pvfs2() {
 	#if [ $? -ne 0 -a $do_vfs -eq 0 ] ; then
 	#	export PVFS2TAB_FILE=${PVFS2_DEST}/pvfs2tab
 	#fi
-	#turn on debugging on each server
 	export PVFS2TAB_FILE=${PVFS2_DEST}/pvfs2tab
-	echo "....setting server-side debug mask"
-	INSTALL-pvfs2-${CVS_TAG}/bin/pvfs2-set-debugmask -m ${PVFS2_MOUNTPOINT} "all"	
+	#turn on debugging on each server
+	#echo "....setting server-side debug mask"
+	#INSTALL-pvfs2-${CVS_TAG}/bin/pvfs2-set-debugmask -m ${PVFS2_MOUNTPOINT} "all"	
 }
 
 setup_pvfs2() {
