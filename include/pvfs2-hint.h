bd2973ed081e1f608ec58564eaa9bc51d98ba174
Walt Ligon <walt@clemson.edu>
2013-08-16 20:45:42 +0000

    several imporant edits, including adding changes from over a year ago that got lot or were never included - the rewrite of the random and round-robin layouts.  and Hint capability for library code. Fixed four small but important bugs: wrong variable use, missing deref, a problem in is_pvfs_path when using KMOUNT, failing to actually close an open directory in closedir.  Plus lots of format editing included in all of that.
    SVN revision 10353

--- include/pvfs2-hint.h
+++ include/pvfs2-hint.h
@@ -28,12 +28,12 @@ typedef struct PVFS_hint_s *PVFS_hint;
 #define PVFS_HINT_NULL NULL
 
 int PVFS_hint_add(PVFS_hint *hint,
-                  const char *type,
+                  const char *name,
                   int length,
                   void *value);
 
 int PVFS_hint_replace(PVFS_hint *hint,
-                      const char *type,
+                      const char *name,
                       int length,
                       void *value);
 
@@ -42,7 +42,7 @@ int PVFS_hint_copy(PVFS_hint old_hint, PVFS_hint *new_hint);
 void PVFS_hint_free(PVFS_hint hint);
 
 /* check to see if a hint has already been added */
-int PVFS_hint_check(PVFS_hint *hints, const char *type);
+int PVFS_hint_check(PVFS_hint *hints, const char *name);
 
 /* check to see if a hint should be transferred to the server */
 int PVFS_hint_check_transfer(PVFS_hint *hints);
