2d100d51336505d72a18e9491577e5893d944b8e
Martin Brandenburg <martin@omnibond.com>
2014-02-10 18:13:37 +0000

    Fix problems from merge and other things.
    SVN revision 10656

--- src/server/pvfs2-server-req.c
+++ src/server/pvfs2-server-req.c
@@ -121,12 +121,12 @@ struct PINT_server_req_entry PINT_server_req_table[] =
     /* 46 */ {PVFS_SERV_MGMT_CREATE_ROOT_DIR, &pvfs2_mgmt_create_root_dir_params},
     /* 47 */ {PVFS_SERV_MGMT_SPLIT_DIRENT, &pvfs2_mgmt_split_dirent_params},
     /* 48 */ {PVFS_SERV_ATOMICEATTR, &pvfs2_atomic_eattr_params},
-    /* 49 */ {PVFS_SERV_TREE_GETATTR, &pvfs2_tree_getattr_params}
+    /* 49 */ {PVFS_SERV_TREE_GETATTR, &pvfs2_tree_getattr_params},
+    /* 50 */ {PVFS_SERV_MGMT_PROC_START, &pvfs2_mgmt_proc_start_params},
+    /* 51 */ {PVFS_SERV_MGMT_PROC_STOP, &pvfs2_mgmt_proc_stop_params},
 #ifdef ENABLE_SECURITY_CERT    
-    /* 50 */ ,{PVFS_SERV_MGMT_GET_USER_CERT, &pvfs2_get_user_cert_params},
-    /* 51 */ {PVFS_SERV_MGMT_GET_USER_CERT_KEYREQ, &pvfs2_get_user_cert_keyreq_params},
-    /* 52 */ {PVFS_SERV_MGMT_PROC_START, &pvfs2_mgmt_proc_start_params},
-    /* 53 */ {PVFS_SERV_MGMT_PROC_STOP, &pvfs2_mgmt_proc_stop_params}
+    /* 52 */ ,{PVFS_SERV_MGMT_GET_USER_CERT, &pvfs2_get_user_cert_params},
+    /* 53 */ {PVFS_SERV_MGMT_GET_USER_CERT_KEYREQ, &pvfs2_get_user_cert_keyreq_params},
 #endif
 };
 
