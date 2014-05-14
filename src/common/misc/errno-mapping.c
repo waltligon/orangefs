8b671a2a051f2caf604b50fe4eb2732ed08724af
Martin Brandenburg <martin@omnibond.com>
2014-05-14 16:12:52 +0000

    Rework PVFS_strerror_r to include error class and not overrun its buffer. Rework PVFS_perror and PVFS_perror_gossip to use PVFS_strerror_r. Also abort() if called after success.
    SVN revision 10815

--- src/common/misc/errno-mapping.c
+++ src/common/misc/errno-mapping.c
@@ -22,8 +22,6 @@
 #define EHOSTDOWN  112
 #endif
 
-#define MAX_PVFS_STRERROR_LEN 256
-
 /* static global controls whether pvfs_sys calls print user errors */
 static int pvfs_perror_gossip_silent = 0;
 
@@ -31,94 +29,123 @@ static int pvfs_perror_gossip_silent = 0;
 DECLARE_ERRNO_MAPPING_AND_FN();
 
 /**
-  the pvfs analog to strerror_r that handles PVFS_error codes as well
-  as errno error codes
-*/
+ * The PVFS analog to strerror_r that handles PVFS_error codes as well as errno
+ * error codes. The fact that this does not ever fail is used in functions
+ * below but should not be relied on in other modules.
+ */
 int PVFS_strerror_r(int errnum, char *buf, int n)
 {
-    int ret = 0, limit, map_err;
-
-    limit = PVFS_util_min(n, MAX_PVFS_STRERROR_LEN);    
-
-    map_err = PVFS_get_errno_mapping(errnum);
-
-    if (IS_PVFS_NON_ERRNO_ERROR(abs(errnum)))
+    size_t i = 0, r;
+    int map_err;
+    int class;
+    map_err = PVFS_get_errno_mapping(-errnum);
+    if (errnum >= 0)
     {
-        snprintf(buf, limit, "%s", PINT_non_errno_strerror_mapping[map_err]);
+        gossip_err("PVFS_strerror_r: called with positive errnum. "
+                "please fix in caller.\n");
+        abort();
+    }
+
+    if (IS_PVFS_NON_ERRNO_ERROR(-errnum))
+    {
+        r = snprintf(buf+i, n-i,
+                "%s", PINT_non_errno_strerror_mapping[map_err]);
+        if (i+r <= n) {
+            i += r;
+        }
     }
     else
     {
-
-#if defined(__DARWIN__)
-        ret = strerror_r(map_err, buf, (size_t)limit);
-#elif defined(HAVE_GNU_STRERROR_R) || defined(_GNU_SOURCE)
-        char *tmpbuf = strerror_r(map_err, buf, limit);
-        if (tmpbuf && (strcmp(tmpbuf, buf)))
+#if defined(HAVE_GNU_STRERROR_R) || defined(_GNU_SOURCE)
+        char *tmp;
+        size_t tmplen;
+        tmp = strerror_r(map_err, buf+i, n-i);
+        tmplen = strlen(tmp);
+        if (strcmp(tmp, buf+i) == 0)
         {
-            limit = PVFS_util_min(limit, strlen(tmpbuf)+1);
-            strncpy(buf, tmpbuf, (size_t)limit);
+            /* If there wasn't a complete copy due to lack of space,
+             * this will not run. */
+            i += tmplen;
         }
-        ret = (tmpbuf ? 0 : -1);
+        else
+        {
+            strncpy(buf+i, tmp, n-i);
+            if (tmplen >= n-i)
+            {
+                i += n-i;
+            }
+            else
+            {
+                i += tmplen;
+            }
+        }
 #elif defined(WIN32)
-        ret = (int) strerror_s(buf, (size_t) limit, map_err);
-#else 
-        ret = (int)strerror_r(map_err, buf, (size_t)limit);
+        if (strerror_s(buf, (size_t)limit, map_err) != 0)
+        {
+            r = snprintf(buf+i, n-i, "could not lookup system error message");
+            if (i+r <= n) {
+                i += r;
+            }
+        }
+#else
+        if (strerror_r(map_err, buf+i, n-i) != 0)
+        {
+            r = snprintf(buf+i, n-i, "could not lookup system error message");
+            if (i+r <= n) {
+                i += r;
+            }
+        }
 #endif
     }
 
-    return ret;
+    class = PVFS_ERROR_CLASS(-errnum);
+    switch (class)
+    {
+    case PVFS_ERROR_BMI:
+        r = snprintf(buf+i, n-i, " (BMI)");
+        break;
+    case PVFS_ERROR_TROVE:
+        r = snprintf(buf+i, n-i, " (TROVE)");
+        break;
+    case PVFS_ERROR_FLOW:
+        r = snprintf(buf+i, n-i, " (FLOW)");
+        break;
+    case PVFS_ERROR_SM:
+        r = snprintf(buf+i, n-i, " (SM)");
+        break;
+    case PVFS_ERROR_SCHED:
+        r = snprintf(buf+i, n-i, " (SCHED)");
+        break;
+    case PVFS_ERROR_CLIENT:
+        r = snprintf(buf+i, n-i, " (CLIENT)");
+        break;
+    case PVFS_ERROR_DEV:
+        r = snprintf(buf+i, n-i, " (DEV)");
+        break;
+    default:
+        r = snprintf(buf+i, n-i, " (no class)");
+        break;
+    }
+    if (i+r <= n) {
+        i += r;
+    }
+
+    return 0;
 }
+
+#define MAX_PVFS_STRERROR_LEN 256
 
 /** PVFS_perror()
  *
  * prints a message on stderr, consisting of text argument followed by
- * a colon, space, and error string for the given retcode.  NOTE: also
- * prints a warning if the error code is not in a pvfs2 format and
- * assumes errno
+ * a colon, space, and error string for the given retcode.
  */
 void PVFS_perror(const char *text, int retcode)
 {
-    /* TODO: test 
-    if (IS_PVFS_NON_ERRNO_ERROR(-retcode))
-    {
-        char buf[MAX_PVFS_STRERROR_LEN] = {0};
-        int index = PVFS_get_errno_mapping(retcode);
-
-        snprintf(buf, MAX_PVFS_STRERROR_LEN, "%s: %s (error class: %d)\n", 
-                 text, PINT_non_errno_strerror_mapping[index], 
-                 PVFS_ERROR_CLASS(-retcode));
-        fprintf(stderr, "%s", buf);
-    }
-    else if (IS_PVFS_ERROR(-retcode))
-    {
-        char buf[MAX_PVFS_STRERROR_LEN] = {0};
-
-        fprintf(stderr, "%s: %s (error class: %d)\n", text,
-                strerror(PVFS_ERROR_TO_ERRNO(-retcode)),
-                PVFS_ERROR_CLASS(-retcode));
-    }
-    else
-    {
-        fprintf(stderr, "Warning: non PVFS2 error code (%d):\n",
-                -retcode);
-        fprintf(stderr, "%s: %s\n", text, strerror(-retcode));
-    }
-    */
-    char buf[MAX_PVFS_STRERROR_LEN] = {0};
-    int ret;
-
-    ret = PVFS_strerror_r(retcode, buf, MAX_PVFS_STRERROR_LEN);
-    if (ret != 0)
-    {
-        fprintf(stderr, "%s: (no message: %d) code: %d\n", text, errno,
-                retcode);
-        return;
-    }
-
-    fprintf(stderr, "%s: %s (error class: %d)\n", text, buf, 
-            PVFS_ERROR_CLASS(abs(retcode)));
-
-    return;
+    char buf[MAX_PVFS_STRERROR_LEN];
+    /* PVFS_strerror_r does not fail. */
+    PVFS_strerror_r(retcode, buf, MAX_PVFS_STRERROR_LEN);
+    fprintf(stderr, "%s: %s\n", text, buf);
 }
 
 /** silences user error messages from system interface calls
@@ -144,46 +171,13 @@ void PVFS_perror_gossip_verbose(void)
  */
 void PVFS_perror_gossip(const char *text, int retcode)
 {
-    if (pvfs_perror_gossip_silent)
+    char buf[MAX_PVFS_STRERROR_LEN];
+    if (!pvfs_perror_gossip_silent)
     {
-        return;
+        /* PVFS_strerror_r does not fail. */
+        PVFS_strerror_r(retcode, buf, MAX_PVFS_STRERROR_LEN);
+        gossip_err("%s: %s\n", text, buf);
     }
-    /* TODO: test
-    if (IS_PVFS_NON_ERRNO_ERROR(-retcode))
-    {
-        char buf[MAX_PVFS_STRERROR_LEN] = {0};
-        int index = PVFS_get_errno_mapping(-retcode);
-
-        snprintf(buf,MAX_PVFS_STRERROR_LEN,"%s: %s\n",text,
-                 PINT_non_errno_strerror_mapping[index]);
-        gossip_err("%s", buf);
-    }
-    else if (IS_PVFS_ERROR(-retcode))
-    {
-        gossip_err("%s: %s\n", text,
-                   strerror(PVFS_ERROR_TO_ERRNO(-retcode)));
-    }
-    else
-    {
-        gossip_err("Warning: non PVFS2 error code (%d):\n", -retcode);
-        gossip_err("%s: %s\n", text, strerror(-retcode));
-    }
-    */
-    char buf[MAX_PVFS_STRERROR_LEN] = {0};
-    int ret;
-
-    ret = PVFS_strerror_r(retcode, buf, MAX_PVFS_STRERROR_LEN);
-    if (ret != 0)
-    {
-        gossip_err("%s: (no message: %d) code: %d\n", text, errno,
-                retcode);
-        return;
-    }
-
-    gossip_err("%s: %s (error class: %d)\n", text, buf, 
-            PVFS_ERROR_CLASS(abs(retcode)));
-
-    return;
 }
 
 /*
