c290d50a8b73aa6897049a6cbe7667812279ad0c
Bhargav Golla <bgolla@clemson.edu>
2014-07-13 23:26:22 +0000

    pvfs2-stat changed
    SVN revision 11093

--- src/apps/admin/pvfs2-stat.c
+++ src/apps/admin/pvfs2-stat.c
@@ -19,6 +19,7 @@
 
 #include "pvfs2.h"
 #include "pvfs2-internal.h"
+#include "pvfs2-usrint.h"
 
 /* We need to set some limit, I suppose */
 #define MAX_NUM_FILES 100 
@@ -42,28 +43,19 @@ static int parse_args(int argc, char** argv, struct options * opts);
 static void enable_verbose(struct options * opts);
 static void enable_dereference(struct options * opts);
 static int do_stat(const char             * pszFile,
-                   const char             * pszRelativeFile, 
-                   const PVFS_fs_id         fs_id, 
-                   const PVFS_credential  * credentials,
                    const struct options   * opts);
-void print_stats(const PVFS_object_ref * ref,
-                 const char            * pszName,
-                 const char            * pszRelativeName,
-                 const PVFS_sys_attr   * attr);
+void print_stats(const char  *pszName,
+                 struct stat *stat_buf);
 
 int main(int argc, char **argv)
 {
    int               ret          = -1,
                      ret_agg      =  0,
                      i            =  0;
-   char           ** ppszPvfsPath = NULL;
-   PVFS_fs_id     *  pfs_id       = NULL;
-   PVFS_credential   credentials;
    struct options    user_opts;
 
    /* Initialize any memory */
    memset(&user_opts,   0, sizeof(user_opts));
-   memset(&credentials, 0, sizeof(credentials));
    
    ret = parse_args(argc, argv, &user_opts);
    if(ret < 0)
@@ -78,37 +70,6 @@ int main(int argc, char **argv)
       fprintf(stdout, "Starting pvfs2-stat\n");
    }
    
-   /* Allocate space to hold the relative pvfs2 path & fs_id for each 
-    * requested file 
-    */
-   ppszPvfsPath = (char **)calloc(user_opts.nNumFiles, sizeof(char *));
-   
-   if(ppszPvfsPath == NULL)
-   {
-      fprintf(stderr, "Unable to allocate memory\n");
-      return(-1);
-   }
-   
-    /* Allocate enough space to hold file system id for each directory */
-   pfs_id = (PVFS_fs_id *)calloc(user_opts.nNumFiles, sizeof(PVFS_fs_id));
-   
-   if(pfs_id == NULL)
-   {
-      fprintf(stderr, "Unable to allocate memory\n");
-      return(-1);
-   }
-   
-   
-   for(i = 0; i < user_opts.nNumFiles; i++)
-   {
-      ppszPvfsPath[i] = (char *)calloc(PVFS_NAME_MAX, sizeof(char));
-      if(ppszPvfsPath[i] == NULL)
-      {
-         fprintf(stderr, "Unable to allocate memory\n");
-         return(-1);
-      }
-   }
-
    ret = PVFS_util_init_defaults();
    if(ret < 0)
    {
@@ -116,36 +77,9 @@ int main(int argc, char **argv)
       return(-1);
    }
    
-   /* Let's verify that all the given files reside on a PVFS2 filesytem */
-   for(i = 0; i < user_opts.nNumFiles; i++)
-   {
-      ret = PVFS_util_resolve(user_opts.pszFiles[i], 
-                              &pfs_id[i], 
-                              ppszPvfsPath[i], 
-                              PVFS_NAME_MAX);
-
-      if (ret < 0)
-      {
-         fprintf(stderr, "Error: could not find file system for %s\n", 
-                 user_opts.pszFiles[i]);
-         return(-1);
-      }
-   }
-
-   /* We will re-use the same credentials for each call */
-   ret = PVFS_util_gen_credential_defaults(&credentials);
-   if (ret < 0)
-   {
-       PVFS_perror("PVFS_util_gen_credential_defaults", ret);
-       return(-1);
-   }
-
    for(i = 0; i < user_opts.nNumFiles; i++)
    {
       ret = do_stat(user_opts.pszFiles[i], 
-                    ppszPvfsPath[i], 
-                    pfs_id[i], 
-                    &credentials,
                     &user_opts);
       if(ret != 0)
       {
@@ -162,91 +96,34 @@ int main(int argc, char **argv)
       free(user_opts.pszFiles);
    }
    
-   if(ppszPvfsPath != NULL)
-   {
-       for(i=0;i<user_opts.nNumFiles;i++)
-       {
-          if(ppszPvfsPath[i] != NULL)
-          {
-             free(ppszPvfsPath[i]);
-          }
-       }
-   
-      free(ppszPvfsPath);
-   }
-   
-   if(pfs_id != NULL)
-   {
-      free(pfs_id);
-   }
-
    return(ret_agg);
 }
 
 static int do_stat(const char             * pszFile,
-                   const char             * pszRelativeFile, 
-                   const PVFS_fs_id         fs_id, 
-                   const PVFS_credential  * credentials,
                    const struct options   * opts)
 {
-   int                  ret = 0;
-   PVFS_sysresp_lookup  lk_response;
-   PVFS_object_ref      ref;
-   PVFS_sysresp_getattr getattr_response;
-
-   /* Initialize memory */
-   memset(&lk_response,     0, sizeof(lk_response));
-   memset(&ref,             0, sizeof(ref));
-   memset(&getattr_response,0, sizeof(getattr_response));
-   
-
+    int                  ret = 0;
+    struct stat stat_buf;
    /* Do we want to follow if the file is a symbolic link */
+    //TODO: Is this usage of stat correct?
    if(opts->nFollowLink)
    {
-      ret = PVFS_sys_lookup(fs_id, 
-                            (char *) pszRelativeFile, 
-                            (PVFS_credential *) credentials, 
-                            &lk_response, 
-                            PVFS2_LOOKUP_LINK_FOLLOW, NULL);
+        ret = pvfs_stat(pszFile, &stat_buf);
    }
    else
    {
-      ret = PVFS_sys_lookup(fs_id, 
-                            (char *) pszRelativeFile, 
-                            (PVFS_credential *) credentials, 
-                            &lk_response, 
-                            PVFS2_LOOKUP_LINK_NO_FOLLOW, NULL);
+        ret = pvfs_lstat(pszFile, &stat_buf);
    }
    
-   if(ret < 0)
-   {
-      if(opts->nVerbose)
-      {
-         fprintf(stderr, "PVFS_sys_lookup call on [%s]\n", pszRelativeFile);
-      }
-      PVFS_perror("PVFS_sys_lookup", ret);
-      return -1;
-   }
-
-   ref.handle = lk_response.ref.handle;
-   ref.fs_id  = fs_id;
-   
-   ret = PVFS_sys_getattr(ref, 
-                          PVFS_ATTR_SYS_ALL_NOHINT,
-                          (PVFS_credential *) credentials, 
-                          &getattr_response, NULL);
-
    if(ret < 0)
    {                          
-      PVFS_perror("PVFS_sys_getattr", ret);
+      perror("pvfs_stat");
       return -1;
    }
 
    /* Display the attributes for the file */
-   print_stats(&ref,
-               pszFile, 
-               pszRelativeFile, 
-               &(getattr_response.attr));
+   print_stats(pszFile, 
+               &stat_buf);
    
    return(0);
 }
@@ -375,145 +252,97 @@ static void enable_dereference(struct options * opts)
    opts->nFollowLink = 1;  
 }
 
-void print_stats(const PVFS_object_ref * ref,
-                 const char            * pszName,
-                 const char            * pszRelativeName,
-                 const PVFS_sys_attr   * attr)
+void print_stats(const char  * pszName,
+                 struct stat *stat_buf)
 {
-   char a_time[100] = "", 
-        m_time[100] = "",  
-        c_time[100] = "";
-   struct passwd * user;
-   struct group  * group;
+    char a_time[100] = "", 
+         m_time[100] = "",  
+         c_time[100] = "";
+    struct passwd * user;
+    struct group  * group;
 
-   fprintf(stdout, "-------------------------------------------------------\n");
-   fprintf(stdout, "  File Name     : %s\n",  pszName);
-   fprintf(stdout, "  Relative Name : %s\n",  pszRelativeName);
-   fprintf(stdout, "  fs ID         : %d\n",  ref->fs_id);
-   fprintf(stdout, "  Handle        : %llu\n", llu(ref->handle));
-   fprintf(stdout, "  Mask          : %o\n",  attr->mask);
-   if(attr->mask & PVFS_ATTR_SYS_PERM)
-   {
-      fprintf(stdout, "  Permissions   : %o\n",  attr->perms);
-   }
+    fprintf(stdout, "-------------------------------------------------------\n");
+    fprintf(stdout, "  File Name     : %s\n",  pszName);
+    
+    fprintf(stdout, "  Permissions   : %04o\n", stat_buf->st_mode & 07777);
 
-   /* Print the type of object */
-   if(attr->mask & PVFS_ATTR_SYS_TYPE)
-   {
-      if(attr->objtype & PVFS_TYPE_METAFILE)
-      {
-         fprintf(stdout, "  Type          : Regular File\n");
-      }
-      else if(attr->objtype & PVFS_TYPE_DIRECTORY)
-      {
-         fprintf(stdout, "  Type          : Directory\n");
-      }
-      else if(attr->objtype & PVFS_TYPE_SYMLINK)
-      {
-         fprintf(stdout, "  Type          : Symbolic Link\n");
-         if(attr->mask &  PVFS_ATTR_SYS_LNK_TARGET)
-         {
+    /* Print the type of object */
+    if(S_ISREG(stat_buf->st_mode))
+    {
+        fprintf(stdout, "  Type          : Regular File\n");
+    }
+    else if(S_ISDIR(stat_buf->st_mode))
+    {
+        fprintf(stdout, "  Type          : Directory\n");
+    }
+    else if(S_ISLNK(stat_buf->st_mode))
+    {
+        fprintf(stdout, "  Type          : Symbolic Link\n");
+        /* TODO: How to get link target from stat*/
+        /*
+        if(attr->mask &  PVFS_ATTR_SYS_LNK_TARGET)
+        {
             fprintf(stdout, "  Link Target   : %s\n", attr->link_target);
-         }
-      }
-   }
+        }*/
+    }
+    
+    /* Print size for non-block and character devices */
+    if(!S_ISCHR(stat_buf->st_mode) && !S_ISBLK(stat_buf->st_mode))
+    {
+        /* If the size of a directory object is zero, let's default the size to 
+         * 4096. This is what the kernel module does, and is the default directory
+         * size on an EXT3 system
+         */
+        if( (stat_buf->st_size == 0) && 
+            S_ISDIR(stat_buf->st_mode))
+        {
+            fprintf(stdout, "  Size          : 4096\n");
+        }
+        else
+        {
+            fprintf(stdout, "  Size          : %lld\n",      lld(stat_buf->st_size));
+        }
 
-   if(attr->mask & PVFS_ATTR_SYS_SIZE)
-   {
-      /* If the size of a directory object is zero, let's default the size to 
-       * 4096. This is what the kernel module does, and is the default directory
-       * size on an EXT3 system
-       */
-      if( (attr->size == 0) && 
-          (attr->objtype & PVFS_TYPE_DIRECTORY))
-      {
-         fprintf(stdout, "  Size          : 4096\n");
-      }
-      else
-      {
-         fprintf(stdout, "  Size          : %lld\n",      lld(attr->size));
-      }
-
-   }
-   if(attr->mask & PVFS_ATTR_SYS_UID)
-   {
-      user  = getpwuid(attr->owner);
-      fprintf(stdout, "  Owner         : %d (%s)\n",  
-              attr->owner, 
-              (user ? user->pw_name : "UNKNOWN"));
-   }      
-   if(attr->mask & PVFS_ATTR_SYS_GID)
-   {
-      group = getgrgid(attr->group);
-      fprintf(stdout, "  Group         : %d (%s)\n",  
-              attr->group, 
-              (group ? group->gr_name : "UNKNOWN"));
-   }
-   if(attr->mask & PVFS_ATTR_SYS_ATIME)
-   {
-      time_t a_tmp = attr->atime;
-      sprintf(a_time, "%s", ctime((const time_t *)&a_tmp));
-      a_time[strlen(a_time)-1] = 0;
-      fprintf(stdout, "  atime         : %llu (%s)\n", llu(attr->atime), a_time);
-   }
-   if(attr->mask & PVFS_ATTR_SYS_MTIME)
-   {
-      time_t m_tmp = attr->mtime;
-      sprintf(m_time, "%s", ctime((const time_t *)&m_tmp));
-      m_time[strlen(m_time)-1] = 0;
-      fprintf(stdout, "  mtime         : %llu (%s)\n", llu(attr->mtime), m_time);
-   }
-   if(attr->mask & PVFS_ATTR_SYS_CTIME)
-   {
-      time_t c_tmp = attr->ctime;
-      sprintf(c_time, "%s", ctime((const time_t *)&c_tmp));
-      c_time[strlen(c_time)-1] = 0;
-      fprintf(stdout, "  ctime         : %llu (%s)\n", llu(attr->ctime), c_time);
-   }
+    }
+    
+    if(stat_buf->st_uid)
+    {
+        user  = getpwuid(stat_buf->st_uid);
+        fprintf(stdout, "  Owner         : %d (%s)\n",  
+                stat_buf->st_uid, 
+                (user ? user->pw_name : "UNKNOWN"));
+    }
+          
+    if(stat_buf->st_gid)
+    {
+        group = getgrgid(stat_buf->st_gid);
+        fprintf(stdout, "  Group         : %d (%s)\n",  
+                stat_buf->st_gid, 
+                (group ? group->gr_name : "UNKNOWN"));
+    }
+    
+    if(stat_buf->st_atime)
+    {
+        time_t a_tmp = stat_buf->st_atime;
+        sprintf(a_time, "%s", ctime((const time_t *)&a_tmp));
+        a_time[strlen(a_time)-1] = 0;
+        fprintf(stdout, "  atime         : %llu (%s)\n", llu(stat_buf->st_atime), a_time);
+    }
+    if(stat_buf->st_mtime)
+    {
+        time_t m_tmp = stat_buf->st_mtime;
+        sprintf(m_time, "%s", ctime((const time_t *)&m_tmp));
+        m_time[strlen(m_time)-1] = 0;
+        fprintf(stdout, "  mtime         : %llu (%s)\n", llu(stat_buf->st_mtime), m_time);
+    }
+    if(stat_buf->st_ctime)
+    {
+        time_t c_tmp = stat_buf->st_ctime;
+        sprintf(c_time, "%s", ctime((const time_t *)&c_tmp));
+        c_time[strlen(c_time)-1] = 0;
+        fprintf(stdout, "  ctime         : %llu (%s)\n", llu(stat_buf->st_ctime), c_time);
+    }
    
-   /* dfile_count is only valid for a file. For a given file, it tells how many
-    *  datafiles there are
-    */
-   if( (attr->mask & PVFS_ATTR_SYS_DFILE_COUNT) &&
-       (attr->objtype == PVFS_TYPE_METAFILE))
-   {
-      fprintf(stdout, "  datafiles     : %d\n", attr->dfile_count);
-   }
-
-   if( (attr->mask & PVFS_ATTR_SYS_BLKSIZE) &&
-       (attr->objtype == PVFS_TYPE_METAFILE))
-   {
-      fprintf(stdout, "  blksize       : %lld\n", lld(attr->blksize));
-   }
-
-   /* dirent_count is only valid on directories */
-   if( (attr->mask & PVFS_ATTR_SYS_DIRENT_COUNT) &&
-       (attr->objtype == PVFS_TYPE_DIRECTORY))
-   {
-      fprintf(stdout, "  dir entries   : %llu\n", llu(attr->dirent_count));
-   }
-
-   if( (attr->mask & PVFS_ATTR_SYS_DISTDIR_ATTR) &&
-       (attr->objtype == PVFS_TYPE_DIRECTORY))
-   {
-      fprintf(stdout, "  dirdata count : %d\n", attr->distr_dir_servers_max);
-   }
-
-   if ((attr->mask & PVFS_ATTR_SYS_TYPE) && 
-          (attr->objtype & PVFS_TYPE_METAFILE))
-   {
-       if (attr->flags == 0)
-           fprintf(stdout, "  flags         : none");
-       else
-           fprintf(stdout, "  flags         : ");
-       if (attr->flags & PVFS_IMMUTABLE_FL)
-           fprintf(stdout, "immutable, ");
-       if (attr->flags & PVFS_APPEND_FL)
-           fprintf(stdout, "append-only, ");
-       if (attr->flags & PVFS_NOATIME_FL)
-           fprintf(stdout, "noatime ");
-       fprintf(stdout, "\n");
-   }
 }
 
 static void usage(int argc, char** argv)
