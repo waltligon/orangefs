9f26a794d0766f12d88626a59904e34b326e5b74
James Burton <jburto2@clemson.edu>
2014-08-13 21:07:52 +0000

    Fix ltp and tail tests. Also run dbench last.
    SVN revision 11196

--- OFSTest/OFSUsrintTest.py
+++ OFSTest/OFSUsrintTest.py
@@ -478,8 +478,8 @@ def ltp(testing_node,output=[]):
     
     testing_node.changeDirectory(LTP_PREFIX)
     
-    print 'sudo PVFS2TAB_FILE=%s/etc/orangefstab LD_LIBRARY_PATH=/opt/db4/lib:%s/lib %s ./runltp -p -l %s/ltp-pvfs-testcases-%s.log -d %s/ltp-tmp -f ltp-pvfs-testcases -a %s/zoo.tmp >& %s/ltp-pvfs-testcases-%s.output' % (testing_node.ofs_installation_location,testing_node.ofs_installation_location,testing_node.ofs_installation_location,preload, vfs_type, testing_node.ofs_mount_point,testing_node.ofs_extra_tests_location,testing_node.ofs_installation_location,vfs_type)
-    rc = testing_node.runSingleCommandAsRoot('PVFS2TAB_FILE=%s/etc/orangefstab LD_LIBRARY_PATH=/opt/db4/lib:%s/lib %s ./runltp -p -l %s/ltp-pvfs-testcases-%s.log -d %s/ltp-tmp-usrint -f ltp-pvfs-testcases -a %s/zoo.tmp 2>&1 | tee %s/ltp-pvfs-testcases-%s.output' % (testing_node.ofs_installation_location,testing_node.ofs_installation_location,testing_node.ofs_installation_location, preload,vfs_type, testing_node.ofs_mount_point,testing_node.ofs_extra_tests_location,testing_node.ofs_installation_location,vfs_type),output)
+    print 'sudo PVFS2TAB_FILE=%s/etc/orangefstab LD_LIBRARY_PATH=%s:%s/lib %s ./runltp -p -l %s/ltp-pvfs-testcases-%s.log -d %s/ltp-tmp -f ltp-pvfs-testcases -a %s/zoo.tmp >& %s/ltp-pvfs-testcases-%s.output' % (testing_node.ofs_installation_location,testing_node.db4_lib_dir, testing_node.ofs_installation_location,preload,testing_node.ofs_installation_location, vfs_type, testing_node.ofs_mount_point,testing_node.ofs_extra_tests_location,testing_node.ofs_installation_location,vfs_type)
+    rc = testing_node.runSingleCommandAsRoot('PVFS2TAB_FILE=%s/etc/orangefstab LD_LIBRARY_PATH=%s:%s/lib %s ./runltp -p -l %s/ltp-pvfs-testcases-%s.log -d %s/ltp-tmp-usrint -f ltp-pvfs-testcases -a %s/zoo.tmp 2>&1 | tee %s/ltp-pvfs-testcases-%s.output' % (testing_node.ofs_installation_location,testing_node.db4_lib_dir,testing_node.ofs_installation_location, preload, testing_node.ofs_installation_location, vfs_type, testing_node.ofs_mount_point,testing_node.ofs_extra_tests_location,testing_node.ofs_installation_location,vfs_type),output)
 
     # check to see if log file is there
     if testing_node.runSingleCommand("[ -f %s/ltp-pvfs-testcases-%s.log ]"% (testing_node.ofs_installation_location,vfs_type)):
@@ -589,7 +589,7 @@ def tail(testing_node,output=[]):
         test_string = "%s line%d\n" % (test_string,i)
     
     preload = "LD_PRELOAD=%s/lib/libofs.so:%s/lib/libpvfs2.so " % (testing_node.ofs_installation_location,testing_node.ofs_installation_location)
-    testing_node.runSingleCommand('%s bash -c \'echo "%s" > %s\'' % (preload,test_string,tail_test))
+    testing_node.runSingleCommand('%s bash -c \'echo \\"%s\\" > %s\'' % (preload,test_string,tail_test))
    
     # now diff it
     rc = testing_node.runSingleCommand("%s diff %s %s" % (preload,tail_test,local_reference),output)
@@ -619,7 +619,6 @@ def usrint_cp(testing_node,output=[]):
     return rc
 
 tests = [ 
-ltp,
 append,
 append2,
 fdtree,
@@ -631,6 +630,7 @@ symlink_usrint,
 tail,
 usrint_cp,
 shelltest,
+bonnie,
+ltp,
 dbench,
-bonnie
  ]
