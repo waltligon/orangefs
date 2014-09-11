46c6d6e197d1a4c281743391698882e075fb02e6
James Burton <jburto2@clemson.edu>
2014-09-11 19:12:18 +0000

    Return return code of programs run as root.
    SVN revision 11315

--- OFSTest/OFSTestNode.py
+++ OFSTest/OFSTestNode.py
@@ -539,7 +539,7 @@ class OFSTestNode(object):
     # @param output Output list
     
     def runSingleCommandAsRoot(self,command,output=[],debug=False):
-        self.runSingleCommand(command,output,"root",debug)
+        return self.runSingleCommand(command,output,"root",debug)
      
     ##
     # @fn runSingleCommandBacktick(self,command,output=[],remote_user=None):
