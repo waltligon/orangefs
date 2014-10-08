e12171570aac71314ccef2afb4fdac6768053db9
James Burton <jburto2@clemson.edu>
2014-10-08 18:00:20 +0000

    Setup hadoop-env.sh after copying templates, not before, so that changes don't get overwritten.
    SVN revision 11420

--- OFSTest/OFSTestNetwork.py
+++ OFSTest/OFSTestNetwork.py
@@ -1628,14 +1628,15 @@ class OFSTestNetwork(object):
 
         for node in hadoop_nodes:
             
+            # copy templates to node
+            #master_node.copyToRemoteNode(source="%s/test/automated/hadoop-tests.d/conf/" % master_node.ofs_source_location,destination_node=node,destination="%s/conf/" % node.hadoop_location,recursive=True)
+            master_node.copyToRemoteNode(source="%s/src/client/hadoop/orangefs-hadoop1/src/main/resources/conf/" % master_node.ofs_source_location,destination_node=node,destination="%s/conf/" % node.hadoop_location,recursive=True)
             # setup hadoop-env.sh
             node.runSingleCommand("echo 'export JAVA_HOME=%s' >> %s/conf/hadoop-env.sh" % (node.jdk6_location,node.hadoop_location))
             node.runSingleCommand("echo 'export LD_LIBRARY_PATH=%s/lib' >> %s/conf/hadoop-env.sh" % (node.ofs_installation_location,node.hadoop_location))
             node.runSingleCommand("echo 'export JNI_LIBRARY_PATH=%s/lib' >> %s/conf/hadoop-env.sh" % (node.ofs_installation_location,node.hadoop_location))
             node.runSingleCommand("echo 'export HADOOP_CLASSPATH=\$JNI_LIBRARY_PATH/orangefs-hadoop1*.jar:\$JNI_LIBRARY_PATH/ofs-jni-*.jar' >> %s/conf/hadoop-env.sh" % node.hadoop_location)
-            # copy templates to node
-            #master_node.copyToRemoteNode(source="%s/test/automated/hadoop-tests.d/conf/" % master_node.ofs_source_location,destination_node=node,destination="%s/conf/" % node.hadoop_location,recursive=True)
-            master_node.copyToRemoteNode(source="%s/src/client/hadoop/orangefs-hadoop1/src/main/resources/conf/" % master_node.ofs_source_location,destination_node=node,destination="%s/conf/" % node.hadoop_location,recursive=True)
+            
             
             # update mapred-site.xml
             node.runSingleCommand("sed -i s/localhost/%s/ %s/conf/mapred-site.xml" % (master_node.hostname,node.hadoop_location))
