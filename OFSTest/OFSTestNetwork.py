67e82ef32f17f7efe4477b36a8a29c33909feb51
James Burton <jburto2@clemson.edu>
2014-10-17 17:33:18 +0000

    Fix typo.
    SVN revision 11462

--- OFSTest/OFSTestNetwork.py
+++ OFSTest/OFSTestNetwork.py
@@ -69,7 +69,6 @@ class OFSTestNetwork(object):
         
         if ip_address != "":
             if ip_address == self.local_master.ip_address:
-                genconfig_str="%s/bin/pvfs2-genconfig %s/etc/orangefs.c
                 return self.local_master
             else:
                 return next((i for i in self.network_nodes if i.ip_address == ip_address), None) 
@@ -1636,7 +1635,7 @@ class OFSTestNetwork(object):
 #             node.runSingleCommand("echo 'export JNI_LIBRARY_PATH=%s/lib' >> %s/conf/hadoop-env.sh" % (node.ofs_installation_location,node.hadoop_location))
 #             node.runSingleCommand("echo 'export HADOOP_CLASSPATH=\$JNI_LIBRARY_PATH/orangefs-hadoop1-2.9.0.jar:\$JNI_LIBRARY_PATH/ofs-jni-2.9.0.jar' >> %s/conf/hadoop-env.sh" % node.hadoop_location)
             
-            node.runSindleCommand('sed -i s,/usr/lib/jvm/java-7-openjdk-amd64,%s,g %s/conf/hadoop-env.sh' % (node.jdk6_location,node.hadoop_location ))
+            node.runSingleCommand('sed -i s,/usr/lib/jvm/java-7-openjdk-amd64,%s,g %s/conf/hadoop-env.sh' % (node.jdk6_location,node.hadoop_location ))
             # update mapred-site.xml
             node.runSingleCommand('sed -i s,/opt/orangefs-trunk,%s,g %s/conf/hadoop-env.sh' % (node.ofs_installation_location,node.hadoop_location ))
             
