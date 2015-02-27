3ff8a594d77362e8aff5076898ebf13b1e3f1c31
James Burton <jburto2@clemson.edu>
2015-02-27 19:40:36 +0000

    Change wildcard for M2_HOME variable.
    SVN revision 11735

--- OFSTest/OFSTestNode.py
+++ OFSTest/OFSTestNode.py
@@ -1324,9 +1324,11 @@ class OFSTestNode(object):
         if rc != 0:
             self.runSingleCommandAsRoot("cd ~; wget --quiet http://apache.mesi.com.ar/maven/maven-3/3.2.*/binaries/apache-maven-3.2.*-bin.tar.gz")
             self.runSingleCommandAsRoot("cd /opt; tar xf ~/apache-maven-3.2.*-bin.tar.gz")
-            self.runSingleCommandAsRoot("ln -s /opt/apache-maven-3.2.*/bin/mvn /usr/bin/mvn")
-            self.setEnvironmentVariable("M2_HOME", "/opt/apache-maven-3.2.*")
-            self.setEnvironmentVariable("M2", "/opt/apache-maven-3.2.*/bin")
+            maven_home = self.runSingleCommandBacktick("ls -d /opt/apache-maven-3.2.*")
+            self.runSingleCommandAsRoot("ln -s %s/bin/mvn /usr/bin/mvn" % maven_home)
+            
+            self.setEnvironmentVariable("M2_HOME", maven_home)
+            self.setEnvironmentVariable("M2", maven_home+"/bin")
             
 
     def installDB4(self):
