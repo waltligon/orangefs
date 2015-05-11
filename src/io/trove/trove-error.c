2714543b5056edf0b4c7f3a5e7e017d9ac240d4b
Martin Brandenburg <martin@omnibond.com>
2015-05-11 15:46:04 +0000

    Fix typo.
    SVN revision 11857

--- src/io/trove/trove-error.c
+++ src/io/trove/trove-error.c
@@ -6,6 +6,7 @@
 
 #include <errno.h>
 
+#include "gossip.h"
 #include "trove.h"
 #include "trove-internal.h"
 
@@ -98,7 +99,7 @@ PVFS_error trove_errno_to_trove_error(int errno_value)
             return s_trove_error_map[i].trove_value;
         }
     }
-    gossip_lerror("trove_errno_to_trove_error: unknown errno value\n");
+    gossip_lerr("trove_errno_to_trove_error: unknown errno value\n");
     return 4242; /* just return some identifiable number */
 }
 
