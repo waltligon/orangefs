d09f1f756560b0a43bfa60c712f85f89793d53f0
Jeffrey Denton <denton@clemson.edu>
2015-06-02 22:12:47 +0000

    Additions to new client cache code.
    SVN revision 11924

--- src/client/sysint/client-cache.c
+++ src/client/sysint/client-cache.c
@@ -2,47 +2,285 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <assert.h>
 
 #include "client-cache.h"
 
 client_cache_t cc;
 
+static void fent_lru_to_front(cc_fent_t *fentp);
+static void fent_lru_remove(cc_fent_t *fentp);
+
+static cc_fent_t *fent_get_next_free();
+static int fent_match_key(cc_fent_t *fentp, uint64_t fhandle, uint32_t fsid);
+static cc_fent_t *fent_getp_by_index(uint16_t index);
+
+cc_fent_t *fent_lookup(uint64_t fhandle, uint32_t fsid);
+cc_fent_t *fent_insert(uint64_t fhandle, uint32_t fsid);
+int fent_remove_by_key(uint64_t fhandle, uint32_t fsid);
+int fent_remove(cc_fent_t *fentp);
+
+static void fent_lru_remove(cc_fent_t *fentp)
+{
+    cc_fent_t *prev = NULL;
+    cc_fent_t *next = NULL;
+
+    assert(fentp);
+
+    prev = fent_getp_by_index(fentp->ru_prev);
+    next = fent_getp_by_index(fentp->ru_next);
+
+    fentp->ru_prev = NIL16;
+    fentp->ru_next = NIL16;
+
+    if(prev != NULL && next != NULL)
+    {
+        /* This entry is in between other entries, so we need to stitch the hole
+         * in the DLL that would be created by removing this item. */
+        /* There is no need to update MRU and LRU indexes. */
+        next->ru_prev = prev->index;
+        prev->ru_next = next->index;
+    }
+    if(prev == NULL && next == NULL)
+    {
+        /* Test if this is the only item on the LRU/MRU list, or this is a new
+         * entry that cannot be removed. */
+        if(cc.ftbl.mru == fentp->index || cc.ftbl.lru == fentp->index)
+        {
+            /* Must be the only item on the LRU/MRU list. */
+            assert(cc.ftbl.mru == fentp->index && cc.ftbl.lru == fentp->index);
+            cc.ftbl.mru = NIL16;
+            cc.ftbl.lru = NIL16;
+        }
+        else
+        {
+            /* This entry must be a new entry, so do nothing. */
+        }
+    }
+    else if(prev == NULL)
+    {
+        /* The first of multiple items on the LRU list (aka the MRU entry). */
+        cc.ftbl.mru = next->index;
+        next->ru_prev = NIL16;
+        if(next->ru_next == NIL16)
+        {
+            cc.ftbl.lru = next->index;
+        }
+    }
+    else if(next == NULL)
+    {
+        /* The last of multiple items on the LRU list (aka the LRU entry) */
+        cc.ftbl.lru = prev->index;
+        prev->ru_next = NIL16;
+        if(prev->ru_prev == NIL16)
+        {
+            cc.ftbl.mru = prev->index;
+        }
+    }
+    else
+    {
+        /* Shouldn't get here! */
+        assert(0);
+    }
+}
+
+static void fent_lru_to_front(cc_fent_t *fentp)
+{
+    cc_fent_t * mru_fentp = NULL;
+    cc_fent_t * lru_fentp = NULL;
+
+    assert(fentp);
+
+    fent_lru_remove(fentp);
+
+    mru_fentp = fent_getp_by_index(cc.ftbl.mru);
+    lru_fentp = fent_getp_by_index(cc.ftbl.lru);
+
+    if(mru_fentp == NULL && lru_fentp == NULL)
+    {
+        /* Previously, no entries in the DLL */
+        cc.ftbl.mru = fentp->index;
+        cc.ftbl.lru = fentp->index;
+        /* ru_prev and ru_next should already be NIL16 due to fent_lru_remove */
+    }
+    else if(mru_fentp == lru_fentp)
+    {
+        /* Previously, only one entry in the DLL. */
+        cc.ftbl.mru = fentp->index;
+        fentp->ru_next = lru_fentp->index;
+        lru_fentp->ru_prev = fentp->index;
+        /* fentp->ru_prev should already be NIL16 due to fent_lru_remove */
+    }
+    else
+    {
+        /* Previously, multiple items in DLL. (MRU and LRU are different) */
+        cc.ftbl.mru = fentp->index;
+        fentp->ru_next = mru_fentp->index;
+        mru_fentp->ru_prev = fentp->index;
+        /* fentp->ru_prev should already be NIL16 due to fent_lru_remove */
+    }
+}
+
+static int fent_match_key(cc_fent_t *fentp, uint64_t fhandle, uint32_t fsid)
+{
+    assert(fentp);
+    if(fentp->file_handle == fhandle && fentp->fsid == fsid)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+static cc_fent_t *fent_getp_by_index(uint16_t index)
+{
+    if(index < cc.fent_limit)
+    {
+        return cc.ftbl.fents + index;
+    }
+    return NULL;
+}
+
+cc_fent_t *fent_lookup(uint64_t fhandle, uint32_t fsid)
+{
+    uint16_t bucket = 0;
+    cc_fent_t *fentp = NULL;
+
+    bucket = (fhandle + fsid) % cc.fent_ht_limit;
+
+    fentp = fent_getp_by_index(cc.ftbl.fents_ht[bucket]);
+    while(fentp != NULL)
+    {
+        if(fent_match_key(fentp, fhandle, fsid))
+        {
+            return fentp;
+        }
+        fentp = fent_getp_by_index(fentp->next);
+    }
+
+    return NULL;
+}
+
+static cc_fent_t *fent_get_next_free()
+{
+    cc_fent_t *fentp = NULL;
+
+    if(cc.ftbl.free_fent == NIL16)
+    {
+        /* TODO attempt to evict file */
+        return NULL;
+    }
+
+    fentp = fent_getp_by_index(cc.ftbl.free_fent);
+    assert(fentp);
+    cc.ftbl.free_fent = fentp->next;
+    return fentp;
+}
+
+cc_fent_t *fent_insert(uint64_t fhandle, uint32_t fsid)
+{
+    cc_fent_t * new_fentp = NULL;
+    cc_fent_t * old_head = NULL;
+    uint16_t bucket = 0;
+
+    new_fentp = fent_get_next_free();
+    if(new_fentp == NULL)
+    {
+        return NULL;
+    }
+
+    /* Fill in fent values */
+    new_fentp->file_handle = fhandle;
+    new_fentp->fsid = fsid;
+    new_fentp->file_size = 0;
+
+    /* TODO */
+    /* Allocate + Initialize mtbl */
+
+    /* Insert at head of hash table chain */
+    bucket = (fhandle + fsid) % cc.fent_ht_limit;
+    old_head = fent_getp_by_index(cc.ftbl.fents_ht[bucket]);
+
+    if(old_head == NULL)
+    {
+        new_fentp->next = NIL16;
+    }
+    else
+    {
+        new_fentp->next = old_head->index;
+    }
+
+    /* Insert at head of ht chain */
+    cc.ftbl.fents_ht[bucket] = new_fentp->index;
+
+    return new_fentp;
+}
+
 int init_cache(
-    uint64_t byte_limit,
+    uint64_t cache_size,
     uint64_t block_size,
     uint16_t fent_limit,
-    uint16_t fent_ht_limit)
+    uint16_t fent_ht_limit,
+    uint16_t ment_limit,
+    uint16_t ment_ht_limit)
 {
-    int ret = 0;
-    int num_blocks = 0;
+    uint16_t *uint16p = NULL;
+    void *voidp = NULL;
+    /* int ret = 0; */
     int i = 0;
-    uint16_t *temp = NULL;
+
     printf("%s\n", __func__);
 
     /* Store limits w/ cc */
+    cc.block_size = block_size;
     cc.fent_limit = fent_limit;
     cc.fent_ht_limit = fent_ht_limit;
-    //cc.ment_limit = ment_limit;
-    //cc.ment_ht_limit = ment_ht_limit;
+    cc.fent_size = (uint16_t) sizeof(cc_fent_t);
+    cc.ment_limit = ment_limit;
+    cc.ment_ht_limit = ment_ht_limit;
+    cc.ment_size = (uint16_t) sizeof(cc_ment_t);
+
+    printf("%s: block_size = %llu\n",
+           __func__,
+           (long long unsigned int) cc.block_size);
+    printf("%s: fent_limit = %hu\n", __func__, cc.fent_limit);
+    printf("%s: fent_ht_limit = %hu\n", __func__, cc.fent_ht_limit);
+    printf("%s: fent_size = %hu\n", __func__, cc.fent_size);
+    printf("%s: ment_limit = %hu\n", __func__, cc.ment_limit);
+    printf("%s: ment_ht_limit = %hu\n", __func__, cc.ment_ht_limit);
+    printf("%s: ment_size = %hu\n", __func__, cc.ment_size);
 
     /* Allocate memory for blocks. */
-    num_blocks = byte_limit / block_size;
-    printf("%s: num_blocks = %d\n", __func__, num_blocks);
-    if(num_blocks == 0)
+    cc.num_blocks = cache_size / block_size;
+    printf("%s: num_blocks = %d\n", __func__, cc.num_blocks);
+    if(cc.num_blocks == 0)
     {
         fprintf(stderr, "%s: WARN num_blocks is ZERO!\n", __func__);
         return 0;
     }
-    cc.blks = calloc(1, num_blocks * block_size);
+    cc.blks = calloc(1, cc.num_blocks * block_size);
     if(cc.blks == NULL)
     {
         fprintf(stderr, "%s: ERROR allocating memory for blks!\n", __func__);
         return -1;
     }
+
+    cc.cache_size = cc.num_blocks * block_size;
     printf("%s: cacheable bytes = %llu = %Lf MiB\n",
            __func__,
-           (long long unsigned int) (num_blocks * block_size),
-           ((long double) ((num_blocks * block_size)) / (1024.0 * 1024.0)));
+           (long long unsigned int) cc.cache_size,
+           ((long double) (cc.cache_size) / (1024.0 * 1024.0)));
+
+    /* Setup free blocks LL */
+    printf("%s: first block address = %p\n", __func__, cc.blks);
+    cc.free_blk = 0;
+    for(i = 0, voidp = cc.blks;
+        i < (cc.num_blocks - 1);
+        voidp += cc.block_size, i++)
+    {
+        /* printf("%s: block address = %p\n", __func__, voidp); */
+        ((free_block_t *) voidp)->next = i + 1;
+    }
+    ((free_block_t *) voidp)->next = NIL16;
 
     /* Allocate memory for fents */
     cc.ftbl.fents = calloc(1, fent_limit * sizeof(cc_fent_t));
@@ -55,6 +293,19 @@ int init_cache(
            __func__,
            (long long unsigned int) fent_limit * sizeof(cc_fent_t));
 
+    /* Setup free fents LL */
+    printf("%s: first fent address = %p\n", __func__, cc.ftbl.fents);
+    cc.ftbl.free_fent = 0;
+    for(i = 0, voidp = cc.ftbl.fents;
+        i < (cc.fent_limit - 1);
+        voidp += cc.fent_size, i++)
+    {
+        /* printf("%s: fent address = %p\n", __func__, voidp); */
+        ((cc_fent_t *) voidp)->index = i;
+        ((cc_fent_t *) voidp)->next = i + 1;
+    }
+    ((cc_fent_t *) voidp)->next = NIL16;
+
     /* Allocate memory for fents ht */
     cc.ftbl.fents_ht = calloc(1, fent_ht_limit * sizeof(uint16_t));
     if(cc.ftbl.fents_ht == NULL)
@@ -66,9 +317,10 @@ int init_cache(
     }
 
     /* Set all hash table buckets to NIL16 */
-    for(i = 0, temp = cc.ftbl.fents_ht; i < fent_ht_limit; i++, temp++)
+    for(i = 0, uint16p = cc.ftbl.fents_ht; i < fent_ht_limit; i++, uint16p++)
     {
-        *temp = NIL16;
+        /* printf("%s: uint16p = %p\n", __func__, uint16p); */
+        *uint16p = NIL16;
     }
     printf("%s: fents_ht bytes = %llu\n",
            __func__,
@@ -79,8 +331,8 @@ int init_cache(
 
 int finalize_cache(void)
 {
-    int ret = 0;
-    int i = 0;
+    /* int ret = 0; */
+    /* int i = 0; */
     printf("%s\n", __func__);
 
     /* Flush all dirty blocks */
@@ -95,7 +347,8 @@ int finalize_cache(void)
     /* Free fents memory region: */
     if(cc.ftbl.fents != NULL)
     {
-        /* TODO delve into fent and free underlying allocated structs like ments */
+        /* TODO delve into fent and free underlying allocated structs like
+         * ments. */
 
         free(cc.ftbl.fents);
     }
@@ -106,20 +359,11 @@ int finalize_cache(void)
         free(cc.ftbl.fents_ht);
     }
 
+    /* TODO */
     /* For every bucket, walk the list and...*/
         /* Free ments memory region */
         /* For every bucket, walk the list and */
-#if 0
-    ret = pthread_rwlock_destroy(rwlockp);
-    if(ret != 0)
-    {
-        fprintf(stderr,
-                "%s: pthread_rwlock_destroy returned %d\n",
-                __func__,
-                ret);
-        return -1;
-    }
-#endif
+
     return 0;
 }
 
@@ -128,7 +372,12 @@ int main(int argc, char** argv)
     int ret = 0;
     printf("%s\n", __func__);
     printf("FYI: sizeof(pthread_rwlock_t) = %zu\n", sizeof(pthread_rwlock_t));
-    ret = init_cache(BYTE_LIMIT, BLOCK_SIZE_B, FENT_LIMIT, FENT_HT_LIMIT);
+    ret = init_cache(BYTE_LIMIT,
+                     BLOCK_SIZE_B,
+                     FENT_LIMIT,
+                     FENT_HT_LIMIT,
+                     MENT_LIMIT,
+                     MENT_HT_LIMIT);
     if(ret != 0)
     {
         fprintf(stderr, "%s: init_cache returned %d\n", __func__, ret);
--- src/client/sysint/client-cache.h
+++ src/client/sysint/client-cache.h
@@ -11,17 +11,18 @@
 # define NILP NIL64
 #endif
 
-#define FENT_LIMIT 256
-#define MENT_LIMIT 256
 #define BLOCK_SIZE_K 256
 #define BLOCK_SIZE_B (BLOCK_SIZE_K * 1024)
 #define BYTE_LIMIT (MENT_LIMIT * BLOCK_SIZE_B)
+
+#define FENT_LIMIT 256
 #define FENT_HT_LIMIT 29
+#define MENT_LIMIT 256
 #define MENT_HT_LIMIT 29
 
 #define NIL16 0XFFFF
 
-typedef struct cc_ment
+typedef struct cc_ment_s
 {
     uint64_t tag;           /* offset of data block in file */
     uint16_t item;          /* index of cache block with data */
@@ -29,15 +30,9 @@ typedef struct cc_ment
     uint16_t dirty_next;    /* if dirty used in dirty list */
     uint16_t lru_prev;      /* used in lru list */
     uint16_t lru_next;      /* used in lru list */
-#if (PVFS2_SIZEOF_VOIDP == 32)
-    char pad[2];
-#elif (PVFS2_SIZEOF_VOIDP == 64)
-    char pad[6];
-#endif
-    pthread_rwlock_t rwlock;
 } cc_ment_t;
 
-typedef struct cc_mtbl
+typedef struct cc_mtbl_s
 {
     uint16_t *ments_ht;         /* Hash table for ments indexes */
     cc_ment_t *ments;           /* all ments */
@@ -47,37 +42,50 @@ typedef struct cc_mtbl
     uint16_t lru_last;          /* index of last block on lru list */
     uint16_t dirty_first;       /* index of first dirty block */
     uint16_t ref_cnt;           /* number of client threads using this file */
-    pthread_rwlock_t rwlock;
 } cc_mtbl_t;
 
-typedef struct cc_fent
+typedef struct cc_fent_s
 {
     cc_mtbl_t mtbl;
     uint64_t file_size; /* Largest I/O offset seen by cache... */
     uint64_t file_handle;
-    uint32_t file_system_id;
-    uint16_t next; /* next fent in ht chain */
+    uint32_t fsid;
+    uint16_t next; /* next fent in ht chain and free fents LL */
     uint16_t index; /* this fent's index in ftbl.fents */
-    pthread_rwlock_t rwlock;
+    uint16_t ru_prev;      /* used in lru list */
+    uint16_t ru_next;      /* used in lru list */
 } cc_fent_t;
 
-typedef struct cc_ftbl
+typedef struct cc_ftbl_s
 {
-    uint16_t *fents_ht;     /* Hash table for fents indexes */
+    uint16_t *fents_ht;     /* Hash table for fent indexes */
     cc_fent_t *fents;       /* All fents */
-    uint16_t free_blk;      /* Index of the next free block */
-    uint16_t free_fent;    /* Index of the next free file entry */
-    pthread_rwlock_t rwlock;
+    uint16_t free_fent;     /* Index of the next free file entry */
+    uint16_t mru;           /* Index of first fent on lru list */
+    uint16_t lru;           /* Index of last fent on lru list */
 } cc_ftbl_t;
 
-typedef struct client_cache
+typedef struct client_cache_s
 {
     cc_ftbl_t ftbl;
     void *blks;
+    uint16_t free_blk;
+    /* Maintain limits in cache struct */
+    uint64_t cache_size;
+    uint64_t block_size;
+    uint16_t num_blocks;
     uint16_t fent_limit;
     uint16_t fent_ht_limit;
+    uint16_t fent_size;
     uint16_t ment_limit;
     uint16_t ment_ht_limit;
+    uint16_t ment_size;
 } client_cache_t;
 
+typedef struct free_block_s
+{
+    uint16_t next; /* Index of next free block in LL */
+} free_block_t;
+
+/* pthread_rwlock_t rwlock; */
 #endif /* CLIENT_CACHE_H */
