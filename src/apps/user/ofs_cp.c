f0b20e1c12d7507d745159438fcf7e028a73f67c
Walt Ligon <walt@clemson.edu>
2015-06-12 19:48:36 +0000

    updates to ofs_cp and ofs_rm, fixed some silly crashes, added some options, added long options to ofs_rm and (hopefully) fixed a compile problem with ofs_cp
    SVN revision 11969

--- src/apps/user/ofs_cp.c
+++ src/apps/user/ofs_cp.c
@@ -16,7 +16,6 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <fts.h>
 #include <time.h>
 #include <libgen.h>
@@ -24,6 +23,18 @@
 
 #include "orange.h"
 #include "pint-sysint-utils.h"
+
+/* This forces inlines off for open variants, which play heck with
+ * programs using the posix.c library
+ */
+#ifdef __USE_FORTIFY_LEVEL
+#undef __USE_FORTIFY_LEVEL
+#endif
+#define __USE_FORTIFY_LEVEL 0
+
+#include <fcntl.h>
+/* This should be the last header so we don't affect any others
+ */
 
 #define PVFS_ATTR_SYS_CP (PVFS_ATTR_SYS_TYPE | \
                           PVFS_ATTR_SYS_PERM | \
@@ -46,6 +57,8 @@ struct options
     int copy_to_dir;
     int verbose;
     int recursive;
+    int preserve;
+    int mode;
     int total_written;
     char *srcfile;
     char *destfile;
@@ -256,7 +269,8 @@ int main (int argc, char **argv)
         case FTS_NSOK : /* no stat ok */
         case FTS_ERR :  /* error */
         default:
-            fprintf(stderr, "unexpected node type from fts_read\n");
+            fprintf(stderr, "%s: %s is unknown file type, not present, or not readable\n", argv[0], node->fts_path);
+            usage(argc, argv);
 	    ret = -1;
             break;
         }
@@ -399,8 +413,8 @@ static int copy_file(char *srcfile,
 	user_opts->total_written += write_size;
     }
 
-    /* preserve permissions */
-    if (pvfs_valid_fd(src) > 0)
+    /* preserve permissions and-or owner */
+    if ((user_opts->mode || user_opts->preserve) && pvfs_valid_fd(src) > 0)
     {
         ret = pvfs_fstat_mask(src, &sbuf, PVFS_ATTR_SYS_CP);
     }
@@ -414,11 +428,25 @@ static int copy_file(char *srcfile,
         goto err_out;
     }
 
-    ret = fchmod(dst, sbuf.st_mode);
-    if (ret < 0)
+    if (user_opts->mode)
     {
-        perror("fchmod");
-        goto err_out;
+        ret = fchmod(dst, sbuf.st_mode);
+        if (ret < 0)
+        {
+            perror("fchmod");
+            goto err_out;
+        }
+    }
+
+    if (user_opts->preserve)
+    {
+        ret = fchown(dst, sbuf.st_uid, sbuf.st_gid);
+        if (ret < 0)
+        {
+            /* note this should only work if root */
+            perror("fchown");
+            goto err_out;
+        }
     }
 
 err_out:
@@ -443,10 +471,12 @@ err_out:
  */
 static int parse_args(int argc, char *argv[], struct options *user_opts)
 {
-    char flags[] = "dtVvrs:n:b:";
+    char flags[] = "?mpdtVvrs:n:b:";
     int one_opt = 0;
     struct stat sbuf;
     int ret = -1;
+
+    opterr = 0;
 
     memset(user_opts, 0, sizeof(struct options));
 
@@ -468,6 +498,12 @@ static int parse_args(int argc, char *argv[], struct options *user_opts)
                 break;
             case('r'):
                 user_opts->recursive = 1;
+                break;
+            case('p'):
+                user_opts->preserve = 1;
+                break;
+            case('m'):
+                user_opts->mode = 1;
                 break;
 	    case('d'):
 		user_opts->debug = 1;
@@ -499,6 +535,7 @@ static int parse_args(int argc, char *argv[], struct options *user_opts)
 		}
 		break;
 	    case('?'):
+            default:
 		usage(argc, argv);
 		exit(EXIT_FAILURE);
 	}
@@ -585,10 +622,13 @@ static void usage(int argc, char **argv)
 	"\n-s <strip_size>\t\t\tsize of access to PVFS2 volume"
 	"\n-n <num_datafiles>\t\tnumber of PVFS2 datafiles to use"
 	"\n-b <buffer_size in bytes>\thow much data to read/write at once"
-        "\v-r\t\t\t\trecursively copy directories"
+        "\n-r\t\t\t\trecursively copy directories"
+        "\n-m\t\t\t\tpreserve mode of the files"
+        "\n-p\t\t\t\tpreserve owner of the files (requires root)"
         "\n-v\t\t\t\tverbose - print path of files as the are copied"
         "\n-d\t\t\t\tprint program debugging information"
 	"\n-t\t\t\t\tprint some timing information"
+	"\n-?\t\t\t\tprint this message"
 	"\n-V\t\t\t\tprint version number and exit\n");
     return;
 }
