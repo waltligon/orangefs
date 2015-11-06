8555307fbc119a6b0d63d38adee6cb3f43123e1c
David Reynolds <david@omnibond.com>
2015-11-06 16:04:14 +0000

    Use active_mtu instead of max_mtu (temporary fix at least until the QDR/FDR hardware issues are sorted out). This version works with IB on ofstest.
    SVN revision 12128

--- src/io/bmi/bmi_ib/openib.c
+++ src/io/bmi/bmi_ib/openib.c
@@ -38,8 +38,9 @@ struct openib_device_priv {
     int nic_max_sge;
     int nic_max_wr;
 
-    /* max MTU reported by NIC port */
+    /* MTU values reported by NIC port */
     int max_mtu;
+    int active_mtu;
 
     /*
      * Temp array for filling scatter/gather lists to pass to IB functions,
@@ -305,10 +306,21 @@ static void init_connection_modify_qp(struct ibv_qp *qp, uint32_t remote_qp_num,
     attr.max_dest_rd_atomic = 1;
     attr.ah_attr.dlid = remote_lid;
     attr.ah_attr.port_num = od->nic_port;
+    /*
     if (od->max_mtu > IBV_MTU) {
         attr.path_mtu = od->max_mtu;
+        //attr.path_mtu = 4;
     }
     else {
+        attr.path_mtu = IBV_MTU;
+    }
+    */
+    if (od->active_mtu > IBV_MTU) 
+    {
+        attr.path_mtu = od->active_mtu;
+    }
+    else
+    {
         attr.path_mtu = IBV_MTU;
     }
     attr.rq_psn = 0;
