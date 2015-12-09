af50608f1b3943b6c23242a55ea6f78125b2c840
David Reynolds <david@omnibond.com>
2015-12-09 21:47:51 +0000

    Remove the following compiler warnings:
    ./src/io/bmi/bmi_ib/openib.c: In function ‘async_event_type_string’:
    ./src/io/bmi/bmi_ib/openib.c:730: warning: enumeration value ‘IBV_EXP_EVENT_DCT_KEY_VIOLATION’ not handled in switch
    ./src/io/bmi/bmi_ib/openib.c:730: warning: enumeration value ‘IBV_EXP_EVENT_DCT_ACCESS_ERR’ not handled in switch
    ./src/io/bmi/bmi_ib/openib.c:730: warning: enumeration value ‘IBV_EXP_EVENT_DCT_REQ_ERR’ not handled in switchi
    
    
    
    SVN revision 12165

--- src/io/bmi/bmi_ib/openib.c
+++ src/io/bmi/bmi_ib/openib.c
@@ -749,6 +749,11 @@ static const char *async_event_type_string(enum ibv_event_type event_type)
 	CASE(IBV_EVENT_CLIENT_REREGISTER);
 #endif
 	CASE(IBV_EVENT_GID_CHANGE);
+
+    /* Experimental event types */
+    CASE(IBV_EXP_EVENT_DCT_KEY_VIOLATION);
+    CASE(IBV_EXP_EVENT_DCT_ACCESS_ERR);
+    CASE(IBV_EXP_EVENT_DCT_REQ_ERR);
     }
     return s;
 }
