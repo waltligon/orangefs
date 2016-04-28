80e66030a5180b3b3a2f9f2e23639043b6f5482b
Mike Marshall <hubcap@omnibond.com>
2016-04-28 18:49:54 +0000

    kmod: arguments to the xattr_handlers changed at 4.4
    SVN revision 12375

--- src/kernel/linux-2.6/xattr-trusted.c
+++ src/kernel/linux-2.6/xattr-trusted.c
@@ -18,75 +18,106 @@
 #include <linux/xattr.h>
 
 int pvfs2_xattr_set_trusted(
-#ifdef HAVE_XATTR_HANDLER_SET_SIX_PARAM
-        struct dentry *dentry,
+#ifdef HAVE_XATTR_HANDLER_SET_4_4
+                            const struct xattr_handler *handler,
+                            struct dentry *dentry,
+                            const char *name,
+                            const void *buffer,
+                            size_t size,
+                            int flags)
+#elif defined (HAVE_XATTR_HANDLER_SET_2.6.33)
+                            struct dentry *dentry,
+                            const char *name,
+                            const void *buffer,
+                            size_t size,
+                            int flags,
+                            int handler_flags)
 #else
-    struct inode *inode, 
-#endif /* HAVE_XATTR_HANDLER_SET_SIX_PARAM */
-    const char *name, 
-    const void *buffer, 
-    size_t size,
-    int flags
-#ifdef HAVE_XATTR_HANDLER_SET_SIX_PARAM
-    , int handler_flags
-#endif /* HAVE_XATTR_HANDLER_SET_SIX_PARAM */
-    )
+                            struct inode *inode, 
+                            const char *name, 
+                            const void *buffer, 
+                            size_t size,
+                            int flags)
+#endif
 {
     int internal_flag = 0;
 
-    gossip_debug(GOSSIP_XATTR_DEBUG, "pvfs2_xattr_set_trusted: name %s, buffer_size %zd\n",
-            name, size);
+    gossip_debug(GOSSIP_XATTR_DEBUG,
+                 "pvfs2_xattr_set_trusted: name %s, buffer_size %zd\n",
+                  name,
+                  size);
+
     if (strcmp(name, "") == 0)
         return -EINVAL;
-    if(!capable(CAP_SYS_ADMIN))
+
+    if (!capable(CAP_SYS_ADMIN))
     {
         gossip_err("pvfs2_xattr_set_trusted: operation not permitted\n");
         return -EPERM;
     }
+
     internal_flag = convert_to_internal_xattr_flags(flags);
 
-#ifdef HAVE_XATTR_HANDLER_SET_SIX_PARAM
-    return pvfs2_inode_setxattr(dentry->d_inode, 
-        PVFS2_XATTR_NAME_TRUSTED_PREFIX, name, buffer, size, internal_flag);
-#else
-    return pvfs2_inode_setxattr(inode, PVFS2_XATTR_NAME_TRUSTED_PREFIX,
-        name, buffer, size, internal_flag);
-#endif /* HAVE_XATTR_HANDLER_SET_SIX_PARAM */
+#if defined(HAVE_XATTR_HANDLER_SET_4_4) || \
+    defined(HAVE_XATTR_HANDLER_SET_2_6_33)
+    return pvfs2_inode_setxattr(dentry->d_inode,
+#else /* pre 2.6.33 */
+    return pvfs2_inode_setxattr(inode,
+#endif
+                                PVFS2_XATTR_NAME_TRUSTED_PREFIX,
+                                name,
+                                buffer,
+                                size,
+                                internal_flag);
 }
 
 int pvfs2_xattr_get_trusted(
-#ifdef HAVE_XATTR_HANDLER_GET_FIVE_PARAM
-    struct dentry *dentry,
+#ifdef HAVE_XATTR_HANDLER_GET_4_4
+                            const struct xattr_handler *handler,
+                            struct dentry *dentry,
+                            const char *name,
+                            void *buffer,
+                            size_t size)
+#elif defined (HAVE_XATTR_HANDLER_GET_2.6.33)
+                            struct dentry *dentry,
+                            const char *name,
+                            void *buffer,
+                            size_t size,
+                            int handler_flags)
 #else
-    struct inode *inode,
-#endif /* HAVE_XATTR_HANDLER_GET_FIVE_PARAM */
-    const char *name, 
-    void *buffer, 
-    size_t size
-#ifdef HAVE_XATTR_HANDLER_GET_FIVE_PARAM
-    , int handler_flags
-#endif /* HAVE_XATTR_HANDLER_GET_FIVE_PARAM */
-    )
+                            struct inode *inode,
+                            const char *name,
+                            void *buffer,
+                            size_t size)
+#endif
 {
-    gossip_debug(GOSSIP_XATTR_DEBUG, "pvfs2_xattr_get_trusted: name %s, buffer_size %zd\n",
-            name, size);
+    gossip_debug(GOSSIP_XATTR_DEBUG,
+                 "pvfs2_xattr_get_trusted: name %s, buffer_size %zd\n",
+                 name,
+                 size);
+
     if (strcmp(name, "") == 0)
         return -EINVAL;
+
     if(!capable(CAP_SYS_ADMIN))
     {
         gossip_err("pvfs2_xattr_get_trusted: operation not permitted\n");
         return -EPERM;
     }
-#ifdef HAVE_XATTR_HANDLER_GET_FIVE_PARAM
-    return pvfs2_inode_getxattr(dentry->d_inode, 
-        PVFS2_XATTR_NAME_TRUSTED_PREFIX, name, buffer, size);
+
+#if defined(HAVE_XATTR_HANDLER_GET_4_4) || \
+    defined(HAVE_XATTR_HANDLER_GET_2_6_33)
+    return pvfs2_inode_getxattr(dentry->d_inode,
 #else
-    return pvfs2_inode_getxattr(inode, PVFS2_XATTR_NAME_TRUSTED_PREFIX,
-        name, buffer, size);
-#endif /* HAVE_XATTR_HANDLER_GET_FIVE_PARAM */
+    return pvfs2_inode_getxattr(inode,
+#endif
+                                PVFS2_XATTR_NAME_TRUSTED_PREFIX,
+                                name,
+                                buffer,
+                                size);
 }
 
-#endif
+#endif /* most of the file is wrapped in this */
 
 #if !defined(PVFS2_LINUX_KERNEL_2_4) && defined(HAVE_GENERIC_GETXATTR)
 
