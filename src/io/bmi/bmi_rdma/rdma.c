7cb79c01e88788b7611900f9c5877b71f06c1ff2
David Reynolds <david@omnibond.com>
2017-09-01 18:18:27 +0000

    Cleaning up/rearranging
    SVN revision 13033

--- src/io/bmi/bmi_rdma/rdma.c
+++ src/io/bmi/bmi_rdma/rdma.c
@@ -68,6 +68,76 @@ static const unsigned int IBV_NUM_CQ_ENTRIES = 1024;
 /* TODO: can the MTU be bumped up? */
 static const int IBV_MTU = IBV_MTU_1024;  /* dmtu, 1k good for mellanox */
 
+/*
+ * RDMA connection info passed to rdma_new_connection.
+ */
+struct rdma_conn_info
+{
+    struct rdma_cm_id *id;  /* connection identifier */
+    int port;               /* peer's port number */
+    char *hostname;         /* peer's addr */
+    char *peername;         /* combination of hostname and port number */
+};
+
+/*
+ * RDMA-private device-wide state.
+ */
+struct rdma_device_priv
+{
+    struct ibv_context *ctx;   /* context used to reference everything */
+    struct ibv_cq *nic_cq;     /* single completion queue for all QPs */
+    struct ibv_pd *nic_pd;     /* single protection domain for all memory/QPs */
+    /* TODO: is the nic_lid field needed? */
+    //uint16_t nic_lid;          /* local id (nic) */
+    int nic_port;              /* port number */
+    struct ibv_comp_channel *channel;
+
+    /* max values as reported by NIC */
+    int nic_max_sge;
+    int nic_max_wr;
+
+    /* MTU values reported by NIC port */
+    int max_mtu;
+    int active_mtu;
+
+    /*
+     * Temp array for filling scatter/gather lists to pass to RDMA functions,
+     * allocated once at start to max size defined as reported by the qp.
+     */
+    struct ibv_sge *sg_tmp_array;
+    uint32_t sg_max_len;
+
+    /*
+     * We use unsignaled sends.  They complete locally but we have no need to
+     * hear about it since the ack protocol with the peer handles freeing up
+     * buffers and whatever.  However, the completion queue _will_ fill up
+     * even though poll returns no results.  Hence you must post a signaled
+     * send every once in a while, per CQ, not per QP.  This tracks when we
+     * need to do the next.
+     */
+    unsigned int num_unsignaled_sends;
+    unsigned int max_unsignaled_sends;
+};
+
+/*
+ * Per-connection state.
+ */
+struct rdma_connection_priv
+{
+    /* connection id */
+    struct rdma_cm_id *id;
+
+    /* ibv local params */
+    struct ibv_qp *qp;
+    struct ibv_mr *eager_send_mr;
+    struct ibv_mr *eager_recv_mr;
+
+    /* TODO: are these still needed? */
+    /* rdma remote params */
+    //uint16_t remote_lid;
+    //uint32_t remote_qp_num;
+};
+
 /* function prototypes */
 static int BMI_rdma_initialize(struct bmi_method_addr *listen_addr,
                                int method_id,
@@ -332,89 +402,10 @@ static void mem_deregister(memcache_entry_t *c);
 
 static int build_rdma_context(void);
 
-/* TODO: quick hack - need to rearrange data structures and prototypes */
-struct rdma_device_priv;
 static int return_active_nic_handle(struct rdma_device_priv *rd,
                                     struct ibv_port_attr *hca_port);
 
 static void cleanup_rdma_context(void);
-
-
-/*
- * RDMA connection info passed to rdma_new_connection.
- */
-struct rdma_conn_info
-{
-    struct rdma_cm_id *id;  /* connection identifier */
-    int port;               /* peer's port number */
-    char *hostname;         /* peer's addr */
-    char *peername;         /* combination of hostname and port number */
-};
-
-/*
- * RDMA-private device-wide state.
- */
-struct rdma_device_priv
-{
-    struct ibv_context *ctx;   /* context used to reference everything */
-    struct ibv_cq *nic_cq;     /* single completion queue for all QPs */
-    struct ibv_pd *nic_pd;     /* single protection domain for all memory/QPs */
-    /* TODO: is the nic_lid field needed? */
-    //uint16_t nic_lid;          /* local id (nic) */
-    int nic_port;              /* port number */
-    struct ibv_comp_channel *channel;
-
-    /* max values as reported by NIC */
-    int nic_max_sge;
-    int nic_max_wr;
-
-    /* MTU values reported by NIC port */
-    int max_mtu;
-    int active_mtu;
-
-    /*
-     * Temp array for filling scatter/gather lists to pass to RDMA functions,
-     * allocated once at start to max size defined as reported by the qp.
-     */
-    struct ibv_sge *sg_tmp_array;
-    uint32_t sg_max_len;
-
-    /*
-     * We use unsignaled sends.  They complete locally but we have no need to
-     * hear about it since the ack protocol with the peer handles freeing up
-     * buffers and whatever.  However, the completion queue _will_ fill up
-     * even though poll returns no results.  Hence you must post a signaled
-     * send every once in a while, per CQ, not per QP.  This tracks when we
-     * need to do the next.
-     */
-    unsigned int num_unsignaled_sends;
-    unsigned int max_unsignaled_sends;
-};
-
-/*
- * Per-connection state.
- */
-struct rdma_connection_priv
-{
-    /* connection id */
-    struct rdma_cm_id *id;
-
-    /* ibv local params */
-    struct ibv_qp *qp;
-    struct ibv_mr *eager_send_mr;
-    struct ibv_mr *eager_recv_mr;
-
-    /* TODO: are these still needed? */
-    /* rdma remote params */
-    //uint16_t remote_lid;
-    //uint32_t remote_qp_num;
-};
-
-/* NOTE:  You have to be sure that ib_uverbs.ko is loaded, otherwise it
- * will segfault and/or not find the ib device.
- * TODO:  Find some way to check and see if ib_uverbs.ko is loaded (if
- *        needed for RDMA/RoCE)
- */
 
 
 /*
@@ -5376,6 +5367,12 @@ static int build_rdma_context(void)
 //    return nic_handle;
 //}
 #endif
+
+/* NOTE:  You have to be sure that ib_uverbs.ko is loaded, otherwise it
+ * will segfault and/or not find the ib device.
+ * TODO:  Find some way to check and see if ib_uverbs.ko is loaded (if
+ *        needed for RDMA/RoCE)
+ */
 
 /*
  * return_active_nic_handle()
