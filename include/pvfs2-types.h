4c9523e418598dd948352b4fe9dc7de8492ec44c
Walt Ligon <walt@omnibond.com>
2025-10-20 18:36:10 +0000

    more updates to debug macros

--- include/pvfs2-types.h
+++ include/pvfs2-types.h
@@ -1562,6 +1562,64 @@ endecode_fields_3a2a_struct (
                                     PVFS_SYS_LIMIT_ISSUER        + \
                                     PVFS_SYS_LIMIT_SIGNATURE)
 
+#define PVFS_debug_PVFS_capability(mask, cap) \
+do { \
+    gossip_if(mask) \
+    { \
+        PVFS_debug_PVFS_capability_nomask(cap); \
+    } \
+    gossip_end; \
+} while (0)
+
+#define PVFS_debug_PVFS_capability_nomask(cap) \
+do { \
+    char sig_buf[16] GCC_UNUSED; \
+    char mask_buf[16] GCC_UNUSED; \
+    int fail = 0; \
+    int i; \
+    if (!cap) \
+    { \
+        gossip_ladebug("capability pointer is NULL\n"); \
+        fail = 1; \
+    } \
+    else \
+    { \
+        if (!(cap)->issuer) \
+        { \
+            gossip_ladebug("capability issuer is NULL\n"); \
+        } \
+        else \
+        { \
+            if (strlen((cap)->issuer) == 0) \
+            { \
+                gossip_ladebug("null capability\n"); \
+            } \
+            else \
+            { \
+                gossip_ladebug("\tissuer: %s\n", (cap)->issuer); \
+            } \
+        } \
+    } \
+    if (!fail) \
+    { \
+        gossip_ladebug("fsid: %u\n", (cap)->fsid); \
+        gossip_ladebug("sig_size: %u\n", (cap)->sig_size); \
+        gossip_ladebug("signature: %s\n", \
+                 PINT_util_bytes2str((cap)->signature, sig_buf, 4)); \
+        gossip_ladebug("timeout: %d\n", (int) (cap)->timeout); \
+        gossip_ladebug("op_mask: %s\n", \
+                 PINT_print_op_mask((cap)->op_mask, mask_buf)); \
+        gossip_ladebug("num_handles: %u\n", (cap)->num_handles); \
+        gossip_ladebug("first handle: %s\n", \
+                 (cap)->num_handles > 0 ? PVFS_OID_str(&(cap)->handle_array[0]) : 0LL); \
+        for (i = 1; i < (cap)->num_handles; i++) \
+        { \
+            gossip_ladebug("handle %d: %s\n", \
+                         i + 1, PVFS_OID_str(&(cap)->handle_array[i])); \
+        } \
+    } \
+} while (0)
+
 /* A credential identifies a user and is signed by the client/user 
    private key. */
 typedef struct PVFS_credential PVFS_credential;
@@ -1594,38 +1652,42 @@ endecode_fields_3a2a1_struct (
                                     PVFS_SYS_LIMIT_SIGNATURE + \
                                     extra_size_PVFS_certificate)
 
-/* 
- * NOTE: for backwards compatibility only. 
- * For all new code use PVFS_credential.
- */
 typedef PVFS_credential PVFS_credentials;
 
-#define DEBUG_PVFS_CREDENTIAL(mask,cred) \
+#define PVFS_debug_PVFS_credential(mask, cred) \
 do { \
     gossip_if(mask) \
-        PVFS_debug_credential(cred); \
+    { \
+        PVFS_debug_PVFS_credential_nomask(cred); \
+    } \
     gossip_end; \
 } while (0)
 
-#define PVFS_debug_credential(cred) \
+#define PVFS_debug_PVFS_credential_nomask(cred) \
 do { \
-    if (cred) { \
+    if (cred) \
+    { \
         gossip_ladebug("PVFS_debug_credential\n"); \
         gossip_ladebug("userid:       %d\n", (cred)->userid); \
         gossip_ladebug("num_groups:   %d\n", (cred)->num_groups);   \
         gossip_ladebug("group_array (%p):\n", (cred)->group_array);  \
-        if((cred)->num_groups > 0 && (cred)->group_array) { \
+        if((cred)->num_groups > 0 && (cred)->group_array) \
+        { \
             int g; \
-            for(g = 0; g < (cred)->num_groups; g++) { \
+            for(g = 0; g < (cred)->num_groups; g++) \
+            { \
                 gossip_ladebug("    group %d\n", (cred)->group_array[g]); \
             } \
         }  \
-        if((cred)->issuer) { \
-                gossip_ladebug("issuer:  %s\n", (cred)->issuer); \
+        if((cred)->issuer) \
+        { \
+            gossip_ladebug("issuer:  %s\n", (cred)->issuer); \
         } \
         gossip_ladebug("sig_size:     %d\n", (cred)->sig_size);  \
         gossip_ladebug("PVFS_debug_credential End\n"); \
-    } else { \
+    } \
+    else \
+    { \
         gossip_ladebug("Credential pointer is NULL\n"); \
     } \
 } while(0)
